f447297a3a9480d8d649f938df25bafd5fa04029
aeoth <paul@theleagueofpaul.com>
2011-01-30 10:54:27 +1100

    * Added Joe McBride's MetroContentControl (see: http://xamlcoder.com/blog/2010/11/04/creating-a-metro-ui-style-control/) * Added extra styles (radiobutton, checkbox, etc) Note: Progress bar is stil a WIP. Translating it from Silverlight -> WPF is a little tricky

---
+++ MahApps.Metro.Controls/MetroContentControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    /// <summary>
+    /// Originally from http://xamlcoder.com/blog/2010/11/04/creating-a-metro-ui-style-control/
+    /// </summary>
+    public class MetroContentControl : ContentControl
+    {
+        public MetroContentControl()
+        {
+            DefaultStyleKey = typeof(MetroContentControl);
+
+            Loaded += MetroContentControlLoaded;
+            Unloaded += MetroContentControlUnloaded;
+        }
+
+        private void MetroContentControlUnloaded(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "AfterUnLoaded", false);
+        }
+
+        private void MetroContentControlLoaded(object sender, RoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "AfterLoaded", true);
+        }
+    }
+}
