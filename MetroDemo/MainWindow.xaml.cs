4fc2f367424f438ee3d0b655f91d244ac883dd68
aeoth <paul@theleagueofpaul.com>
2011-02-02 21:03:43 +1100

    * added some accenting support * discovered 2-3 wpf bugs

--- MetroDemo/MainWindow.xaml.cs
+++ MetroDemo/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Input;
 
 namespace MetroDemo
@@ -29,6 +32,46 @@ namespace MetroDemo
         private void BtnMaxClick(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+
+        private void MiDarkRed(object sender, RoutedEventArgs e)
+        {
+            var redRd = new ResourceDictionary();
+            var darkRd = new ResourceDictionary();
+
+            redRd.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Accents/Red.xaml");
+            darkRd.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Accents/BaseDark.xaml");
+
+
+            ApplyResourceDictionary(redRd);
+            ApplyResourceDictionary(darkRd);
+            
+
+        }
+
+        private void MiLightGreen(object sender, RoutedEventArgs e)
+        {
+            var greenRd = new ResourceDictionary();
+            var lightRd = new ResourceDictionary();
+
+            greenRd.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Accents/Green.xaml");
+            lightRd.Source = new Uri("pack://application:,,,/MahApps.Metro;component/Styles/Accents/BaseLight.xaml");
+
+
+            ApplyResourceDictionary(greenRd);
+            ApplyResourceDictionary(lightRd);
+        }
+
+        private void ApplyResourceDictionary(ResourceDictionary rd)
+        {
+            foreach (DictionaryEntry r in rd)
+            {
+                if (Resources.Contains(r.Key))
+                    Resources.Remove(r.Key);
+
+                Resources.Add(r.Key, r.Value);
+            }
         }
     }
 }
