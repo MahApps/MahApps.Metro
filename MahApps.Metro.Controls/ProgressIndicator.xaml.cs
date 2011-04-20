8aacf17e01936bc66c291910c1951c1daed06884
aeoth <paul@theleagueofpaul.com>
2011-04-20 19:32:57 +1000

    Progress indicator control Basedark changed to a dark grey/charcoal rather than solid bblack Fix for textbox layout issue

---
+++ MahApps.Metro.Controls/ProgressIndicator.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace MahApps.Metro.Controls
+{
+    public class WidthPercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var percentage = Double.Parse(parameter.ToString());
+            return ((double) value)*percentage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class ProgressIndicator
+    {
+        public ProgressIndicator()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static readonly DependencyProperty ProgressColourProperty = DependencyProperty.RegisterAttached("ProgressColour", typeof(Brush), typeof(ProgressIndicator), new UIPropertyMetadata(null));
+
+        public Brush ProgressColour
+        {
+            get { return (Brush)GetValue(ProgressColourProperty); }
+            set { SetValue(ProgressColourProperty, value); }
+        }
+
+        public void Stop()
+        {
+            this.Dispatcher.BeginInvoke(new Action(() =>
+                                                  {
+                                                      var s = this.Resources["animate"] as Storyboard;
+                                                      s.Stop();
+                                                      this.Visibility = Visibility.Collapsed;
+                                                  })
+                );
+        }
+    }
+}
