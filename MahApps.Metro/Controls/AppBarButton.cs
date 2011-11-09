ee5efb74fdfee47da159fabe3860b9c02bceb5a5
Jake Ginnivan <jake.ginnivan@readify.net>
2011-11-09 18:36:29 +0800

    Added AppBarButton for a button with Text under an image

---
+++ MahApps.Metro/Controls/AppBarButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    public class AppBarButton : Button
+    {
+        public AppBarButton()
+        {
+            DefaultStyleKey = typeof (AppBarButton);
+        }
+
+        public static readonly DependencyProperty MetroImageSourceProperty =
+            DependencyProperty.Register("MetroImageSource", typeof(Visual), typeof(AppBarButton), new PropertyMetadata(default(Visual)));
+
+        public Visual MetroImageSource
+        {
+            get { return (Visual)GetValue(MetroImageSourceProperty); }
+            set { SetValue(MetroImageSourceProperty, value); }
+        }
+    }
+}
