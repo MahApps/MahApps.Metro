ecc4bb07e2002b0c158774f7edfe7ba74ea6fd12
Jake Ginnivan <jake@ginnivan.net>
2011-11-09 16:53:12 +0800

    Added WindowCommands for the minimise/maximise/close buttons

---
+++ MahApps.Metro/Controls/WindowCommands.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    public partial class WindowCommands
+    {
+        public WindowCommands()
+        {
+            InitializeComponent();
+        }
+
+        public event ClosingWindowEventHandler ClosingWindow;
+
+        protected void OnClosingWindow(ClosingWindowEventHandlerArgs args)
+        {
+            var handler = ClosingWindow;
+            if (handler != null) handler(this, args);
+        }
+
+        private void MinimiseClick(object sender, RoutedEventArgs e)
+        {
+            var parentWindow = GetParentWindow();
+            if (parentWindow != null)
+            {
+                parentWindow.WindowState = WindowState.Minimized;
+            }
+        }
+
+        private void MaximiseClick(object sender, RoutedEventArgs e)
+        {
+            var parentWindow = GetParentWindow();
+            if (parentWindow != null)
+            {
+                parentWindow.WindowState = parentWindow.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            var closingWindowEventHandlerArgs = new ClosingWindowEventHandlerArgs();
+            OnClosingWindow(closingWindowEventHandlerArgs);
+
+            if (closingWindowEventHandlerArgs.Cancelled) return;
+
+            var parentWindow = GetParentWindow();
+            if (parentWindow != null)
+            {
+                parentWindow.Close();
+            }
+        }
+
+        private Window GetParentWindow()
+        {
+            var parent = VisualTreeHelper.GetParent(this);
+
+            while (parent != null && !(parent is Window))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            var parentWindow = parent as Window;
+            return parentWindow;
+        }
+
+        public delegate void ClosingWindowEventHandler(object sender, ClosingWindowEventHandlerArgs args);
+
+        public class ClosingWindowEventHandlerArgs : EventArgs
+        {
+            public bool Cancelled { get; set; }
+        }
+    }
+}
