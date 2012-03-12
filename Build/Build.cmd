9c8d4ffb3783a6be79454118031a26ca0d4207b0
aeoth <paul@theleagueofpaul.com>
2012-03-12 15:09:12 +1100

    build/assembly cleanup

--- Build/Build.cmd
+++ Build/Build.cmd
@@ -3,6 +3,6 @@ call "%VS100COMNTOOLS%vsvars32.bat"
 mkdir log\
 mkdir lib\net40\
 
-msbuild.exe /ToolsVersion:4.0 "..\MahApps.Metro\MahApps.Metro.csproj" /p:configuration=Release
+msbuild.exe /ToolsVersion:4.0 "..\MahApps.Metro\MahApps.Metro.csproj" /p:StrongName=True /p:configuration=Release
 ..\Utilities\NuGet.exe pack %~dp0..\MahApps.Metro\MahApps.Metro.nuspec -OutputDirectory %~dp0
 pause
