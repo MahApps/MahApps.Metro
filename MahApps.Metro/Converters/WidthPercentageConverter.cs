bc5fb204949c54faa2d0898f781488d3d87a1c2c
Robert Baker <se7enalive@gmail.com>
2012-03-18 16:11:59 -0700

    Fixed bug when scaling ProgressIndicator, it is now proportional scale. Changed Rectangle to Ellipse to match Win8 Styling.
    Set MinHeight and MaxHeight for ProgressIndicator. Changed the MetroCircleButtonStyle to change the opacity of the content property when the IsEnabled=False.
    
    Changed TabItem Headers to use HeaderFamily instead of DefaultFont
    
    Fixed Window Controls from not turning White when the dark theme is used and the titlebar banner is off.
    
    Added NormalForegroundBrush which uses NormalForegroundColor
    Changed BaseDark NormalForegroundColor from White to GrayBrush6
    Updated ListView, Expander, GroupItem,  to have more of a Metro feel, uses NormalForegroundColor for content.
    Added 2 font families used in Metro apps. Segoe Semilight and Semibold and changed the columnheader and expander to those fonts and recommended size.

---
+++ MahApps.Metro/Converters/WidthPercentageConverter.cs
@@ -0,0 +1,20 @@
+namespace MahApps.Metro.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class WidthPercentageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var percentage = Double.Parse(parameter.ToString(), new CultureInfo("en-US"));
+            return ((double) value)*percentage;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
