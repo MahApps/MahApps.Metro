49a8506785524b1c5c12c1fdfc9b9eafccd80429
aeoth <paul@theleagueofpaul.com>
2012-04-02 17:47:27 +1000

    Added a watermarker helper for the Textbox control
    Cleaned up several styles (such as removing VSM for far simpler triggers)
    Changed style on textbox to be closer to Win8

---
+++ MahApps.Metro/Controls/TextboxHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace MahApps.Metro.Controls
+{
+    public static class TextboxHelper
+    {
+        public static readonly DependencyProperty WatermarkProperty = DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(TextboxHelper), new FrameworkPropertyMetadata(string.Empty));
+
+        public static string GetWatermark(DependencyObject d)
+        {
+            return (string)d.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+    }
+}
