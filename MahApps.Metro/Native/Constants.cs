bf3d123188b59beaa43fbc3e495e8fccbbcc7b76
Tanel T <tanel@submain.eu>
2012-04-04 02:17:08 -0700

    Implemented SystemMenu on Titlebar and on window icon if visible. Tried to make it behave as close to regular window as possible.

--- MahApps.Metro/Native/Constants.cs
+++ MahApps.Metro/Native/Constants.cs
@@ -7,8 +7,6 @@
         public const int WM_NCPAINT = 0x85;
         public const int WM_NCACTIVATE = 0x86;
         public const int WM_GETMINMAXINFO = 0x24;
-        public const int WM_CREATE = 0x0001;
-        public const long WS_MAXIMIZE = 0x01000000;
         public const int GCLP_HBRBACKGROUND = -0x0A;
         public const int WM_NCHITTEST = 0x84;
         public const int HTLEFT = 0x0A;
@@ -19,5 +17,8 @@
         public const int HTBOTTOM = 0x0F;
         public const int HTBOTTOMLEFT = 0x10;
         public const int HTBOTTOMRIGHT = 0x11;
+        public const uint TPM_RETURNCMD = 0x0100;
+        public const uint TPM_LEFTBUTTON = 0x0;
+        public const uint SYSCOMMAND = 0x0112;
     }
 }
