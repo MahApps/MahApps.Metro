d041762fbe2b87825d882946f4002da496453034
Paul Jenkins <paul@jenkinsfamily.com.au>
2012-05-07 20:04:39 +1000

    Cleaned up ProgressRing a little, set default foreground colour to AccentColorBrush. There isn't a particular Win8 "standard" colour scheme

--- MahApps.Metro/Controls/ProgressRing.cs
+++ MahApps.Metro/Controls/ProgressRing.cs
@@ -12,34 +12,21 @@ namespace MahApps.Metro.Controls
     [TemplateVisualState(Name = "Active", GroupName = "ActiveStates")]
     public class ProgressRing : Control
     {
-        public static readonly DependencyProperty BindableWidthProperty =
-            DependencyProperty.Register("BindableWidth", typeof (double), typeof (ProgressRing),
-                                        new PropertyMetadata(default(double), BindableWidthCallback));
+        public static readonly DependencyProperty BindableWidthProperty = DependencyProperty.Register("BindableWidth", typeof(double), typeof(ProgressRing), new PropertyMetadata(default(double), BindableWidthCallback));
 
-        public static readonly DependencyProperty IsActiveProperty =
-            DependencyProperty.Register("IsActive", typeof (bool), typeof (ProgressRing),
-                                        new PropertyMetadata(default(bool), IsActiveChanged));
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(ProgressRing), new PropertyMetadata(default(bool), IsActiveChanged));
 
-        public static readonly DependencyProperty IsLargeProperty =
-            DependencyProperty.Register("IsLarge", typeof (bool), typeof (ProgressRing),
-                                        new PropertyMetadata(true, IsLargeChangedCallback));
+        public static readonly DependencyProperty IsLargeProperty = DependencyProperty.Register("IsLarge", typeof(bool), typeof(ProgressRing), new PropertyMetadata(true, IsLargeChangedCallback));
 
-        public static readonly DependencyProperty MaxSideLengthProperty =
-            DependencyProperty.Register("MaxSideLength", typeof (double), typeof (ProgressRing),
-                                        new PropertyMetadata(default(double)));
+        public static readonly DependencyProperty MaxSideLengthProperty = DependencyProperty.Register("MaxSideLength", typeof(double), typeof(ProgressRing), new PropertyMetadata(default(double)));
 
-        public static readonly DependencyProperty EllipseDiameterProperty =
-            DependencyProperty.Register("EllipseDiameter", typeof (double), typeof (ProgressRing),
-                                        new PropertyMetadata(default(double)));
+        public static readonly DependencyProperty EllipseDiameterProperty = DependencyProperty.Register("EllipseDiameter", typeof(double), typeof(ProgressRing), new PropertyMetadata(default(double)));
 
-        public static readonly DependencyProperty EllipseOffsetProperty =
-            DependencyProperty.Register("EllipseOffset", typeof (Thickness), typeof (ProgressRing),
-                                        new PropertyMetadata(default(Thickness)));
+        public static readonly DependencyProperty EllipseOffsetProperty = DependencyProperty.Register("EllipseOffset", typeof(Thickness), typeof(ProgressRing), new PropertyMetadata(default(Thickness)));
 
         static ProgressRing()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof (ProgressRing),
-                                                     new FrameworkPropertyMetadata(typeof (ProgressRing)));
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ProgressRing), new FrameworkPropertyMetadata(typeof(ProgressRing)));
         }
 
         public ProgressRing()
@@ -49,62 +36,54 @@ namespace MahApps.Metro.Controls
 
         public double MaxSideLength
         {
-            get { return (double) GetValue(MaxSideLengthProperty); }
+            get { return (double)GetValue(MaxSideLengthProperty); }
             private set { SetValue(MaxSideLengthProperty, value); }
         }
 
         public double EllipseDiameter
         {
-            get { return (double) GetValue(EllipseDiameterProperty); }
+            get { return (double)GetValue(EllipseDiameterProperty); }
             private set { SetValue(EllipseDiameterProperty, value); }
         }
 
         public Thickness EllipseOffset
         {
-            get { return (Thickness) GetValue(EllipseOffsetProperty); }
+            get { return (Thickness)GetValue(EllipseOffsetProperty); }
             private set { SetValue(EllipseOffsetProperty, value); }
         }
 
         public double BindableWidth
         {
-            get { return (double) GetValue(BindableWidthProperty); }
+            get { return (double)GetValue(BindableWidthProperty); }
             private set { SetValue(BindableWidthProperty, value); }
         }
 
         public bool IsActive
         {
-            get { return (bool) GetValue(IsActiveProperty); }
+            get { return (bool)GetValue(IsActiveProperty); }
             set { SetValue(IsActiveProperty, value); }
         }
 
         public bool IsLarge
         {
-            get { return (bool) GetValue(IsLargeProperty); }
+            get { return (bool)GetValue(IsLargeProperty); }
             set { SetValue(IsLargeProperty, value); }
         }
 
-        private static void BindableWidthCallback(DependencyObject dependencyObject,
-                                                  DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        private static void BindableWidthCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
         {
-            if (dependencyObject is ProgressRing)
-            {
-                var ring = (ProgressRing) dependencyObject;
-                ring.SetEllipseDiameter((double) dependencyPropertyChangedEventArgs.NewValue);
-                ring.SetEllipseOffset((double) dependencyPropertyChangedEventArgs.NewValue);
-                ring.SetMaxSideLength((double) dependencyPropertyChangedEventArgs.NewValue);
-            }
+            var ring = dependencyObject as ProgressRing;
+            if (ring == null)
+                return;
+
+            ring.SetEllipseDiameter((double)dependencyPropertyChangedEventArgs.NewValue);
+            ring.SetEllipseOffset((double)dependencyPropertyChangedEventArgs.NewValue);
+            ring.SetMaxSideLength((double)dependencyPropertyChangedEventArgs.NewValue);
         }
 
         private void SetMaxSideLength(double width)
         {
-            if (width <= 60)
-            {
-                MaxSideLength = 60.0;
-            }
-            else
-            {
-                MaxSideLength = width;
-            }
+            MaxSideLength = width <= 60 ? 60.0 : width;
         }
 
         private void SetEllipseDiameter(double width)
@@ -115,7 +94,7 @@ namespace MahApps.Metro.Controls
             }
             else
             {
-                EllipseDiameter = width*0.1 + 6;
+                EllipseDiameter = width * 0.1 + 6;
             }
         }
 
@@ -128,36 +107,33 @@ namespace MahApps.Metro.Controls
             }
             else
             {
-                EllipseOffset = new Thickness(0, width*0.4 + 24, 0, 0);
+                EllipseOffset = new Thickness(0, width * 0.4 + 24, 0, 0);
             }
         }
 
-        private static void IsLargeChangedCallback(DependencyObject dependencyObject,
-                                                   DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        private static void IsLargeChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
         {
             var ring = dependencyObject as ProgressRing;
-            if (ring != null)
-            {
-                if ((bool) dependencyPropertyChangedEventArgs.NewValue)
-                    VisualStateManager.GoToState(ring, "Large", true);
-                else
-                    VisualStateManager.GoToState(ring, "Small", true);
-            }
-        }
+            if (ring == null)
+                return;
 
+            if ((bool)dependencyPropertyChangedEventArgs.NewValue)
+                VisualStateManager.GoToState(ring, "Large", true);
+            else
+                VisualStateManager.GoToState(ring, "Small", true);
+        }
 
         private void OnSizeChanged(object sender, SizeChangedEventArgs sizeChangedEventArgs)
         {
             BindableWidth = ActualWidth;
         }
 
-        private static void IsActiveChanged(DependencyObject dependencyObject,
-                                            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        private static void IsActiveChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
         {
             var ring = dependencyObject as ProgressRing;
             if (ring != null)
             {
-                if ((bool) dependencyPropertyChangedEventArgs.NewValue)
+                if ((bool)dependencyPropertyChangedEventArgs.NewValue)
 
                     VisualStateManager.GoToState(ring, "Active", true);
                 else
@@ -181,24 +157,14 @@ namespace MahApps.Metro.Controls
         }
     }
 
-
     internal class WidthToMaxSideLengthConverter : IValueConverter
     {
-        #region IValueConverter Members
-
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is double)
             {
-                var width = (double) value;
-                if (width <= 60)
-                {
-                    return 60.0;
-                }
-                else
-                {
-                    return width;
-                }
+                var width = (double)value;
+                return width <= 60 ? 60.0 : width;
             }
 
             return null;
@@ -208,7 +174,5 @@ namespace MahApps.Metro.Controls
         {
             throw new NotImplementedException();
         }
-
-        #endregion
     }
 }
