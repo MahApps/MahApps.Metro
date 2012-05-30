1b3422159030d74f470360c380529374eee9f512
Paul Jenkins <paul@jenkinsfamily.com.au>
2012-05-30 12:24:12 +1000

    Added RangeSlider from http://avaloncontrolslib.codeplex.com/ (MS-PL)

---
+++ MahApps.Metro/Controls/RangeSelectionChangedEventArgs.cs
@@ -0,0 +1,22 @@
+using System.Windows;
+
+namespace MahApps.Metro.Controls
+{
+    public class RangeSelectionChangedEventArgs : RoutedEventArgs
+    {
+        public long NewRangeStart { get; set; }
+        public long NewRangeStop { get; set; }
+
+        internal RangeSelectionChangedEventArgs(long newRangeStart, long newRangeStop)
+        {
+            NewRangeStart = newRangeStart;
+            NewRangeStop = newRangeStop;
+        }
+
+        internal RangeSelectionChangedEventArgs(RangeSlider slider)
+            : this(slider.RangeStartSelected, slider.RangeStopSelected)
+        {
+            
+        }
+    }
+}
