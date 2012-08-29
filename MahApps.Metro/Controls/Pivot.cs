c9bd9725a2c36626c343a9dc9b74a6ab3de02fef
Paul <paul@jenkinsfamily.com.au>
2012-08-29 20:42:06 +1000

    Pivot navigation

--- MahApps.Metro/Controls/Pivot.cs
+++ MahApps.Metro/Controls/Pivot.cs
@@ -4,9 +4,13 @@ using System.Windows.Controls;
 namespace MahApps.Metro.Controls
 {
     [TemplatePart(Name = "PART_Scroll", Type = typeof(ScrollViewer))]
+    [TemplatePart(Name = "PART_Headers", Type = typeof(ListView))]
     public class Pivot : ItemsControl
     {
         private ScrollViewer scroller;
+        private ListView headers;
+        private PivotItem selectedItem;
+
         public static readonly RoutedEvent SelectionChangedEvent = EventManager.RegisterRoutedEvent("SelectionChanged", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Pivot));
         public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(Pivot), new PropertyMetadata(default(string)));
         public static readonly DependencyProperty HeaderTemplateProperty = DependencyProperty.Register("HeaderTemplate", typeof(DataTemplate), typeof(Pivot));
@@ -31,6 +35,9 @@ namespace MahApps.Metro.Controls
 
         public void GoToItem(PivotItem item)
         {
+            if (item == null || item == selectedItem)
+                return;
+
             var widthToScroll = 0.0;
             for (int i = 0; i < Items.Count; i++)
             {
@@ -54,6 +61,36 @@ namespace MahApps.Metro.Controls
         {
             base.OnApplyTemplate();
             scroller = (ScrollViewer)GetTemplateChild("PART_Scroll");
+            headers = (ListView) GetTemplateChild("PART_Headers");
+
+            if (scroller != null) 
+                scroller.ScrollChanged += scroller_ScrollChanged;
+
+            if (headers != null) 
+                headers.SelectionChanged += headers_SelectionChanged;
+        }
+
+        void headers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GoToItem((PivotItem)headers.SelectedItem);
+        }
+
+        void scroller_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var position = 0.0;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                var pivotItem = ((PivotItem) Items[i]);
+                var widthOfItem = pivotItem.ActualWidth;
+                if (e.HorizontalOffset <= (position + widthOfItem -1))
+                {
+                    selectedItem = pivotItem;
+                    if (headers.SelectedItem != selectedItem)
+                        headers.SelectedItem = selectedItem;
+                    break;
+                }
+                position += widthOfItem;
+            }
         }
     }
 }
