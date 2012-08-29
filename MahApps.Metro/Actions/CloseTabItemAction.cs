78f5ea1dc6ac0024d965aa76d69a88db983d7ae8
Paul <paul@jenkinsfamily.com.au>
2012-08-29 19:06:51 +1000

    little bit of cleanup

--- MahApps.Metro/Actions/CloseTabItemAction.cs
+++ MahApps.Metro/Actions/CloseTabItemAction.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interactivity;
 
@@ -10,27 +6,24 @@ namespace MahApps.Metro.Actions
 {
     public class CloseTabItemAction : TriggerAction<DependencyObject>
     {
-        protected override void Invoke(object parameter)
-        {
-            this.TabControl.Items.Remove(this.TabItem);
-        }
-
-        public static readonly DependencyProperty TabControlProperty =
-            DependencyProperty.Register("TabControl", typeof (TabControl), typeof (CloseTabItemAction), new PropertyMetadata(default(TabControl)));
+        public static readonly DependencyProperty TabControlProperty = DependencyProperty.Register("TabControl", typeof(TabControl), typeof(CloseTabItemAction), new PropertyMetadata(default(TabControl)));
+        public static readonly DependencyProperty TabItemProperty = DependencyProperty.Register("TabItem", typeof(TabItem), typeof(CloseTabItemAction), new PropertyMetadata(default(TabItem)));
 
         public TabControl TabControl
         {
-            get { return (TabControl) GetValue(TabControlProperty); }
+            get { return (TabControl)GetValue(TabControlProperty); }
             set { SetValue(TabControlProperty, value); }
         }
 
-        public static readonly DependencyProperty TabItemProperty =
-            DependencyProperty.Register("TabItem", typeof (TabItem), typeof (CloseTabItemAction), new PropertyMetadata(default(TabItem)));
-
         public TabItem TabItem
         {
-            get { return (TabItem) GetValue(TabItemProperty); }
+            get { return (TabItem)GetValue(TabItemProperty); }
             set { SetValue(TabItemProperty, value); }
+        }
+
+        protected override void Invoke(object parameter)
+        {
+            TabControl.Items.Remove(TabItem);
         }
     }
 }
