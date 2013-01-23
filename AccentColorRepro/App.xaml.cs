095bfa7a8914ad787033c8c6c568b587008e66e1
Henrik Ohlsson <henrik.ohlsson@capsor.se>
2013-01-23 08:54:01 +0100

    Added a demo project for illustrating the AccentColorBrush issue.

---
+++ AccentColorRepro/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AccentColorRepro
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
