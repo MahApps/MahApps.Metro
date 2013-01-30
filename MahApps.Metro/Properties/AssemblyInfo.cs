8c67865d5a9f17e257052f408bc3e88714894e84
Brendan Forster <me@brendanforster.com>
2013-01-30 14:06:55 +1100

    added task and removed duplicate fields from existing file

--- MahApps.Metro/Properties/AssemblyInfo.cs
+++ MahApps.Metro/Properties/AssemblyInfo.cs
@@ -3,18 +3,8 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyTitle("MahApps.Metro")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MahApps")]
-[assembly: AssemblyProduct("MahApps.Metro")]
-[assembly: AssemblyCopyright("Copyright © MahApps 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.10.1.0")]
-[assembly: AssemblyFileVersion("0.10.1.0")]
+
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/shared", "MahApps.Metro.Behaviours")]
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/shared", "MahApps.Metro.Converters")]
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/controls", "MahApps.Metro.Controls")]
