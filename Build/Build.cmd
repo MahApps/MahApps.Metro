a399b9e8c808cca12a46e82e6efb6a1d1543cb92
Brendan Forster <me@brendanforster.com>
2013-02-03 17:01:17 +1100

    deprecated old script

--- Build/Build.cmd
+++
@@ -1,8 +0,0 @@
-@echo on
-call "%VS100COMNTOOLS%vsvars32.bat"
-
-msbuild.exe /ToolsVersion:4.0 "..\MahApps.Metro\MahApps.Metro.csproj" /p:StrongName=True /p:configuration=Release
-msbuild.exe /ToolsVersion:4.0 "..\MahApps.Metro\MahApps.Metro.NET45.csproj" /p:StrongName=True /p:configuration=Release
-
-..\Utilities\NuGet.exe pack %~dp0..\MahApps.Metro\MahApps.Metro.nuspec -OutputDirectory %~dp0
-pause
