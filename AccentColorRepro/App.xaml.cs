221376412af8357d4df4a20514b929395d15da76
Brendan Forster <me@brendanforster.com>
2013-02-03 15:01:40 +1100

    cleanup code behind files in sample

--- AccentColorRepro/App.xaml.cs
+++ AccentColorRepro/App.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System.Windows;
-
-namespace AccentColorRepro
+﻿namespace AccentColorRepro
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
