a01943cc7ed2c3c0f519aa720bbe812ba16e5909
Brendan Forster <me@brendanforster.com>
2013-02-03 16:59:26 +1100

    current stable version named here

--- Build/build.ps1
+++ Build/build.ps1
@@ -5,17 +5,18 @@ function Get-ScriptDirectory
 }
 
 # build the solution from scratch
+$build_number = "0.10.2.0"
 $version = "v4.0.30319"
 $proj = Join-Path (Get-ScriptDirectory) ..\MahApps.Metro\MahApps.Metro.csproj
 $solution_dir = Join-Path (Get-ScriptDirectory) ..\
 
-. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:SolutionDir=$solution_dir /p:StrongName=True /p:configuration=Release /m /v:q
+. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:SolutionDir=$solution_dir /p:StrongName=True /p:configuration=Release /m /v:q /p:BUILD_NUMBER=$build_number
 
 # package it up
 
 $nuget = Join-Path (Get-ScriptDirectory) ..\Utilities\NuGet.exe
 $nuspec = Join-Path (Get-ScriptDirectory) ..\MahApps.Metro\MahApps.Metro.nuspec
 
-. $nuget pack $nuspec -OutputDirectory (Get-ScriptDirectory)
+. $nuget pack $nuspec -OutputDirectory (Get-ScriptDirectory) -Version $build_number
 
 
