95d4868dfde7d0f07e0d07d06265b8e6a6e67d49
Brendan Forster <me@brendanforster.com>
2013-02-03 17:59:12 +1100

    removed unnecessary using statements

--- MahApps.Metro/Properties/AssemblyInfo.cs
+++ MahApps.Metro/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
