98b8b5e9c1608ea9f3a6e87faaf929695e7f4ec2
Amrykid <amrykid@gmail.com>
2013-02-08 19:17:53 -0600

    Added a helper class for changing the font size.

---
+++ MahApps.Metro/Controls/TabItemHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public class TabItemHelper : DependencyObject
+    {
+        public static readonly DependencyProperty HeaderFontSizeProperty = DependencyProperty.RegisterAttached("HeaderFontSize", typeof(double), typeof(TabItemHelper), new PropertyMetadata(26.67));
+
+        public static double GetHeaderFontSize(DependencyObject obj)
+        {
+            return (double)obj.GetValue(HeaderFontSizeProperty);
+        }
+
+        public static void SetHeaderFontSize(DependencyObject obj, double value)
+        {
+            obj.SetValue(HeaderFontSizeProperty, value);
+        }
+    }
+}
