e16154e0157ebe8e7720d5a7e26feac95b204dc5
Jan Karger <punkerat76@jkarger.de>
2013-02-14 13:02:29 +0100

    "save window settings elsewhere" scenario

--- MahApps.Metro/Behaviours/WindowsSettingBehaviour.cs
+++ MahApps.Metro/Behaviours/WindowsSettingBehaviour.cs
@@ -7,7 +7,11 @@ namespace MahApps.Metro.Behaviours
     {
         protected override void OnAttached()
         {
-            WindowSettings.SetSave(AssociatedObject, AssociatedObject.SaveWindowPosition);
+            if (AssociatedObject != null && AssociatedObject.SaveWindowPosition) {
+                // save with custom settings class or use the default way
+                var windowPlacementSettings = this.AssociatedObject.WindowPlacementSettings ?? new WindowApplicationSettings(this.AssociatedObject);
+                WindowSettings.SetSave(AssociatedObject, windowPlacementSettings);
+            }
         }
     }
 }
