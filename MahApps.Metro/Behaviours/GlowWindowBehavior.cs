b9fcd2223c4180109d9995ce051665f688290ead
Chandramouleswaran <chandramouleswaran@gmail.com>
2013-02-17 14:04:21 -0600

    1. Glow window additional functions. 2. Toolbar - Add margin so that it looks similar to VS.

--- MahApps.Metro/Behaviours/GlowWindowBehavior.cs
+++ MahApps.Metro/Behaviours/GlowWindowBehavior.cs
@@ -8,29 +8,51 @@ using MahApps.Metro.Controls;
 
 namespace MahApps.Metro.Behaviours
 {
-    class GlowWindowBehavior : Behavior<Window>
+    public class GlowWindowBehavior : Behavior<Window>
     {
+        private GlowWindow left, right, top, bottom;
         protected override void OnAttached()
         {
             base.OnAttached();
 
             this.AssociatedObject.Loaded += (sender, e) =>
             {
-                var left = new GlowWindow(this.AssociatedObject, GlowDirection.Left);
-                var right = new GlowWindow(this.AssociatedObject, GlowDirection.Right);
-                var top = new GlowWindow(this.AssociatedObject, GlowDirection.Top);
-                var bottom = new GlowWindow(this.AssociatedObject, GlowDirection.Bottom);
+                left = new GlowWindow(this.AssociatedObject, GlowDirection.Left);
+                right = new GlowWindow(this.AssociatedObject, GlowDirection.Right);
+                top = new GlowWindow(this.AssociatedObject, GlowDirection.Top);
+                bottom = new GlowWindow(this.AssociatedObject, GlowDirection.Bottom);
 
-                left.Show();
-                right.Show();
-                top.Show();
-                bottom.Show();
+                Show();
 
                 left.Update();
                 right.Update();
                 top.Update();
                 bottom.Update();
-            }; ;
+            };
+
+            this.AssociatedObject.Closed += (sender, args) =>
+            {
+                left.Close();
+                right.Close();
+                top.Close();
+                bottom.Close();
+            };
+        }
+
+        public void Hide()
+        {
+            left.Hide();
+            right.Hide();
+            bottom.Hide();
+            top.Hide();
+        }
+
+        public void Show()
+        {
+            left.Show();
+            right.Show();
+            top.Show();
+            bottom.Show();
         }
     }
 }
