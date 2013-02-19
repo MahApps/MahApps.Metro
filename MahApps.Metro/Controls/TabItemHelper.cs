b20b515e0decdeedd35cf3f327ff86d683509fdc
Amrykid <amrykid@gmail.com>
2013-02-19 17:00:44 -0600

    Removed whitespace and added comments.

--- MahApps.Metro/Controls/TabItemHelper.cs
+++ MahApps.Metro/Controls/TabItemHelper.cs
@@ -10,21 +10,19 @@ namespace MahApps.Metro.Controls
 
         private static void HeaderFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (((Control)d).IsLoaded)
+            if (((Control)d).IsLoaded) //Check if the tabitem is loaded.
             {
-                SetHeaderLabelFontSize(d, e);
+                SetHeaderLabelFontSize(d, e); //The tabitem is loaded. Set the font size immediately.
             }
             else
             {
+                //Tabitem hasn't loaded yet. Attach a temporary event handler to set the font size once it loads.
                 RoutedEventHandler handler = null; handler = delegate(object sender, RoutedEventArgs args)
                    {
-
                        SetHeaderLabelFontSize(d, e);
 
                        ((Control)d).Loaded -= handler;
                    };
-
-
                 ((Control)d).Loaded += handler;
             }
         }
@@ -32,7 +30,6 @@ namespace MahApps.Metro.Controls
         private static void SetHeaderLabelFontSize(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var obj = ((Control)d).Template.FindName("root", (FrameworkElement)d) as Label;
-
             obj.FontSize = (double)e.NewValue;
         }
 
