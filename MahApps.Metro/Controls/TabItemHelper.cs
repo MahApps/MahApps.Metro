c59ed96bc7fdd847874fe56fb5cfcb318ad473df
Amrykid <amrykid@gmail.com>
2013-02-19 16:56:47 -0600

    Using black magic to set the header font size through code instead of the template.
    Signed-off-by: Amrykid <amrykid@gmail.com>

--- MahApps.Metro/Controls/TabItemHelper.cs
+++ MahApps.Metro/Controls/TabItemHelper.cs
@@ -6,7 +6,35 @@ namespace MahApps.Metro.Controls
 {
     public class TabItemHelper : DependencyObject
     {
-        public static readonly DependencyProperty HeaderFontSizeProperty = DependencyProperty.RegisterAttached("HeaderFontSize", typeof(double), typeof(TabItemHelper), new PropertyMetadata(26.67));
+        public static readonly DependencyProperty HeaderFontSizeProperty = DependencyProperty.RegisterAttached("HeaderFontSize", typeof(double), typeof(TabItemHelper), new PropertyMetadata(26.67, new PropertyChangedCallback(HeaderFontSizeChanged)));
+
+        private static void HeaderFontSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (((Control)d).IsLoaded)
+            {
+                SetHeaderLabelFontSize(d, e);
+            }
+            else
+            {
+                RoutedEventHandler handler = null; handler = delegate(object sender, RoutedEventArgs args)
+                   {
+
+                       SetHeaderLabelFontSize(d, e);
+
+                       ((Control)d).Loaded -= handler;
+                   };
+
+
+                ((Control)d).Loaded += handler;
+            }
+        }
+
+        private static void SetHeaderLabelFontSize(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = ((Control)d).Template.FindName("root", (FrameworkElement)d) as Label;
+
+            obj.FontSize = (double)e.NewValue;
+        }
 
         public static double GetHeaderFontSize(DependencyObject obj)
         {
