2a025b704c2ec536a748692de47a5fa7ba9d30ee
Selim <selimozyalcin@hotmail.com>
2013-02-21 04:14:52 +0200

    TabControl's TabStrip's margin and font size of the TabItem can be set by the user

---
+++ MahApps.Metro/Controls/MetroAnimatedSingleRowTabControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public class MetroAnimatedSingleRowTabControl : TabControl
+    {
+        public Thickness TabStripMargin
+        {
+            get { return (Thickness)GetValue(TabStripMarginProperty); }
+            set { SetValue(TabStripMarginProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TabStripMargin.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TabStripMarginProperty =
+            DependencyProperty.Register("TabStripMargin", typeof(Thickness), typeof(MetroAnimatedSingleRowTabControl), new PropertyMetadata(new Thickness(0)));
+
+        public MetroAnimatedSingleRowTabControl()
+        {
+            DefaultStyleKey = typeof(MetroAnimatedSingleRowTabControl);
+        }
+    }
+}
