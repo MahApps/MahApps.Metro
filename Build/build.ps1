19ac019795f4a69e8f6cca269cbb9d2949277653
Brendan Forster <me@brendanforster.com>
2013-04-12 09:21:33 +1200

    right so i was an idiot

--- Build/build.ps1
+++ Build/build.ps1
@@ -8,13 +8,12 @@ function Get-ScriptDirectory
 $build_number = "0.11.0.0"
 $version = "v4.0.30319"
 $proj = Join-Path (Get-ScriptDirectory) ..\MahApps.Metro\MahApps.Metro.csproj
-$solution_dir = Join-Path (Get-ScriptDirectory) ..\
 
-. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:SolutionDir=$solution_dir /p:StrongName=True /p:configuration=Release /m /v:q /p:BUILD_NUMBER=$build_number
+. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:StrongName=False /p:configuration=Release /m /v:q /p:BUILD_NUMBER=$build_number
 
 $proj = Join-Path (Get-ScriptDirectory) ..\MahApps.Metro\MahApps.Metro.NET45.csproj
 
-. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:SolutionDir=$solution_dir /p:StrongName=True /p:configuration=Release /m /v:q /p:BUILD_NUMBER=$build_number
+. $env:windir\Microsoft.NET\Framework\$version\MSBuild.exe $proj /t:Rebuild /ToolsVersion:4.0 /p:StrongName=False /p:configuration=Release /m /v:q /p:BUILD_NUMBER=$build_number
 
 # package it up
 
@@ -22,5 +21,3 @@ $nuget = Join-Path (Get-ScriptDirectory) ..\Utilities\NuGet.exe
 $nuspec = Join-Path (Get-ScriptDirectory) ..\MahApps.Metro\MahApps.Metro.nuspec
 
 . $nuget pack $nuspec -OutputDirectory (Get-ScriptDirectory) -Version $build_number
-
-
