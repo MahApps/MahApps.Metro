52af067bf650dfbbb62a16f068ed3cdd40b0b7cc
Amrykid <amrykid@gmail.com>
2013-05-20 06:53:46 -0500

    Forgot to override the MetroAnimated*TabControls.

--- MahApps.Metro/Controls/MetroAnimatedTabControl.cs
+++ MahApps.Metro/Controls/MetroAnimatedTabControl.cs
@@ -19,5 +19,10 @@ namespace MahApps.Metro.Controls
         // Using a DependencyProperty as the backing store for TabStripMargin.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TabStripMarginProperty =
             DependencyProperty.Register("TabStripMargin", typeof(Thickness), typeof(MetroAnimatedTabControl), new PropertyMetadata(new Thickness(0)));
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new MetroTabItem(); //Overrides the TabControl's default behavior and returns a MetroTabItem instead of a regular one.
+        }
     }
 }
