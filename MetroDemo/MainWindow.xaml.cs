6ef6ae8f3c7e00e1a29072704f8158f94c33984d
Dennis Daume <daume.dennis@gmail.com>
2013-05-22 22:14:43 +0200

    Added theme and accent changing options through in the menu
    Oh and apparently the TextBlock "control" doesn't respond to theme
    changing, so I had to replace every single one with a Label

--- MetroDemo/MainWindow.xaml.cs
+++ MetroDemo/MainWindow.xaml.cs
@@ -1,23 +1,75 @@
 ﻿using System;
+using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Threading;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
 
 namespace MetroDemo
 {
     public partial class MainWindow
     {
+        private Theme currentTheme = Theme.Light;
+        private Accent currentAccent = ThemeManager.DefaultAccents.First(x => x.Name == "Blue");
+
         public MainWindow()
         {
             DataContext = new MainWindowViewModel();
             InitializeComponent();
-            var t = new DispatcherTimer(TimeSpan.FromSeconds(2), DispatcherPriority.Normal, (sender, args) => this.TransitionTick(), this.Dispatcher);
+            var t = new DispatcherTimer(TimeSpan.FromSeconds(2), DispatcherPriority.Normal, Tick, this.Dispatcher);
         }
 
-        private void TransitionTick()
+        void Tick(object sender, EventArgs e)
         {
             var dateTime = DateTime.Now;
             transitioning.Content = new TextBlock {Text = "Transitioning Content! " + dateTime, SnapsToDevicePixels = true};
             customTransitioning.Content = new TextBlock {Text = "Custom transistion! " + dateTime, SnapsToDevicePixels = true};
+        }
+
+        private void ChangeAccent(string accentName)
+        {
+            this.currentAccent = ThemeManager.DefaultAccents.First(x => x.Name == accentName);
+
+            ThemeManager.ChangeTheme(this, this.currentAccent, this.currentTheme);
+        }
+
+        private void AccentRed(object sender, RoutedEventArgs e)
+        {
+            this.ChangeAccent("Red");
+        }
+
+        private void AccentGreen(object sender, RoutedEventArgs e)
+        {
+            this.ChangeAccent("Green");
+        }
+
+        private void AccentBlue(object sender, RoutedEventArgs e)
+        {
+            this.ChangeAccent("Blue");
+        }
+
+        private void AccentPurple(object sender, RoutedEventArgs e)
+        {
+            this.ChangeAccent("Purple");
+        }
+
+        private void AccentOrange(object sender, RoutedEventArgs e)
+        {
+            this.ChangeAccent("Orange");
+        }
+
+        private void ThemeLight(object sender, RoutedEventArgs e)
+        {
+            this.currentTheme = Theme.Light;
+            ThemeManager.ChangeTheme(this, this.currentAccent, Theme.Light);
+        }
+
+        private void AccentDark(object sender, RoutedEventArgs e)
+        {
+            this.currentTheme = Theme.Dark;
+            ThemeManager.ChangeTheme(this, this.currentAccent, Theme.Dark);
         }
     }
 }
