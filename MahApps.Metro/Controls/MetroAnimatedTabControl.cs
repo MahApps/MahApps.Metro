caef59cc07f01a6a87a7bfc9ac8714ebd9020f50
Amrykid <amrykid@gmail.com>
2013-06-01 13:18:52 -0500

    Redid the closable Tab Items. Close Button should be placed in a /better/ location depending on the Header's fontsize.

--- MahApps.Metro/Controls/MetroAnimatedTabControl.cs
+++ MahApps.Metro/Controls/MetroAnimatedTabControl.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MahApps.Metro.Controls
 {
@@ -24,5 +25,14 @@ namespace MahApps.Metro.Controls
         {
             return new MetroTabItem(); //Overrides the TabControl's default behavior and returns a MetroTabItem instead of a regular one.
         }
+
+        public ICommand CloseTabCommand
+        {
+            get { return (ICommand)GetValue(CloseTabCommandProperty); }
+            set { SetValue(CloseTabCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty CloseTabCommandProperty =
+            DependencyProperty.Register("CloseTabCommand", typeof(ICommand), typeof(MetroAnimatedTabControl), new PropertyMetadata(null));
     }
 }
