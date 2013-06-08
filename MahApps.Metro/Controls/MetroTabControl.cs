0a2afbaadd94308a916cb36b5e93405c368866b5
Amrykid <amrykid@gmail.com>
2013-06-08 07:41:44 -0500

    CloseTabCommand should work /better/. All TabControls inherit from BaseMetroTabControl in order to make extending the TabControls easier.

--- MahApps.Metro/Controls/MetroTabControl.cs
+++ MahApps.Metro/Controls/MetroTabControl.cs
@@ -1,14 +1,23 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace MahApps.Metro.Controls
 {
-    public class MetroTabControl : TabControl
+    public class MetroTabControl : BaseMetroTabControl
     {
-        public MetroTabControl()
+        public MetroTabControl():base()
         {
-            DefaultStyleKey = typeof(MetroTabControl);
+            DefaultStyleKey = typeof(MetroTabControl);         
+        }
+    }
+
+    public abstract class BaseMetroTabControl: TabControl
+    {
+        public BaseMetroTabControl()
+        {
+            InternalCloseTabCommand = new DefaultCloseTabCommand(this);
         }
 
         public Thickness TabStripMargin
@@ -19,11 +28,17 @@ namespace MahApps.Metro.Controls
 
         // Using a DependencyProperty as the backing store for TabStripMargin.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty TabStripMarginProperty =
-            DependencyProperty.Register("TabStripMargin", typeof(Thickness), typeof(MetroTabControl), new PropertyMetadata(new Thickness(0)));
+            DependencyProperty.Register("TabStripMargin", typeof(Thickness), typeof(BaseMetroTabControl), new PropertyMetadata(new Thickness(0)));
 
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new MetroTabItem() { OwningTabControl = this }; //Overrides the TabControl's default behavior and returns a MetroTabItem instead of a regular one.
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            element.SetCurrentValue(MetroTabItem.DataContextProperty, item);
+            base.PrepareContainerForItemOverride(element, item);
         }
 
         public ICommand CloseTabCommand
@@ -33,10 +48,25 @@ namespace MahApps.Metro.Controls
         }
 
         public static readonly DependencyProperty CloseTabCommandProperty =
-            DependencyProperty.Register("CloseTabCommand", typeof(ICommand), typeof(MetroTabControl), new PropertyMetadata(new DefaultCloseTabCommand()));
+            DependencyProperty.Register("CloseTabCommand", typeof(ICommand), typeof(BaseMetroTabControl), new PropertyMetadata(null));
+
+        private ICommand InternalCloseTabCommand
+        {
+            get { return (ICommand)GetValue(InternalCloseTabCommandProperty); }
+            set { SetValue(InternalCloseTabCommandProperty, value); }
+        }
+        private static readonly DependencyProperty InternalCloseTabCommandProperty =
+            DependencyProperty.Register("InternalCloseTabCommand", typeof(ICommand), typeof(BaseMetroTabControl), new PropertyMetadata(null));
+
 
         internal class DefaultCloseTabCommand : ICommand
         {
+            private BaseMetroTabControl owner = null;
+            internal DefaultCloseTabCommand(BaseMetroTabControl Owner)
+            {
+                owner = Owner;
+            }
+
             public bool CanExecute(object parameter)
             {
                 return true;
@@ -46,11 +76,18 @@ namespace MahApps.Metro.Controls
 
             public void Execute(object parameter)
             {
-                if (parameter != null && parameter is MetroTabItem)
+                if (parameter != null)
                 {
-                    var tabItem = (MetroTabItem)parameter;
-
-                    ((TabControl)tabItem.OwningTabControl).Items.Remove(tabItem);
+                    if (owner.CloseTabCommand != null)
+                        owner.CloseTabCommand.Execute(parameter);
+                    else
+                    {
+                        if (parameter is MetroTabItem)
+                        {
+                            var tabItem = (MetroTabItem)parameter;
+                            owner.Items.Remove(tabItem);
+                        }
+                    }
                 }
             }
         }
