594933cdaa57f837a05d20178193217a6e2a0a66
Amrykid <amrykid@gmail.com>
2013-06-22 11:07:10 -0500

    You can now disable the window transition animation and MetroContentControl's transitions in general.

--- MahApps.Metro/Controls/MetroContentControl.cs
+++ MahApps.Metro/Controls/MetroContentControl.cs
@@ -16,6 +16,13 @@ namespace MahApps.Metro.Controls
             set { SetValue(ReverseTransitionProperty, value); }
         }
 
+        public static readonly DependencyProperty TransitionsEnabledProperty = DependencyProperty.Register("TransitionsEnabled", typeof(bool), typeof(MetroContentControl), new FrameworkPropertyMetadata(true));
+
+        public bool TransitionsEnabled
+        {
+            get { return (bool)GetValue(TransitionsEnabledProperty); }
+            set { SetValue(TransitionsEnabledProperty, value); }
+        }
 
         public MetroContentControl()
         {
@@ -29,35 +36,48 @@ namespace MahApps.Metro.Controls
 
         void MetroContentControlIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (!IsVisible)
-                VisualStateManager.GoToState(this, ReverseTransition ? "AfterUnLoadedReverse" : "AfterUnLoaded", false);
-            else
-                VisualStateManager.GoToState(this, ReverseTransition ? "AfterLoadedReverse" : "AfterLoaded", true);
+            if (TransitionsEnabled)
+            {
+                if (!IsVisible)
+                    VisualStateManager.GoToState(this, ReverseTransition ? "AfterUnLoadedReverse" : "AfterUnLoaded", false);
+                else
+                    VisualStateManager.GoToState(this, ReverseTransition ? "AfterLoadedReverse" : "AfterLoaded", true);
+            }
         }
 
         private void MetroContentControlUnloaded(object sender, RoutedEventArgs e)
         {
-            VisualStateManager.GoToState(this, ReverseTransition ? "AfterUnLoadedReverse" : "AfterUnLoaded", false);
+            if (TransitionsEnabled)
+                VisualStateManager.GoToState(this, ReverseTransition ? "AfterUnLoadedReverse" : "AfterUnLoaded", false);
         }
 
         private void MetroContentControlLoaded(object sender, RoutedEventArgs e)
         {
-            VisualStateManager.GoToState(this, ReverseTransition ? "AfterLoadedReverse" : "AfterLoaded", true);
+            if (TransitionsEnabled)
+                VisualStateManager.GoToState(this, ReverseTransition ? "AfterLoadedReverse" : "AfterLoaded", true);
+            else
+            {
+                var root = ((Grid)GetTemplateChild("root"));
+                root.Opacity = 1.0;
+                ((System.Windows.Media.TranslateTransform)root.RenderTransform).X = 0;
+            }
         }
 
         public void Reload()
         {
+            if (!TransitionsEnabled) return;
+
             if (ReverseTransition)
             {
                 VisualStateManager.GoToState(this, "BeforeLoaded", true);
-                VisualStateManager.GoToState(this, "AfterUnLoadedReverse", true);            
+                VisualStateManager.GoToState(this, "AfterUnLoadedReverse", true);
             }
             else
             {
                 VisualStateManager.GoToState(this, "BeforeLoaded", true);
-                VisualStateManager.GoToState(this, "AfterLoaded", true);                
+                VisualStateManager.GoToState(this, "AfterLoaded", true);
             }
-            
+
         }
     }
 }
