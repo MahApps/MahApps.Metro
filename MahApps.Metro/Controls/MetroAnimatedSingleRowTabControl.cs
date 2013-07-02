ee56080aae6286fc116f7f9eccf9217d7ada5659
Andrea <apierini@maticad.it>
2013-07-02 16:11:18 +0200

    Fix tabs margin when scrollbar is collapsed

--- MahApps.Metro/Controls/MetroAnimatedSingleRowTabControl.cs
+++ MahApps.Metro/Controls/MetroAnimatedSingleRowTabControl.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
@@ -12,4 +14,29 @@ namespace MahApps.Metro.Controls
             DefaultStyleKey = typeof(MetroAnimatedSingleRowTabControl);
         }
     }
+
+    public class MetroAnimatedSingleRowTabControlVerticalScrollMargin : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value) == Visibility.Collapsed ? new Thickness(0, 2, 0, 2) : new Thickness(0, 15, 0, 15);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MetroAnimatedSingleRowTabControlHorizontalScrollMargin : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value) == Visibility.Collapsed ? new Thickness(2, 2, 2, 0) : new Thickness(15, 2, 15, 0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
