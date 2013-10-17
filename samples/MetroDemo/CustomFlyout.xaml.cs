cdcd33cfd4e5166c2cc1d26fc3bba5ce10ec2e29
punker76 <punkerat76@gmail.com>
2013-10-17 16:26:55 +0200

    enhance flyout demo

---
+++ samples/MetroDemo/CustomFlyout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MahApps.Metro.Controls;
+using MetroDemo.Models;
+
+namespace MetroDemo
+{
+    /// <summary>
+    /// Interaction logic for CustomFlyout.xaml
+    /// </summary>
+    public partial class CustomFlyout : Flyout
+    {
+        public List<Artist> Artists { get; set; }
+
+        public CustomFlyout()
+        {
+            Artists = SampleData.Artists;
+            InitializeComponent();
+        }
+    }
+}
