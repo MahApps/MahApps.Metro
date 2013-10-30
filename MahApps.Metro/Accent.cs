f7b896074d2c0ea4e627be185432cd0b1b436fda
Amrykid <amrykid@gmail.com>
2013-10-30 07:49:06 -0500

    Documented more of Accent.

--- MahApps.Metro/Accent.cs
+++ MahApps.Metro/Accent.cs
@@ -8,12 +8,26 @@ namespace MahApps.Metro
     /// </summary>
     public class Accent
     {
+        /// <summary>
+        /// The ResourceDictionary that represents this Accent.
+        /// </summary>
         public ResourceDictionary Resources;
+        /// <summary>
+        /// Gets/sets the name of the Accent.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Creates a new Accent object.
+        /// </summary>
         public Accent()
         { }
 
+        /// <summary>
+        /// Creates a new Accent object with a name and ResourceDictionary URI.
+        /// </summary>
+        /// <param name="name">The name of the new Accent.</param>
+        /// <param name="resourceAddress">The URI of the accent ResourceDictionary.</param>
         public Accent(string name, Uri resourceAddress)
         {
             Name = name;
