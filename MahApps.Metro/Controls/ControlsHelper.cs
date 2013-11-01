70b64251672836e94abd923e0a24391f72afd67d
punker76 <punkerat76@gmail.com>
2013-11-02 00:17:17 +0100

    Fix bug in MetroAnimatedSingleRowTabControl when tabs not on top

--- MahApps.Metro/Controls/ControlsHelper.cs
+++ MahApps.Metro/Controls/ControlsHelper.cs
@@ -92,5 +92,22 @@ namespace MahApps.Metro.Controls
         {
             obj.SetValue(ButtonWidthProperty, value);
         }
+
+        /// <summary>
+        /// This property can be used to set vertical scrollbar left side from the tabpanel (look at MetroAnimatedSingleRowTabControl)
+        /// </summary>
+        public static readonly DependencyProperty VerticalScrollBarOnLeftSideProperty =
+            DependencyProperty.RegisterAttached("VerticalScrollBarOnLeftSide", typeof(bool), typeof(ControlsHelper),
+                                                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.Inherits));
+
+        public static bool GetVerticalScrollBarOnLeftSide(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(VerticalScrollBarOnLeftSideProperty);
+        }
+
+        public static void SetVerticalScrollBarOnLeftSide(DependencyObject obj, bool value)
+        {
+            obj.SetValue(VerticalScrollBarOnLeftSideProperty, value);
+        }
     }
 }
