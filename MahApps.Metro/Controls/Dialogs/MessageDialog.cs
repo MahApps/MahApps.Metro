6dbe9425720f02a88e32def4af7c6eccb8871665
Amrykid <amrykid@gmail.com>
2013-11-07 18:18:24 -0600

    This thing won't work for the life of it.

--- MahApps.Metro/Controls/Dialogs/MessageDialog.cs
+++ MahApps.Metro/Controls/Dialogs/MessageDialog.cs
@@ -12,21 +12,22 @@ namespace MahApps.Metro.Controls.Dialogs
     /// <summary>
     /// An internal control that represents a message dialog. Please use MetroWindow.ShowMessage instead!
     /// </summary>
-    public class MessageDialog : BaseMetroDialog
+    public partial class MessageDialog : BaseMetroDialog
     {
-        private const string PART_AffirmativeButton = "PART_AffirmativeButton";
-        private const string PART_NegativeButton = "PART_NegativeButton";
+        //private const string PART_AffirmativeButton = "PART_AffirmativeButton";
+        //private const string PART_NegativeButton = "PART_NegativeButton";
 
         private Button AffirmativeButton = null;
         private Button NegativeButton = null;
 
-        static MessageDialog()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(MessageDialog), new FrameworkPropertyMetadata(typeof(MessageDialog)));
-        }
+        //static MessageDialog()
+        //{
+        //    //DefaultStyleKeyProperty.OverrideMetadata(typeof(MessageDialog), new FrameworkPropertyMetadata(typeof(MessageDialog)));
+        //}
         internal MessageDialog(MetroWindow parentWindow)
             : base(parentWindow)
         {
+            
         }
 
         internal Task<MessageDialogResult> WaitForButtonPressAsync()
@@ -105,42 +106,69 @@ namespace MahApps.Metro.Controls.Dialogs
             return tcs.Task;
         }
 
-        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(MessageDialog), new PropertyMetadata(default(string)));
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(MessageDialog), new PropertyMetadata(default(string)));
         public static readonly DependencyProperty AffirmativeButtonTextProperty = DependencyProperty.Register("AffirmativeButtonText", typeof(string), typeof(MessageDialog), new PropertyMetadata("OK"));
         public static readonly DependencyProperty NegativeButtonTextProperty = DependencyProperty.Register("NegativeButtonText", typeof(string), typeof(MessageDialog), new PropertyMetadata("Cancel"));
-        public static readonly DependencyProperty ButtonStyleProperty = DependencyProperty.Register("ButtonStyle", typeof(MessageDialogStyle), typeof(MessageDialog), new PropertyMetadata(MessageDialogStyle.Affirmative));
+        public static readonly DependencyProperty ButtonStyleProperty = DependencyProperty.Register("ButtonStyle", typeof(MessageDialogStyle), typeof(MessageDialog), new PropertyMetadata(MessageDialogStyle.Affirmative, new PropertyChangedCallback((s,e) =>
+            {
+                MessageDialog md = (MessageDialog)s;
+
+                SetButtonState(md);
+            })));
+
+        private static void SetButtonState(MessageDialog md)
+        {
+            if (md.AffirmativeButton == null) return;
+
+            switch (md.ButtonStyle)
+            {
+                case MessageDialogStyle.Affirmative:
+                    {
+                        md.AffirmativeButton.Visibility = Visibility.Visible;
+                        md.NegativeButton.Visibility = Visibility.Collapsed;
+                    }
+                    break;
+                case MessageDialogStyle.AffirmativeAndNegative:
+                    {
+                        md.AffirmativeButton.Visibility = Visibility.Visible;
+                        md.NegativeButton.Visibility = Visibility.Visible;
+                    }
+                    break;
+            }
+        }
+
+        private void Dialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            //AffirmativeButton = ((Grid)this.DialogBody).GetChildObjects().Where(x => x.GetValue(FrameworkElement.NameProperty) == PART_AffirmativeButton).First() as Button;
+
+            SetButtonState(this);
+        }
 
         public MessageDialogStyle ButtonStyle
         {
             get { return (MessageDialogStyle)GetValue(ButtonStyleProperty); }
             set { SetValue(ButtonStyleProperty, value); }
         }
-        public string Title
-        {
-            get { return (string)this.GetValue(TitleProperty); }
-            set { SetValue(TitleProperty, value); }
-        }
         public string Message
         {
-            get { return (string)this.GetValue(MessageProperty); }
+            get { return (string)GetValue(MessageProperty); }
             set { SetValue(MessageProperty, value); }
         }
         public string AffirmativeButtonText
         {
-            get { return (string)this.GetValue(AffirmativeButtonTextProperty); }
+            get { return (string)GetValue(AffirmativeButtonTextProperty); }
             set { SetValue(AffirmativeButtonTextProperty, value); }
         }
         public string NegativeButtonText
         {
-            get { return (string)this.GetValue(NegativeButtonTextProperty); }
+            get { return (string)GetValue(NegativeButtonTextProperty); }
             set { SetValue(NegativeButtonTextProperty, value); }
         }
 
         public override void OnApplyTemplate()
         {
-            AffirmativeButton = GetTemplateChild(PART_AffirmativeButton) as Button;
-            NegativeButton = GetTemplateChild(PART_NegativeButton) as Button;
+            //AffirmativeButton = GetTemplateChild(PART_AffirmativeButton) as Button;
+            //NegativeButton = GetTemplateChild(PART_NegativeButton) as Button;
 
             base.OnApplyTemplate();
         }
