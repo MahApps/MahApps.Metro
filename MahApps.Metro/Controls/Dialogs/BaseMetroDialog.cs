a57fc0e936e2e0bd50df1cefd36324a7a489fdf2
Amrykid <amrykid@gmail.com>
2013-11-07 18:18:24 -0600

    This thing won't work for the life of it.

--- MahApps.Metro/Controls/Dialogs/BaseMetroDialog.cs
+++ MahApps.Metro/Controls/Dialogs/BaseMetroDialog.cs
@@ -9,12 +9,30 @@ namespace MahApps.Metro.Controls.Dialogs
 {
     public abstract class BaseMetroDialog: Control
     {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(MessageDialog), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty DialogBodyProperty = DependencyProperty.Register("DialogBody", typeof(object), typeof(MessageDialog), new PropertyMetadata(null));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public object DialogBody
+        {
+            get { return GetValue(DialogBodyProperty); }
+            set { SetValue(DialogBodyProperty, value); }
+        }
+
         static BaseMetroDialog()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(BaseMetroDialog), new FrameworkPropertyMetadata(typeof(BaseMetroDialog)));
         }
 
         public BaseMetroDialog(MetroWindow owningWindow)
+        {
+        }
+        public BaseMetroDialog()
         {
         }
     }
