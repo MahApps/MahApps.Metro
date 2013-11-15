f34b5b515712ebff1997566d1eaf291b67613fde
Amrykid <amrykid@gmail.com>
2013-11-15 08:24:31 -0600

    Documentation and event implementation.

--- MahApps.Metro/Controls/MetroNavigationWindow.cs
+++ MahApps.Metro/Controls/MetroNavigationWindow.cs
@@ -27,9 +27,44 @@ namespace MahApps.Metro.Controls
         void MetroNavigationWindow_Loaded(object sender, RoutedEventArgs e)
         {
             PART_Frame.Navigated += PART_Frame_Navigated;
+            PART_Frame.Navigating += PART_Frame_Navigating;
+            PART_Frame.NavigationFailed += PART_Frame_NavigationFailed;
+            PART_Frame.NavigationProgress += PART_Frame_NavigationProgress;
+            PART_Frame.NavigationStopped += PART_Frame_NavigationStopped;
+            PART_Frame.LoadCompleted += PART_Frame_LoadCompleted;
             PART_BackButton.Click += PART_BackButton_Click;
         }
-
+        [System.Diagnostics.DebuggerNonUserCode]
+        void PART_Frame_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            if (LoadCompleted != null)
+                LoadCompleted(this, e);
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        void PART_Frame_NavigationStopped(object sender, NavigationEventArgs e)
+        {
+            if (NavigationStopped != null)
+                NavigationStopped(this, e);
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        void PART_Frame_NavigationProgress(object sender, NavigationProgressEventArgs e)
+        {
+            if (NavigationProgress != null)
+                NavigationProgress(this, e);
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        void PART_Frame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            if (NavigationFailed != null)
+                NavigationFailed(this, e);
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
+        void PART_Frame_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (Navigating != null)
+                Navigating(this, e);
+        }
+        [System.Diagnostics.DebuggerNonUserCode]
         void PART_BackButton_Click(object sender, RoutedEventArgs e)
         {
             if (CanGoBack)
@@ -38,18 +73,29 @@ namespace MahApps.Metro.Controls
 
         void MetroNavigationWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
+            PART_Frame.Navigating -= PART_Frame_Navigating;
+            PART_Frame.NavigationFailed -= PART_Frame_NavigationFailed;
+            PART_Frame.NavigationProgress -= PART_Frame_NavigationProgress;
+            PART_Frame.NavigationStopped -= PART_Frame_NavigationStopped;
+            PART_Frame.LoadCompleted -= PART_Frame_LoadCompleted;
             PART_BackButton.Click -= PART_BackButton_Click;
             this.Loaded -= MetroNavigationWindow_Loaded;
             PART_Frame.Navigated -= PART_Frame_Navigated;
             this.Closing -= MetroNavigationWindow_Closing;
         }
 
+        [System.Diagnostics.DebuggerNonUserCode]
         void PART_Frame_Navigated(object sender, NavigationEventArgs e)
         {
             PART_Title.Content = (PART_Frame.Content as Page).Title;
             (this as IUriContext).BaseUri = e.Uri;
 
+            PageContent = PART_Frame.Content;
+
             PART_BackButton.Visibility = CanGoBack ? Visibility.Visible : System.Windows.Visibility.Hidden;
+
+            if (Navigated != null)
+                Navigated(this, e);
         }
 
         public static readonly DependencyProperty OverlayContentProperty = DependencyProperty.Register("OverlayContent", typeof(object), typeof(MetroNavigationWindow));
@@ -58,6 +104,14 @@ namespace MahApps.Metro.Controls
         {
             get { return GetValue(OverlayContentProperty); }
             set { SetValue(OverlayContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty PageContentProperty = DependencyProperty.Register("PageContent", typeof(object), typeof(MetroNavigationWindow));
+
+        public object PageContent
+        {
+            get { return GetValue(PageContentProperty); }
+            private set { SetValue(PageContentProperty, value); }
         }
 
 
@@ -160,10 +214,55 @@ namespace MahApps.Metro.Controls
         /// <param name="extraData">A Object that contains data to be used for processing during navigation.</param>
         /// <returns>true if a navigation is not canceled; otherwise, false.</returns>
         /// <see cref="http://msdn.microsoft.com/en-us/library/ms591149(v=vs.110).aspx"/>
+        [System.Diagnostics.DebuggerNonUserCode]
         public bool Navigate(Uri source, Object extraData)
         {
             return PART_Frame.Navigate(source, extraData);
         }
+        /// <summary>
+        /// Stops further downloading of content for the current navigation request.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.stoploading(v=vs.110).aspx"/>
+        [System.Diagnostics.DebuggerNonUserCode]
+        public void StopLoading()
+        {
+            PART_Frame.StopLoading();
+        }
 
+        /// <summary>
+        /// Occurs when navigation to a content fragment begins, which occurs immediately, if the desired fragment is in the current content, or after the source XAML content has been loaded, if the desired fragment is in different content.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.fragmentnavigation(v=vs.110).aspx"/>
+        public event FragmentNavigationEventHandler FragmentNavigation;
+        /// <summary>
+        /// Occurs when a new navigation is requested.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.navigating(v=vs.110).aspx"/>
+        public event NavigatingCancelEventHandler Navigating;
+        /// <summary>
+        /// Occurs when an error is raised while navigating to the requested content.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.navigationfailed(v=vs.110).aspx"/>
+        public event NavigationFailedEventHandler NavigationFailed;
+        /// <summary>
+        /// Occurs periodically during a download to provide navigation progress information.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.navigationprogress(v=vs.110).aspx"/>
+        public event NavigationProgressEventHandler NavigationProgress;
+        /// <summary>
+        /// Occurs when the StopLoading method is called, or when a new navigation is requested while a current navigation is in progre
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.navigationstopped(v=vs.110).aspx"/>
+        public event NavigationStoppedEventHandler NavigationStopped;
+        /// <summary>
+        /// Occurs when the content that is being navigated to has been found, and is available from the PageContent property, although it may not have completed loading
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.navigated(v=vs.110).aspx"/>
+        public event NavigatedEventHandler Navigated;
+        /// <summary>
+        /// Occurs when content that was navigated to has been loaded, parsed, and has begun rendering.
+        /// </summary>
+        /// <see cref="http://msdn.microsoft.com/en-us/library/system.windows.navigation.navigationwindow.loadcompleted(v=vs.110).aspx"/>
+        public event LoadCompletedEventHandler LoadCompleted;
     }
 }
