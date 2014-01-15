39678152f606f3782803411380e5e4fbad2a8142
Dennis Daume <daume.dennis@gmail.com>
2014-01-15 19:10:27 +0100

    Goodbye, Panorama

--- samples/MetroDemo/MainWindow.xaml.cs
+++ samples/MetroDemo/MainWindow.xaml.cs
@@ -62,11 +62,6 @@ namespace MetroDemo
                 flyoutDemo.Show();
         }
 
-        private void LaunchPanoramaDemo(object sender, RoutedEventArgs e)
-        {
-            new PanoramaDemo().Show();
-        }
-
         private void LaunchIcons(object sender, RoutedEventArgs e)
         {
             new IconsWindow().Show();
