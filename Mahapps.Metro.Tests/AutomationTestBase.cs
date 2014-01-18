295364770f6fddbc47cf6829144e52125cf0cf85
Dennis Daume <daume.dennis@gmail.com>
2014-01-18 16:46:53 +0100

    Create the base for our tests

---
+++ Mahapps.Metro.Tests/AutomationTestBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mahapps.Metro.Tests
+{
+    public class AutomationTestBase : IDisposable
+    {
+        public AutomationTestBase()
+        {
+            TestHost.Start();
+        }
+
+        public void Dispose()
+        {
+            TestHost.Shutdown();
+        }
+    }
+}
---
+++ Mahapps.Metro.Tests/MetroWindowTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls;
+using Xunit;
+
+namespace Mahapps.Metro.Tests
+{
+    public class MetroWindowTest : AutomationTestBase
+    {
+        [Fact]
+        public async Task MetroWindowSmokeTest()
+        {
+            await TestHost.SwitchToAppThread();
+
+            Window window = new MetroWindow();
+        }
+    }
+}
