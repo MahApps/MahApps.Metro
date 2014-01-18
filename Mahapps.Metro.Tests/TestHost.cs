26c5b644bc5fc7dfb253cbb90076e10faf6423bc
Dennis Daume <daume.dennis@gmail.com>
2014-01-18 16:46:53 +0100

    Create the base for our tests

---
+++ Mahapps.Metro.Tests/TestHost.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Mahapps.Metro.Tests
+{
+    public static class TestHost
+    {
+        private static Application app;
+        private static Thread appThread;
+        private static AutoResetEvent gate = new AutoResetEvent(false);
+
+        public static void Start()
+        {
+            appThread = new Thread(StartDispatcher);
+            appThread.SetApartmentState(ApartmentState.STA);
+            appThread.Start();
+
+            gate.WaitOne();
+        }
+
+        public static void StartDispatcher()
+        {
+            app = new Application { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+            app.Startup += (sender, args) => gate.Set();
+            app.Run();
+        }
+
+        public static SwitchContextToUiThreadAwaiter SwitchToAppThread()
+        {
+            return new SwitchContextToUiThreadAwaiter(app.Dispatcher);
+        }
+
+        public static void Shutdown()
+        {
+            app.Dispatcher.BeginInvokeShutdown(DispatcherPriority.Send);
+            appThread.Join();
+        }
+    }
+}
