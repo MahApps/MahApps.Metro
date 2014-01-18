ba3212c1e5887bcbf7062b2a6cb8a8292ab31935
Dennis Daume <daume.dennis@gmail.com>
2014-01-19 00:59:28 +0100

    ´That shouldn't be there

--- Mahapps.Metro.Tests/TestHost.cs
+++ Mahapps.Metro.Tests/TestHost.cs
@@ -24,8 +24,6 @@ namespace Mahapps.Metro.Tests
         {
             app = new TestApp { ShutdownMode = ShutdownMode.OnExplicitShutdown };
             app.Startup += (sender, args) => gate.Set();
-           var appdomain =  AppDomain.CreateDomain("TestDomain");
-           var apppp = Application.Current;
             app.Run();
         }
 
