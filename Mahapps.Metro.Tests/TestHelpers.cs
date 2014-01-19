b73a7c1d0be9add95d8f90bc31dc00f7f4b0c261
Dennis Daume <daume.dennis@gmail.com>
2014-01-19 03:24:39 +0100

    Simplify the test helper

--- Mahapps.Metro.Tests/TestHelpers.cs
+++ Mahapps.Metro.Tests/TestHelpers.cs
@@ -1,45 +1,34 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Mahapps.Metro.Tests
 {
     public static class TestHelpers
     {
-        public static Task AwaitLoaded(this Window window)
+        public static Task<T> CreateInvisibleWindowAsync<T>() where T : Window, new()
         {
-            var completionSource = new TaskCompletionSource<object>();
-
-            RoutedEventHandler handler = null;
-
-            handler = (sender, args) =>
-            {
-                completionSource.TrySetResult(null);
-                window.Loaded -= handler;
-            };
-
-            window.Loaded += handler;
-            
-            if (window.IsLoaded)
-            {
-                completionSource.TrySetResult(null);
-            }
-
-            return completionSource.Task;
-        }
-
-        public static async Task<T> CreateInvisibleWindowAsync<T>() where T : Window, new()
-        {
-            var wnd = new T
+            var window = new T
             {
                 Visibility = Visibility.Hidden, 
                 ShowInTaskbar = false
             };
 
-            wnd.Show();
+            var completionSource = new TaskCompletionSource<T>();
 
-            await wnd.AwaitLoaded();
+            EventHandler handler = null;
 
-            return wnd;
+            handler = (sender, args) =>
+            {
+                window.Activated -= handler;
+                completionSource.SetResult(window);
+            };
+
+            window.Activated += handler;
+            
+            window.Show();
+
+            return completionSource.Task;
         }
     }
 }
