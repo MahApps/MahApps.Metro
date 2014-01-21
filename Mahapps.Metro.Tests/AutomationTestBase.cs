155d33e4e5ffd80762f93c5e9de728270224839b
Dennis Daume <daume.dennis@gmail.com>
2014-01-21 21:15:20 +0100

    Create the ultimate hack so we can "Run All" tests

--- Mahapps.Metro.Tests/AutomationTestBase.cs
+++ Mahapps.Metro.Tests/AutomationTestBase.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using System.Linq;
+using System.Windows;
+using MahApps.Metro;
 
 namespace Mahapps.Metro.Tests
 {
     /// <summary>
     /// This is the base class for all of our UI tests.
     /// </summary>
-    public class AutomationTestBase : IDisposable
+    public class AutomationTestBase
     {
         public AutomationTestBase()
         {
-            TestHost.Start();
-        }
+            TestHost.Initialize();
 
-        public void Dispose()
-        {
-            TestHost.Shutdown();
+            // Reset the application as good as we can
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                foreach (Window window in Application.Current.Windows)
+                {
+                    window.Close();
+                }
+            }));
+
+            ThemeManager.ChangeTheme(Application.Current, ThemeManager.DefaultAccents.First(x => x.Name == "Blue"), Theme.Light);
         }
     }
 }
---
+++ Mahapps.Metro.Tests/CleanWindowTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using MahApps.Metro;
+using Xunit;
+
+namespace Mahapps.Metro.Tests
+{
+    public class CleanWindowTest : AutomationTestBase
+    {
+        [Fact]
+        public async Task DefaultWindowCommandColorIsBlack()
+        {
+            await TestHost.SwitchToAppThread();
+
+            var window = await TestHelpers.CreateInvisibleWindowAsync<CleanWindow>();
+
+            Assert.Equal((SolidColorBrush)ThemeManager.LightResource["BlackBrush"], (SolidColorBrush) window.WindowCommands.Foreground);
+        }
+    }
+}
