12481d01b96eaf5796b39c40802776e911b636ed
Jan Karger <punkerat76@gmail.com>
2014-02-27 15:04:10 +0100

    introduce ContentDirection attached property

--- MahApps.Metro/Controls/ControlsHelper.cs
+++ MahApps.Metro/Controls/ControlsHelper.cs
@@ -319,5 +319,39 @@ namespace MahApps.Metro.Controls
         {
             obj.SetValue(TransitionProperty, value);
         }
+
+        /// <summary>
+        /// This property can be used to handle the style for CheckBox and RadioButton
+        /// LeftToRight means content left and button right and RightToLeft vise versa
+        /// </summary>
+        public static readonly DependencyProperty ContentDirectionProperty =
+            DependencyProperty.RegisterAttached("ContentDirection", typeof(FlowDirection), typeof(ControlsHelper),
+                                                new FrameworkPropertyMetadata(FlowDirection.LeftToRight,
+                                                                              //FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.Inherits,
+                                                                              ContentDirectionPropertyChanged));
+
+        /// <summary>
+        /// This property can be used to handle the style for CheckBox and RadioButton
+        /// LeftToRight means content left and button right and RightToLeft vise versa
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(ToggleButton))]
+        public static FlowDirection GetContentDirection(UIElement element)
+        {
+            return (FlowDirection)element.GetValue(ContentDirectionProperty);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(ToggleButton))]
+        public static void SetContentDirection(UIElement element, FlowDirection value)
+        {
+            element.SetValue(ContentDirectionProperty, value);
+        }
+
+        private static void ContentDirectionPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var tb = d as ToggleButton;
+            if (null == tb) {
+                throw new InvalidOperationException("The property 'ContentDirection' may only be set on ToggleButton elements.");
+            }
+        }
     }
 }
