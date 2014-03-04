1be5ca4a4fbd6d1ba8e15a908488d1f6c57d3460
100GPing100 <zeluis.100@gmail.com>
2014-03-04 17:18:42 +0000

    Fix #1124
    ShowIconOnTitleBar=true + TitlebarHeight=0
    Click anywhere on window and it closes.

--- MahApps.Metro/Controls/MetroWindow.cs
+++ MahApps.Metro/Controls/MetroWindow.cs
@@ -499,6 +499,10 @@ namespace MahApps.Metro.Controls
 
         protected void TitleBarMouseDown(object sender, MouseButtonEventArgs e)
         {
+            if (this.TitlebarHeight <= 0) {
+                return;
+            }
+            
             var mousePosition = e.GetPosition(this);
             bool isIconClick = ShowIconOnTitleBar && mousePosition.X <= TitlebarHeight && mousePosition.Y <= TitlebarHeight;
 
