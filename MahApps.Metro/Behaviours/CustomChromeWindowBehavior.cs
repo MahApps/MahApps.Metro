154d166468739f718928a7e07eb0bd60603e6c04
Jan Karger <punkerat76@gmail.com>
2014-03-14 10:33:34 +0100

    introduce CustomChromeWindowBehavior

---
+++ MahApps.Metro/Behaviours/CustomChromeWindowBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+#if NET_4
+using Microsoft.Windows.Shell;
+#else
+using System.Windows.Shell;
+#endif
+
+namespace MahApps.Metro.Behaviours
+{
+    public class CustomChromeWindowBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            var windowChrome = new WindowChrome();
+            windowChrome.ResizeBorderThickness = new Thickness(6);
+            windowChrome.CaptionHeight = 30;
+            windowChrome.CornerRadius = new CornerRadius(0);
+            windowChrome.GlassFrameThickness = new Thickness(0);
+
+            AssociatedObject.SetValue(WindowChrome.WindowChromeProperty, windowChrome);
+        }
+    }
+}
