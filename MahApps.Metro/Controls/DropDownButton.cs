12f30c03f0ab37faad5d474bce14f1b0c617300b
Денис Запорожец <mderkins@gmail.com>
2014-03-14 23:45:08 +0200

    Introduce DropDownButton

---
+++ MahApps.Metro/Controls/DropDownButton.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    [ContentProperty("ItemsSource")]
+    [TemplatePart(Name = "PART_Container", Type = typeof(Grid)),
+    TemplatePart(Name = "PART_Button", Type = typeof(Button)),
+    TemplatePart(Name = "PART_Image", Type = typeof(Image)),
+    TemplatePart(Name = "PART_ButtonContent", Type = typeof(ContentControl)),
+    TemplatePart(Name = "PART_Menu", Type = typeof(ContextMenu)),
+    TemplatePart(Name = "PART_Expander", Type = typeof(Button))]
+    public class DropDownButton : Control
+    {
+
+        #region Events
+
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler), typeof(DropDownButton));
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        #endregion
+
+
+        #region DependencyProperties
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<object>), typeof(DropDownButton));
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(DropDownButton),
+                new UIPropertyMetadata(null));
+
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(DropDownButton));
+
+        public static readonly DependencyProperty ExtraTagProperty =
+            DependencyProperty.Register("ExtraTag", typeof(Object), typeof(DropDownButton));
+
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(DropDownButton),
+                new FrameworkPropertyMetadata(Orientation.Horizontal));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(ImageSource), typeof(DropDownButton));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(DropDownButton));
+
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(DropDownButton));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(Object), typeof(DropDownButton));
+
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(Object), typeof(DropDownButton));
+
+
+        public Object Content
+        {
+            get { return (Object)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        public Object CommandParameter
+        {
+            get { return (Object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public ObservableCollection<object> ItemsSource
+        {
+            get { return (ObservableCollection<object>)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public Boolean IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+
+        public Object ExtraTag
+        {
+            get { return GetValue(ExtraTagProperty); }
+            set { SetValue(ExtraTagProperty, value); }
+        }
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        public ImageSource Icon
+        {
+            get { return (ImageSource)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        #endregion
+
+
+        #region Variables
+
+        private Button _clickButton;
+        private Button _expander;
+        private ContextMenu _menu;
+
+        #endregion
+
+
+        public DropDownButton()
+        {
+            ItemsSource = new ObservableCollection<Object>();
+        }
+        static DropDownButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropDownButton), new FrameworkPropertyMetadata(typeof(DropDownButton)));
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            e.RoutedEvent = ClickEvent;
+            RaiseEvent(e);
+        }
+
+        private void ExpanderClick(object sender, RoutedEventArgs e)
+        {
+            _menu.Placement = PlacementMode.Bottom;
+            if (Orientation == Orientation.Horizontal)
+            {
+                _menu.PlacementTarget = _clickButton;
+            }
+            else
+            {
+                _menu.PlacementTarget = _expander;
+            }
+            _menu.IsOpen = true;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _clickButton = EnforceInstance<Button>("PART_Button");
+            _expander = EnforceInstance<Button>("PART_Expander");
+            _menu = EnforceInstance<ContextMenu>("PART_Menu");
+            InitializeVisualElementsContainer();
+        }
+
+        //Get element from name. If it exist then element instance return, if not, new will be created
+        T EnforceInstance<T>(string partName) where T : FrameworkElement, new()
+        {
+            T element = GetTemplateChild(partName) as T ?? new T();
+            return element;
+        }
+
+        private void InitializeVisualElementsContainer()
+        {
+            MouseRightButtonUp += DropDownButton_MouseRightButtonUp;
+            _expander.Click += ExpanderClick;
+            _clickButton.Click += ButtonClick;
+        }
+
+        void DropDownButton_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _menu.Visibility = Visibility.Visible;
+            e.Handled = true;
+        }
+
+    }
+}
