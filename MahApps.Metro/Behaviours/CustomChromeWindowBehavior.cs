04f3ad7e106e8f33a18170b56f315145b26db494
Jan Karger <punkerat76@gmail.com>
2014-03-14 11:01:14 +0100

    more functionallity
    - remove BorderlessWindowBehavior usage
    - remove MetroContentControl, because animating the window content gives
    us nasty black flickering
    - IsHitTestVisibleInChromeProperty for title bar objects

--- MahApps.Metro/Behaviours/CustomChromeWindowBehavior.cs
+++ MahApps.Metro/Behaviours/CustomChromeWindowBehavior.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interactivity;
+using MahApps.Metro.Controls;
 #if NET_4
 using Microsoft.Windows.Shell;
 #else
@@ -19,13 +21,57 @@ namespace MahApps.Metro.Behaviours
         {
             base.OnAttached();
 
+            var window = AssociatedObject as MetroWindow;
+            var captionHeight = window != null ? window.TitlebarHeight : 30;
+
             var windowChrome = new WindowChrome();
             windowChrome.ResizeBorderThickness = new Thickness(6);
-            windowChrome.CaptionHeight = 30;
+            windowChrome.CaptionHeight = captionHeight;
             windowChrome.CornerRadius = new CornerRadius(0);
             windowChrome.GlassFrameThickness = new Thickness(0);
 
             AssociatedObject.SetValue(WindowChrome.WindowChromeProperty, windowChrome);
+
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = sender as MetroWindow;
+            if (window == null)
+            {
+                return;
+            }
+
+            var icon = window.GetPart<UIElement>("PART_Icon");
+            if (icon != null)
+            {
+                icon.SetValue(WindowChrome.IsHitTestVisibleInChromeProperty, true);
+            }
+            
+            var titleBar = window.GetPart<UIElement>("PART_TitleBar");
+            if (titleBar != null)
+            {
+                titleBar.SetValue(WindowChrome.IsHitTestVisibleInChromeProperty, true);
+            }
+            
+            var leftWindowCommands = window.GetPart<ContentPresenter>("PART_LeftWindowCommands");
+            if (leftWindowCommands != null)
+            {
+                leftWindowCommands.SetValue(WindowChrome.IsHitTestVisibleInChromeProperty, true);
+            }
+            
+            var windowCommands = window.GetPart<ContentPresenter>("PART_RightWindowCommands");
+            if (windowCommands != null)
+            {
+                windowCommands.SetValue(WindowChrome.IsHitTestVisibleInChromeProperty, true);
+            }
+            
+            var windowButtonCommands = window.GetPart<ContentControl>("PART_WindowButtonCommands");
+            if (windowButtonCommands != null)
+            {
+                windowButtonCommands.SetValue(WindowChrome.IsHitTestVisibleInChromeProperty, true);
+            }
         }
     }
 }
