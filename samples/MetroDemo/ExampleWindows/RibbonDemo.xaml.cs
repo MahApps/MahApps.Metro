b5dec42982f2d904cad0460c3d9264f68493c9e0
Tide <thecrimsontidefan@gmail.com>
2014-03-14 23:22:30 -0400

    Added code to change the theme and accent on the ribbon demo. Commented out all templates in styles.xaml for now. Added more controls to the ribbon demo. Set the tab scroll buttons to hidden for now.

--- samples/MetroDemo/ExampleWindows/RibbonDemo.xaml.cs
+++ samples/MetroDemo/ExampleWindows/RibbonDemo.xaml.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
+using MahApps.Metro;
 using MahApps.Metro.Controls;
 
 namespace MetroDemo.ExampleWindows
@@ -22,7 +25,40 @@ namespace MetroDemo.ExampleWindows
     {
         public RibbonDemo()
         {
+            var vm = new MainWindowViewModel();
+            DataContext = vm;
             InitializeComponent();
+
+            var theme = ThemeManager.DetectTheme(Application.Current);
+            var accent = ThemeManager.DefaultAccents.First(x => x.Name == theme.Item2.Name);
+
+            foreach (var accentColorItem in vm.AccentColors.Where(accentColorItem => accentColorItem.Name == accent.Name))
+            {
+                this.accentsComboBox.SelectedItem = accentColorItem;
+            }
+        }
+        
+        private void DarkTheme(object sender, RoutedEventArgs e)
+        {
+            var theme = ThemeManager.DetectTheme(Application.Current);
+            ThemeManager.ChangeTheme(Application.Current, theme.Item2, Theme.Dark);
+        }
+
+        private void LightTheme(object sender, RoutedEventArgs e)
+        {
+            var theme = ThemeManager.DetectTheme(Application.Current);
+            ThemeManager.ChangeTheme(Application.Current, theme.Item2, Theme.Light);
+        }
+
+        private void Accents_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            RibbonGallery source = e.OriginalSource as RibbonGallery;
+            if (source == null) return;
+            var accentItem = (AccentColorMenuData)source.SelectedItem;
+
+            var theme = ThemeManager.DetectTheme(Application.Current);
+            var accent = ThemeManager.DefaultAccents.First(x => x.Name == accentItem.Name);
+            ThemeManager.ChangeTheme(Application.Current, accent, theme.Item1);
         }
     }
 }
