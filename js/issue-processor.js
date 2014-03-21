86199509c1d7a090dcb831cd8f1b1180fd106605
Dennis Daume <daume.dennis@gmail.com>
2014-03-21 13:32:06 +0000

    Remove console output

--- js/issue-processor.js
+++ js/issue-processor.js
@@ -1,7 +1,5 @@
 $(function() {
-
   $(".post").each(function(element) {
-    console.log($(this).html());
     var replaced = $(this).html().replace(/(#([0-9]+))/g, '<a href="https://github.com/MahApps/MahApps.Metro/issues/$2">$1</a>');
     $(this).html(replaced);
   });
