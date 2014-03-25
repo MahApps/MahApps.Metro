b62db2e20835ba537c132660418cfec0058677af
Jan Karger <punkerat76@gmail.com>
2014-03-25 15:05:58 +0100

    i forgot that issue from the old behavior

--- MahApps.Metro/Behaviours/BorderlessWindowBehavior.cs
+++ MahApps.Metro/Behaviours/BorderlessWindowBehavior.cs
@@ -40,7 +40,17 @@ namespace MahApps.Metro.Behaviours
             AssociatedObject.SetValue(WindowChrome.WindowChromeProperty, windowChrome);
 
             // no transparany, because it hase more then one unwanted issues
-            AssociatedObject.AllowsTransparency = false;
+            if (!AssociatedObject.IsLoaded)
+            {
+                try
+                {
+                    AssociatedObject.AllowsTransparency = false;
+                }
+                catch (Exception)
+                {
+                    //For some reason, we can't determine if the window has loaded or not, so we swallow the exception.
+                }
+            }
             AssociatedObject.WindowStyle = WindowStyle.None;
             savedBorderThickness = AssociatedObject.BorderThickness;
 
