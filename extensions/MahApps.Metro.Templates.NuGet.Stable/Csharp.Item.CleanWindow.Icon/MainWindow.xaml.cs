06f29a3c590095c6e9430d8769640296b99f77e5
Tide <thecrimsontidefan@gmail.com>
2014-04-05 23:03:59 -0400

    Added Vsix  projects to install MahApps.Metro project and item templates.  There are two versions.  The first version does not install any references and is for those who compile MahApps.Metro from source.  The second version uses the last stable NuGet release of MahApps.  The references are automatically installed via NuGet.

---
+++ extensions/MahApps.Metro.Templates.NuGet.Stable/Csharp.Item.CleanWindow.Icon/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// Interaction logic for $safeitemrootname$.xaml
+    /// </summary>
+    public partial class $safeitemrootname$ : MetroWindow
+    {
+        public $safeitemrootname$()
+        {
+            InitializeComponent();
+        }
+    }
+}
