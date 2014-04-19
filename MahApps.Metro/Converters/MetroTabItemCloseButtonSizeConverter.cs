a894b6dd1c6bd56886e860f2d3d07fdbf042664b
oloopy <oloopy@gmail.com>
2014-04-19 17:26:12 +0800

    The size of CloseButton presented in MetroTabItem will be changed proportionately considering its HeaderFontSize. In addition, an example has been added into MetroDemo project.

---
+++ MahApps.Metro/Converters/MetroTabItemCloseButtonSizeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MahApps.Metro.Converters
+{
+    public class MetroTabItemCloseButtonFontSizeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) * 0.7 * 0.85;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) / 0.7 / 0.85;
+        }
+    }
+
+    public class MetroTabItemCloseButtonWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) * 0.7;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) / 0.7;
+        }
+    }
+}
