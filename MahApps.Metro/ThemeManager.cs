17f10ef3afee9e65b8c4be051f7c2d8ee0cd5e66
Geert van Horrik <geert@catenalogic.com>
2014-05-30 12:38:54 +0200

    Fix for Dialogs crash when app style is set via ThemeManager #1358

--- MahApps.Metro/ThemeManager.cs
+++ MahApps.Metro/ThemeManager.cs
@@ -197,8 +197,63 @@ namespace MahApps.Metro
         {
             if (resources == null) throw new ArgumentNullException("resources");
 
-            return Accents.FirstOrDefault(x => x.Resources.Source == resources.Source);
+            var builtInAccent = Accents.FirstOrDefault(x => x.Resources.Source == resources.Source);
+            if (builtInAccent != null)
+            {
+                return builtInAccent;
+            }
+
+            // support dynamically created runtime resource dictionaries
+            if (IsAccentDictionary(resources))
+            {
+                return new Accent
+                {
+                    Name = "Runtime accent",
+                    Resources = resources
+                };
+            }
+
+            return null;
         }
+
+        /// <summary>
+        /// Determines whether the specified resource dictionary represents an <see cref="Accent"/>.
+        /// <para />
+        /// This might include runtime accents which do not have a resource uri.
+        /// </summary>
+        /// <param name="resources">The resources.</param>
+        /// <returns><c>true</c> if the resource dictionary is an <see cref="Accent"/>; otherwise, <c>false</c>.</returns>
+        /// <exception cref="System.ArgumentNullException">resources</exception>
+        public static bool IsAccentDictionary(ResourceDictionary resources)
+        {
+            if (resources == null) throw new ArgumentNullException("resources");
+
+            // Note: add more checks if these keys aren't sufficient
+            var styleKeys = new List<string>(new []
+            {
+                "HighlightColor",
+                "AccentColor",
+                "AccentColor2",
+                "AccentColor3",
+                "AccentColor4",
+                "HighlightBrush",
+                "AccentColorBrush",
+                "AccentColorBrush2",
+                "AccentColorBrush3",
+                "AccentColorBrush4",
+            });
+
+            foreach (var styleKey in styleKeys)
+            {
+                if (!resources.Contains(styleKey))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         /// <summary>
         /// Gets a resource from the detected AppStyle.
         /// </summary>
