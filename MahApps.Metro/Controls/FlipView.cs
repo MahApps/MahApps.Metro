9bc38a2ebfff6b471d5dd5248e919323ceb19186
Bruno Juchli <bruno.juchli@gmail.com>
2014-10-17 13:05:06 +0200

    removed superfluous deregistration of local hander of local event. After the finalizer is executed neither will the event occur ever again nor can the handler hinder garbage collection. Furthermore the usage of the Dispatcher can proof dangerous if used when doing AppDomain.Unload (possible deadlock?!)

--- MahApps.Metro/Controls/FlipView.cs
+++ MahApps.Metro/Controls/FlipView.cs
@@ -1,18 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MahApps.Metro.Controls
 {
@@ -72,14 +63,7 @@ namespace MahApps.Metro.Controls
         {
             this.Focus();
         }
-        ~FlipView()
-        {
-            Dispatcher.Invoke(new EmptyDelegate(() =>
-            {
-                this.Loaded -= FlipView_Loaded;
-            }));
-        }
-
+        
         private delegate void EmptyDelegate();
 
         protected override bool IsItemItsOwnContainerOverride(object item)
