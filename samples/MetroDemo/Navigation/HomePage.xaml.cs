2e4c00366731dace813185e1500b94d25c150ac6
Aybe <aybe.one@hotmail.com>
2014-10-23 18:45:09 +0200

    'Metro' style for 'Frame' control (part 2)
    Provides OnNavigated() helper, needs a fix for being merged in Metro
    assembly.
    
    Explanations: I have tried to move the methods inside MetroWindowHelpers
    and use #if for exclude async stuff from .NET 4.0. While both Metro
    assemblies build fine, demo projects never succeed. So for the time
    being I have left them in the demo page unless someone can fix it.

--- samples/MetroDemo/Navigation/HomePage.xaml.cs
+++ samples/MetroDemo/Navigation/HomePage.xaml.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace MetroDemo.Navigation
 {
@@ -22,6 +16,91 @@ namespace MetroDemo.Navigation
         public HomePage()
         {
             InitializeComponent();
+
+
+            // Example of a synchronous operation when OnNavigated to here.
+            OnNavigated(this, e =>
+            {
+                // Print some text on the console
+                string format = string.Format("MetroWindow.OnNavigated example, extra data received: {0}",
+                    e.ExtraData);
+                Console.WriteLine(format);
+            });
+
+#if NET_4_5
+            // Example of an asynchronous operation when OnNavigated to here.
+            OnNavigated(this, async e =>
+            {
+                // Show a message dialog to the user
+                var frame = (Frame) e.Navigator;
+                var window = frame.TryFindParent<MetroWindow>();
+                if (window != null)
+                {
+                    string message = String.Format("Extra data received: {0}", e.ExtraData);
+                    await window.ShowMessageAsync("MetroWindow.OnNavigated async example", message);
+                }
+            });
+#endif
         }
+
+
+        /// <summary>
+        ///     Gets if the specified object is being navigated to.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private static bool IsNavigatedTo(object o, NavigationEventArgs e)
+        {
+            if (o == null) throw new ArgumentNullException("o");
+            if (e == null) throw new ArgumentNullException("e");
+            return e.Content != null && Equals(e.Content, o);
+        }
+
+        /// <summary>
+        ///     Provides an oppurtunity to execute an action when a content is being navigated to.
+        /// </summary>
+        /// <param name="content">Content object being navigated to.</param>
+        /// <param name="action">Action to execute.</param>
+        public static void OnNavigated(object content, Action<NavigationEventArgs> action)
+        {
+            if (content == null) throw new ArgumentNullException("content");
+            if (action == null) throw new ArgumentNullException("action");
+            NavigatedEventHandler navigated = null;
+            navigated = (sender, e) =>
+            {
+                if (IsNavigatedTo(content, e))
+                {
+                    action(e);
+                }
+
+                Application.Current.Navigated -= navigated;
+            };
+            Application.Current.Navigated += navigated;
+        }
+
+#if NET_4_5
+        /// <summary>
+        ///     Provides an oppurtunity to execute an asynchronous action when a content is being navigated to.
+        /// </summary>
+        /// <param name="content">Content object being navigated to.</param>
+        /// <param name="function">Asynchronous action to execute.</param>
+        public static void OnNavigated(object content, Func<NavigationEventArgs, Task> function)
+        {
+            if (content == null) throw new ArgumentNullException("content");
+            if (function == null) throw new ArgumentNullException("function");
+            NavigatedEventHandler navigated = null;
+            navigated = async (sender, e) =>
+            {
+                if (IsNavigatedTo(content, e))
+                {
+                    await function(e);
+                }
+
+                Application.Current.Navigated -= navigated;
+            };
+            Application.Current.Navigated += navigated;
+        }
+#endif
     }
 }
