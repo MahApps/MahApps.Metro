f50ccaa1cc3ab9c365ad02cf31c2cbdb6b461f39
Aybe <aybe.one@hotmail.com>
2014-10-23 16:31:12 +0200

    'Metro' style for 'Frame' control
    Styles the 'Frame' control to look like 'MetroNavigationWindow'.
    
    This is part 1 of 2, the second part will provide a helper to receive
    navigation events within a 'MetroWindow' just as it is the case for a
    'MetroNavigationWindow'.

---
+++ samples/MetroDemo/Navigation/NavigationFrameWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MetroDemo.Navigation
+{
+    public partial class NavigationFrameWindow : MetroWindow
+    {
+        public NavigationFrameWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void NavigateTo(object o)
+        {
+            Frame1.Navigate(o);
+        }
+    }
+}
