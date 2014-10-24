b17f5eedd6946ffeca52781c20cef63f70acb080
Charles Weld <charles.weld@gmail.com>
2014-10-24 16:43:33 +1100

    Fixed popup event handling bug
    Previously the owning window's PreviewMouseDown event, used to auto close the popover, was being subscribed to
    on the popover's load event and unsubscribed on unload however this didn't take into account that the Load could be called multiple
    times. The popover now keeps track of if it's subscribe to the given event using a disposable handler, which is also used
    to unsubscribe.

--- MahApps.Metro/Controls/MetroPopover.cs
+++ MahApps.Metro/Controls/MetroPopover.cs
@@ -20,6 +20,39 @@ namespace MahApps.Metro.Controls
     [ContentProperty("Content")]
     public class MetroPopover : FrameworkElement
     {
+        #region Disposable event handler
+
+        static class Disposable
+        {
+            public static IDisposable Subscription<TEventHandler>(Action<TEventHandler> subscribe, Action<TEventHandler> unsubscribe, TEventHandler handler)
+            {
+                subscribe(handler);
+                return new DisposableSubscription<TEventHandler>(unsubscribe, handler);
+            }
+        }
+
+        /// <summary>
+        /// Unsubscribes the given event handler on disposal.
+        /// </summary>
+        class DisposableSubscription<TEventHandler> : IDisposable
+        {
+            readonly Action<TEventHandler> description;
+            readonly TEventHandler handler;
+
+            public DisposableSubscription(Action<TEventHandler> unsubscribe, TEventHandler handler)
+            {
+                this.description = unsubscribe;
+                this.handler = handler;
+            }
+
+            public void Dispose()
+            {
+                description(handler);
+            }
+        }
+
+        #endregion
+
         #region Popover adorner
 
         // see tech.pro/tutorial/856/wpf-tutorial-using-a-visual-collection by Michael Kuehl
@@ -170,6 +203,7 @@ namespace MahApps.Metro.Controls
         #endregion
 
         PopoverAdorner _adorner;
+        IDisposable _previewOwningWindowClickSubscription;
 
         static MetroPopover()
         {
@@ -187,35 +221,53 @@ namespace MahApps.Metro.Controls
         public static readonly DependencyProperty TargetProperty = DependencyProperty.Register("Target", typeof(UIElement), typeof(MetroPopover), new PropertyMetadata(null, OnTargetChanged));
         public static readonly DependencyProperty AutoCloseProperty = DependencyProperty.Register("AutoClose", typeof(bool), typeof(MetroPopover), new PropertyMetadata(true));
 
+        /// <summary>
+        /// The element to place the popover under. Note horizontal placement is controlled via the <see cref="HorizontalAlignment"/> property.
+        /// </summary>
         public UIElement Target
         {
             get { return (UIElement)GetValue(TargetProperty); }
             set { SetValue(TargetProperty, value); }
         }
         
+        /// <summary>
+        /// Gets or sets the content to be displayed in the popover.
+        /// </summary>
         public object Content
         {
             get { return (object)GetValue(ContentProperty); }
             set { SetValue(ContentProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating if the popover open. 
+        /// </summary>
         public bool IsOpen
         {
             get { return (bool)GetValue(IsOpenProperty); }
             set { SetValue(IsOpenProperty, value); }
         }
 
+        /// <summary>
+        /// Gets or sets a <see cref="Boolean"/> value that indicates if the popover should be automatically closed when the user clicks else where in the owning window.
+        /// </summary>
         public bool AutoClose
         {
             get { return (bool)GetValue(AutoCloseProperty); }
             set { SetValue(AutoCloseProperty, value); }
         }
 
+        /// <summary>
+        /// Opens the popover.
+        /// </summary>
         public void Open()
         {
             IsOpen = true;
         }
 
+        /// <summary>
+        /// Closes the popover.
+        /// </summary>
         public void Close()
         {
             IsOpen = false;
@@ -277,26 +329,27 @@ namespace MahApps.Metro.Controls
 
         // Attach to parent window.
 
-        void OnUnloaded(object sender, RoutedEventArgs e)
-        {
-            Window owner = this.TryFindParent<Window>();
-            if(owner != null) {
-                owner.PreviewMouseDown -= OnPreviewOwningWindowMouseDown;
-            }
-        }
-
         void OnLoaded(object sender, RoutedEventArgs e)
         {
             Window owner = this.TryFindParent<Window>();
-            if (owner != null) {
-                owner.PreviewMouseDown += OnPreviewOwningWindowMouseDown;
+            if (owner != null && _previewOwningWindowClickSubscription == null) {
+                _previewOwningWindowClickSubscription = Disposable.Subscription<MouseButtonEventHandler>(handler => owner.PreviewMouseDown += handler, handler => owner.PreviewMouseDown -= handler, OnPreviewOwningWindowMouseDown);
             }
         }
 
+        void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (_previewOwningWindowClickSubscription != null) {
+                _previewOwningWindowClickSubscription.Dispose();
+                _previewOwningWindowClickSubscription = null;
+            }
+        }
+        
         private void OnPreviewOwningWindowMouseDown(object sender, MouseButtonEventArgs e)
         {
             var originalSource = e.OriginalSource as Visual;
             if (AutoClose && IsPopoverOpen() &&
+                originalSource != null &&    // didn't click on target or popover (adorner)
                 !Target.IsAncestorOf(originalSource) &&
                 !_adorner.IsAncestorOf(originalSource)) {
                 Close();
