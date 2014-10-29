7fca120f57c043e699f5556293ac75b5127fb589
Dennis Daume <daume.dennis@gmail.com>
2014-10-29 18:57:57 +0100

    Created the ButtonHelper.PreserveTextCase class to override the case behavior for certain buttons

---
+++ MahApps.Metro/Controls/ButtonHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public static class ButtonHelper
+    {
+        public static readonly DependencyProperty PreserveTextCaseProperty =
+                DependencyProperty.RegisterAttached("PreserveTextCase", typeof(bool), typeof(ButtonHelper), new FrameworkPropertyMetadata(false, 
+                    FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        [AttachedPropertyBrowsableForType(typeof(Button))]
+        public static bool GetPreserveTextCase(UIElement element)
+        {
+            return (bool)element.GetValue(PreserveTextCaseProperty);
+        }
+
+        public static void SetPreserveTextCase(UIElement element, bool value)
+        {
+            element.SetValue(PreserveTextCaseProperty, value);
+        }
+    }
+}
