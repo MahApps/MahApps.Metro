d21d4ba00e7fa0cb0eae585ee07d104579ef6cb7
Yang CAO <ycao@synergiz.com>
2015-02-06 10:47:01 +0100

    [Color.xaml] création d'un nouveau Accents nommé Dexip

--- MahApps.Metro/ThemeManager.cs
+++ MahApps.Metro/ThemeManager.cs
@@ -28,7 +28,7 @@ namespace MahApps.Metro
 
                 var colors = new[] {
                                        "Red", "Green", "Blue", "Purple", "Orange", "Lime", "Emerald", "Teal", "Cyan", "Cobalt",
-                                       "Indigo", "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna"
+                                       "Indigo", "Violet", "Pink", "Magenta", "Crimson", "Amber", "Yellow", "Brown", "Olive", "Steel", "Mauve", "Taupe", "Sienna", "Dexip"
                                    };
 
                 _accents = new List<Accent>(colors.Length);
