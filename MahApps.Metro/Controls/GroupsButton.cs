731aec4ba56b12e918d507958a48f96784e178f9
Harchay <achraf.harchay@yahoo.fr>
2015-03-03 20:34:10 +0100

    Feature Request: Button Groups #1820
    i'dlike to share this new feature which allow creating
    a button groups

---
+++ MahApps.Metro/Controls/GroupsButton.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace MahApps.Metro.Controls
+{
+    /// <summary>
+    ///     this Control allow to implement a group of button here are only two button , you have to add n button one by one here 
+    ///     and in the GroupButtom.xaml
+    /// </summary>
+    [ContentProperty("ItemsSource")]
+    [DefaultEvent("SelectionChanged"),
+    TemplatePart(Name = "PART_Container", Type = typeof(Grid)),
+    TemplatePart(Name = "PART_Button", Type = typeof(Button)),
+    TemplatePart(Name = "PART_Button2", Type = typeof(Button))]
+    public class GroupsButton : ItemsControl
+    {
+
+        #region Events
+
+        public static readonly RoutedEvent ClickEvent =
+            EventManager.RegisterRoutedEvent("Click", RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler), typeof(GroupsButton));
+
+        public static readonly RoutedEvent SelectionChangedEvent =
+            EventManager.RegisterRoutedEvent("SelectionChanged", RoutingStrategy.Bubble,
+                typeof(SelectionChangedEventHandler), typeof(GroupsButton));
+
+        public event SelectionChangedEventHandler SelectionChanged
+        {
+            add { AddHandler(SelectionChangedEvent, value); }
+            remove { RemoveHandler(SelectionChangedEvent, value); }
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add { AddHandler(ClickEvent, value); }
+            remove { RemoveHandler(ClickEvent, value); }
+        }
+
+        #endregion
+
+
+        #region DependencyProperties
+
+       
+
+        
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(Object), typeof(GroupsButton),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty ExtraTagProperty =
+            DependencyProperty.Register("ExtraTag", typeof(Object), typeof(GroupsButton));
+
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(GroupsButton),
+                new FrameworkPropertyMetadata(Orientation.Horizontal));
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(Object), typeof(GroupsButton));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(GroupsButton));
+
+        public static readonly DependencyProperty CommandTargetProperty =
+            DependencyProperty.Register("CommandTarget", typeof(IInputElement), typeof(GroupsButton));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(Object), typeof(GroupsButton));
+
+
+        public Object CommandParameter
+        {
+            get { return (Object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public IInputElement CommandTarget
+        {
+            get { return (IInputElement)GetValue(CommandTargetProperty); }
+            set { SetValue(CommandTargetProperty, value); }
+        }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        
+
+        public Object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+       
+
+        public Object ExtraTag
+        {
+            get { return GetValue(ExtraTagProperty); }
+            set { SetValue(ExtraTagProperty, value); }
+        }
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        public Object Icon
+        {
+            get { return GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        #endregion
+
+
+        #region Variables
+
+        private Button _clickButton;
+        private Button _Button2;
+       
+
+
+        #endregion
+
+
+        static GroupsButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GroupsButton), new FrameworkPropertyMetadata(typeof(GroupsButton)));
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            e.RoutedEvent = ClickEvent;
+            RaiseEvent(e);
+        }
+
+        private void Button2Click(object sender, RoutedEventArgs e)
+        {
+            e.RoutedEvent = ClickEvent;
+            RaiseEvent(e);
+        }
+
+       
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _clickButton = EnforceInstance<Button>("PART_Button");
+            _Button2 = EnforceInstance<Button>("PART_Button2");
+           
+            InitializeVisualElementsContainer();
+        }
+
+        //Get element from name. If it exist then element instance return, if not, new will be created
+        T EnforceInstance<T>(string partName) where T : FrameworkElement, new()
+        {
+            T element = GetTemplateChild(partName) as T ?? new T();
+            return element;
+        }
+
+        private void InitializeVisualElementsContainer()
+        {
+           
+            _clickButton.Click += ButtonClick;
+            _Button2.Click += Button2Click;
+           
+        }
+
+       
+
+       
+
+        private void OutsideCapturedElementHandler(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            
+            Mouse.RemovePreviewMouseDownOutsideCapturedElementHandler(this, OutsideCapturedElementHandler);
+        }
+
+    }
+
+}
