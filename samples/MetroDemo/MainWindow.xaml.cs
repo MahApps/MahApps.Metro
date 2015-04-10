9d2c42f03a8a6de719d3fcbda9a4f916a8d190f6
Christian Hermann <c.hermann@bitbeans.de>
2015-04-10 09:28:47 +0200

    New try for LoginDialog Password Preview Feature
    Fixes #1546.

--- samples/MetroDemo/MainWindow.xaml.cs
+++ samples/MetroDemo/MainWindow.xaml.cs
@@ -186,6 +186,21 @@ namespace MetroDemo
 
             await this.HideMetroDialogAsync(dialog);
         }
+
+         private async void ShowLoginDialogPasswordPreview(object sender, RoutedEventArgs e)
+        {
+            this.MetroDialogOptions.ColorScheme = UseAccentForDialogsMenuItem.IsChecked ? MetroDialogColorScheme.Accented : MetroDialogColorScheme.Theme;
+            LoginDialogData result = await this.ShowLoginAsync("Authentication", "Enter your credentials", new LoginDialogSettings { ColorScheme = this.MetroDialogOptions.ColorScheme, InitialUsername = "MahApps", EnablePasswordPreview = true });
+            if (result == null)
+            {
+                //User pressed cancel
+            }
+            else
+            {
+                MessageDialogResult messageResult = await this.ShowMessageAsync("Authentication Information", String.Format("Username: {0}\nPassword: {1}", result.Username, result.Password));
+            }
+        }
+
         private async void ShowProgressDialog(object sender, RoutedEventArgs e)
         {
             this.MetroDialogOptions.ColorScheme = UseAccentForDialogsMenuItem.IsChecked ? MetroDialogColorScheme.Accented : MetroDialogColorScheme.Theme;
