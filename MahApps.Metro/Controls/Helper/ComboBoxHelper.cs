dcf9a9882a7ba65840f33dd44ec63802137eb80a
Jan Karger <punkerat76@gmail.com>
2015-06-01 22:06:26 +0200

    MaxLength and CharacterCasing DPs

--- MahApps.Metro/Controls/Helper/ComboBoxHelper.cs
+++ MahApps.Metro/Controls/Helper/ComboBoxHelper.cs
@@ -33,5 +33,51 @@ namespace MahApps.Metro.Controls
         {
             return (bool)obj.GetValue(EnableVirtualizationWithGroupingProperty);
         }
+
+        public static readonly DependencyProperty MaxLengthProperty = DependencyProperty.RegisterAttached("MaxLength", typeof(int), typeof(ComboBoxHelper), new FrameworkPropertyMetadata(0), new ValidateValueCallback(MaxLengthValidateValue));
+
+        private static bool MaxLengthValidateValue(object value)
+        {
+            return ((int)value) >= 0;
+        }
+
+        /// <summary>
+        /// Gets the Maximum number of characters the TextBox can accept.
+        /// </summary>
+        public static int GetMaxLength(UIElement obj)
+        {
+            return (int)obj.GetValue(MaxLengthProperty);
+        }
+
+        /// <summary>
+        /// Sets the Maximum number of characters the TextBox can accept.
+        /// </summary>
+        public static void SetMaxLength(UIElement obj, int value)
+        {
+            obj.SetValue(MaxLengthProperty, value);
+        }
+
+        public static readonly DependencyProperty CharacterCasingProperty = DependencyProperty.RegisterAttached("CharacterCasing", typeof(CharacterCasing), typeof(ComboBoxHelper), new FrameworkPropertyMetadata(CharacterCasing.Normal), new ValidateValueCallback(CharacterCasingValidateValue));
+
+        private static bool CharacterCasingValidateValue(object value)
+        {
+            return (CharacterCasing.Normal <= (CharacterCasing)value && (CharacterCasing)value <= CharacterCasing.Upper);
+        }
+
+        /// <summary>
+        /// Gets the Character casing of the TextBox.
+        /// </summary>
+        public static CharacterCasing GetCharacterCasing(UIElement obj)
+        {
+            return (CharacterCasing)obj.GetValue(CharacterCasingProperty);
+        }
+
+        /// <summary>
+        /// Sets the Character casing of the TextBox.
+        /// </summary>
+        public static void SetCharacterCasing(UIElement obj, CharacterCasing value)
+        {
+            obj.SetValue(CharacterCasingProperty, value);
+        }
     }
 }
