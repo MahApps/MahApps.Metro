3b18ac034865f44732700482096f5e907b8603dc
Jan Karger <punkerat76@gmail.com>
2015-06-26 00:06:09 +0200

    the price is too much...

---
+++ samples/MetroDemo/ValueConverter/AlbumPriceIsTooMuchConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MetroDemo.ValueConverter
+{
+    public class AlbumPriceIsTooMuchConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is decimal)
+            {
+                var price = (decimal)value;
+                if (price > 15)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
