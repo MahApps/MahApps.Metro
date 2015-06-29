656ea6055298bf6e529422865d62663d651368ad
Jan Karger <punkerat76@gmail.com>
2015-06-29 13:59:44 +0200

    similar trick like DataGridCell does

---
+++ MahApps.Metro/Controls/Helper/DataGridRowHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public static class DataGridRowHelper
+    {
+        public static readonly DependencyProperty SelectionUnitProperty = DependencyProperty.RegisterAttached("SelectionUnit", typeof(DataGridSelectionUnit), typeof(DataGridRowHelper), new FrameworkPropertyMetadata(DataGridSelectionUnit.FullRow));
+
+        /// <summary>
+        /// Gets the value to define the DataGridRow selection behavior.
+        /// </summary>
+        [AttachedPropertyBrowsableForType(typeof(DataGridRow))]
+        public static DataGridSelectionUnit GetSelectionUnit(UIElement element)
+        {
+            return (DataGridSelectionUnit)element.GetValue(SelectionUnitProperty);
+        }
+
+        /// <summary>
+        /// Sets the value to define the DataGridRow selection behavior.
+        /// </summary>
+        public static void SetSelectionUnit(UIElement element, DataGridSelectionUnit value)
+        {
+            element.SetValue(SelectionUnitProperty, value);
+        }
+
+    }
+}
