2747e0561d17de59352183105e93c2bdfb66869e
unknown <DZaporozhets@WS-DZAPOROZHETS.lohika.com>
2015-07-07 12:56:04 +0300

    Add placement attached property to TextBoxHelper to have possibility move close button to the left side.

--- MahApps.Metro/Controls/Helper/TextBoxHelper.cs
+++ MahApps.Metro/Controls/Helper/TextBoxHelper.cs
@@ -22,6 +22,7 @@ namespace MahApps.Metro.Controls
         public static readonly DependencyProperty UseFloatingWatermarkProperty = DependencyProperty.RegisterAttached("UseFloatingWatermark", typeof(bool), typeof(TextBoxHelper), new FrameworkPropertyMetadata(false, ButtonCommandOrClearTextChanged));
         public static readonly DependencyProperty TextLengthProperty = DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(TextBoxHelper), new UIPropertyMetadata(0));
         public static readonly DependencyProperty ClearTextButtonProperty = DependencyProperty.RegisterAttached("ClearTextButton", typeof(bool), typeof(TextBoxHelper), new FrameworkPropertyMetadata(false, ButtonCommandOrClearTextChanged));
+        public static readonly DependencyProperty ButtonsPlacementProperty = DependencyProperty.RegisterAttached("ButtonsPlacement", typeof(ButtonsPlacementVariant), typeof(TextBoxHelper), new FrameworkPropertyMetadata(ButtonsPlacementVariant.Right, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
         /// <summary>
         /// The clear text button behavior property. It sets a click event to the button if the value is true.
         /// </summary>
@@ -300,6 +301,16 @@ namespace MahApps.Metro.Controls
             obj.SetValue(ClearTextButtonProperty, value);
         }
 
+        public static ButtonsPlacementVariant GetButtonsPlacement(DependencyObject d)
+        {
+           return (ButtonsPlacementVariant)d.GetValue(ButtonsPlacementProperty);
+        }
+
+        public static void SetButtonsPlacement(DependencyObject obj, ButtonsPlacementVariant value)
+        {
+           obj.SetValue(ButtonsPlacementProperty, value);
+        }
+
         /// <summary>
         /// Gets the clear text button behavior.
         /// </summary>
@@ -317,6 +328,8 @@ namespace MahApps.Metro.Controls
         {
             obj.SetValue(IsClearTextButtonBehaviorEnabledProperty, value);
         }
+
+        
 
         public static ICommand GetButtonCommand(DependencyObject d)
         {
