96a52f1208106eda259080b3b18f333f1d6d9dc8
Markus Remmel <markus.remmel@remolution.software.com>
2015-09-04 17:38:33 +0200

    Removed reference to System.IO

--- MahApps.Metro/Controls/WindowSettings.cs
+++ MahApps.Metro/Controls/WindowSettings.cs
@@ -2,7 +2,6 @@
 using System.ComponentModel;
 using System.Configuration;
 using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
