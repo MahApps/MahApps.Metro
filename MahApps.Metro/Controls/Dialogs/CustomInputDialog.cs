6cfed5dbad320eaa00b3fd94397f3698c9374c8d
hanlej <hanley_jordan@cat.com>
2015-10-16 11:15:32 +0100

    change waitbuttonpress method to abstract.

--- MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
+++ MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
@@ -1,88 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
 namespace MahApps.Metro.Controls.Dialogs
 {
-    public class CustomInputDialog<T> : BaseMetroDialog where T : class
-    {
-
-        internal virtual Task<T> WaitForButtonPressAsync()
-        {
-            Dispatcher.BeginInvoke(new Action(() => {
-                this.Focus();
-            }));
-
-            var tcs = new TaskCompletionSource<T>();
-
-            RoutedEventHandler negativeHandler = null;
-            KeyEventHandler negativeKeyHandler = null;
-
-            RoutedEventHandler affirmativeHandler = null;
-            KeyEventHandler affirmativeKeyHandler = null;
-
-            KeyEventHandler escapeKeyHandler = null;
-
-            Action cleanUpHandlers = null;
-
-            var cancellationTokenRegistration = DialogSettings.CancellationToken.Register(() => {
-                cleanUpHandlers();
-                tcs.TrySetResult(null);
-            });
-
-            cleanUpHandlers = () => {
-                cancellationTokenRegistration.Dispose();
-            };
-
-            escapeKeyHandler = (sender, e) => {
-                if (e.Key == Key.Escape)
-                {
-                    cleanUpHandlers();
-
-                    tcs.TrySetResult(null);
-                }
-            };
-
-            negativeKeyHandler = (sender, e) => {
-                if (e.Key == Key.Enter)
-                {
-                    cleanUpHandlers();
-
-                    tcs.TrySetResult(null);
-                }
-            };
-
-            affirmativeKeyHandler = (sender, e) => {
-                if (e.Key == Key.Enter)
-                {
-                    cleanUpHandlers();
-
-                    tcs.TrySetResult(Input);
-                }
-            };
-
-            negativeHandler = (sender, e) => {
-                cleanUpHandlers();
-
-                tcs.TrySetResult(null);
-
-                e.Handled = true;
-            };
-
-            affirmativeHandler = (sender, e) => {
-                cleanUpHandlers();
-
-                tcs.TrySetResult(Input);
-
-                e.Handled = true;
-            };
-
-            this.KeyDown += escapeKeyHandler;
-
-            return tcs.Task;
-        }
+    public abstract class CustomInputDialog<T> : BaseMetroDialog where T : class {
+        public abstract Task<T> WaitForButtonPressAsync(); 
 
         protected override void OnLoaded()
         {
@@ -90,21 +16,16 @@ namespace MahApps.Metro.Controls.Dialogs
             this.NegativeButtonText = this.DialogSettings.NegativeButtonText;
         }
 
-        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(InputDialog), new PropertyMetadata(default(string)));
-        public static readonly DependencyProperty InputProperty = DependencyProperty.Register("Input", typeof(string), typeof(InputDialog), new PropertyMetadata(default(string)));
-        public static readonly DependencyProperty AffirmativeButtonTextProperty = DependencyProperty.Register("AffirmativeButtonText", typeof(string), typeof(InputDialog), new PropertyMetadata("OK"));
-        public static readonly DependencyProperty NegativeButtonTextProperty = DependencyProperty.Register("NegativeButtonText", typeof(string), typeof(InputDialog), new PropertyMetadata("Cancel"));
+        public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty InputProperty = DependencyProperty.Register("Input", typeof(TaskCompletionSource<T>), typeof(CustomInputDialog<T>), new PropertyMetadata(default(T)));
+        public static readonly DependencyProperty AffirmativeButtonTextProperty = DependencyProperty.Register("AffirmativeButtonText", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata("OK"));
+        public static readonly DependencyProperty NegativeButtonTextProperty = DependencyProperty.Register("NegativeButtonText", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata("Cancel"));
+        
 
         public string Message
         {
             get { return (string)GetValue(MessageProperty); }
             set { SetValue(MessageProperty, value); }
-        }
-
-        public T Input
-        {
-            get { return (T)GetValue(InputProperty); }
-            set { SetValue(InputProperty, value); }
         }
 
         public string AffirmativeButtonText
