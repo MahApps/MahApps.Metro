ea06bed2d16dd5b668cb41834cb623f0fc914d13
hanlej <hanley_jordan@cat.com>
2015-10-21 08:15:57 +0100

    Remove unused references

--- MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
+++ MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 
 namespace MahApps.Metro.Controls.Dialogs
 {
