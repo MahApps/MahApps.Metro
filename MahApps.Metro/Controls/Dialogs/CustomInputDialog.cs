e4f33d6a49649487967b54a2b91912631efe3646
hanlej <hanley_jordan@cat.com>
2015-11-03 13:26:23 +0000

    Remove redundant Input property

--- MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
+++ MahApps.Metro/Controls/Dialogs/CustomInputDialog.cs
@@ -13,7 +13,6 @@ namespace MahApps.Metro.Controls.Dialogs
         }
 
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata(default(string)));
-        public static readonly DependencyProperty InputProperty = DependencyProperty.Register("Input", typeof(TaskCompletionSource<T>), typeof(CustomInputDialog<T>), new PropertyMetadata(default(T)));
         public static readonly DependencyProperty AffirmativeButtonTextProperty = DependencyProperty.Register("AffirmativeButtonText", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata("OK"));
         public static readonly DependencyProperty NegativeButtonTextProperty = DependencyProperty.Register("NegativeButtonText", typeof(string), typeof(CustomInputDialog<T>), new PropertyMetadata("Cancel"));
         
