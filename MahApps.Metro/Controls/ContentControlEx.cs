46fc4aff212b3ebdfcb11a9deb150f98b9de0a49
Jan Karger <punkerat76@gmail.com>
2015-11-14 22:37:57 +0100

    new ContentControlEx

---
+++ MahApps.Metro/Controls/ContentControlEx.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public class ContentControlEx : ContentControl
+    {
+        /// <summary>
+        /// The DependencyProperty for the CharacterCasing property.
+        /// Controls whether or not content is converted to upper or lower case
+        /// Default Value: CharacterCasing.Normal
+        /// </summary>
+        public static readonly DependencyProperty ContentCharacterCasingProperty =
+            DependencyProperty.Register(
+                "ContentCharacterCasing",
+                typeof(CharacterCasing),
+                typeof(ContentControlEx),
+                new FrameworkPropertyMetadata(CharacterCasing.Normal, FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsMeasure),
+                new ValidateValueCallback(value => CharacterCasing.Normal <= (CharacterCasing)value && (CharacterCasing)value <= CharacterCasing.Upper));
+
+        /// <summary> 
+        /// Character casing of the Content
+        /// </summary> 
+        public CharacterCasing ContentCharacterCasing
+        {
+            get { return (CharacterCasing)GetValue(ContentCharacterCasingProperty); }
+            set { SetValue(ContentCharacterCasingProperty, value); }
+        }
+    }
+}
