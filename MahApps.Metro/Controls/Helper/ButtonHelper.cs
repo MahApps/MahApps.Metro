0e061d8de2871040696b62e97f862f8bb6881784
Jan Karger <punkerat76@gmail.com>
2015-11-14 16:47:25 +0100

    CornerRadius for ComboBox, Closes #2207

--- MahApps.Metro/Controls/Helper/ButtonHelper.cs
+++ MahApps.Metro/Controls/Helper/ButtonHelper.cs
@@ -7,7 +7,7 @@ namespace MahApps.Metro.Controls
 {
     public static class ButtonHelper
     {
-        [Obsolete(@"This property will be deleted in the next release. You should use ContentCharacterCasing attached property located at ControlsHelper.")]
+        [Obsolete(@"This property will be deleted in the next release. You should use ContentCharacterCasing attached property located in ControlsHelper.")]
         public static readonly DependencyProperty PreserveTextCaseProperty =
             DependencyProperty.RegisterAttached("PreserveTextCase", typeof(bool), typeof(ButtonHelper),
                                                 new FrameworkPropertyMetadata(
@@ -45,6 +45,7 @@ namespace MahApps.Metro.Controls
         /// <summary>
         /// DependencyProperty for <see cref="CornerRadius" /> property.
         /// </summary>
+        [Obsolete(@"This property will be deleted in the next release. You should use CornerRadius attached property located in ControlsHelper.")]
         public static readonly DependencyProperty CornerRadiusProperty
             = DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ButtonHelper),
                                                   new FrameworkPropertyMetadata(
@@ -61,12 +62,12 @@ namespace MahApps.Metro.Controls
         [AttachedPropertyBrowsableForType(typeof(ToggleButton))]
         public static CornerRadius GetCornerRadius(UIElement element)
         {
-            return (CornerRadius)element.GetValue(CornerRadiusProperty);
+            return ControlsHelper.GetCornerRadius(element);
         }
 
         public static void SetCornerRadius(UIElement element, CornerRadius value)
         {
-            element.SetValue(CornerRadiusProperty, value);
+            ControlsHelper.SetCornerRadius(element, value);
         }
     }
 }
--- MahApps.Metro/Controls/Helper/ControlsHelper.cs
+++ MahApps.Metro/Controls/Helper/ControlsHelper.cs
@@ -209,5 +209,30 @@ namespace MahApps.Metro.Controls
         {
             return (Brush)obj.GetValue(MouseOverBorderBrushProperty);
         }
+
+        /// <summary>
+        /// DependencyProperty for <see cref="CornerRadius" /> property.
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty
+            = DependencyProperty.RegisterAttached("CornerRadius", typeof(CornerRadius), typeof(ControlsHelper),
+                                                  new FrameworkPropertyMetadata(
+                                                      new CornerRadius(),
+                                                      FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+
+        /// <summary> 
+        /// The CornerRadius property allows users to control the roundness of the button corners independently by 
+        /// setting a radius value for each corner. Radius values that are too large are scaled so that they
+        /// smoothly blend from corner to corner. (Can be used e.g. at MetroButton style)
+        /// Description taken from original Microsoft description :-D
+        /// </summary>
+        public static CornerRadius GetCornerRadius(UIElement element)
+        {
+            return (CornerRadius)element.GetValue(CornerRadiusProperty);
+        }
+
+        public static void SetCornerRadius(UIElement element, CornerRadius value)
+        {
+            element.SetValue(CornerRadiusProperty, value);
+        }
     }
 }
