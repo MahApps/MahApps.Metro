6b44cbd4952f978fda00991e7cd54b460a0a6ae0
Jan Karger <punkerat76@gmail.com>
2015-12-31 18:08:27 +0100

    :pushpin: v1.2.0

--- MahApps.Metro/Properties/AssemblyInfo.cs
+++ MahApps.Metro/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-[assembly: AssemblyCopyright("Copyright © MahApps.Metro 2011-2015")]
+[assembly: AssemblyCopyright("Copyright © MahApps.Metro 2011-2016")]
 [assembly: ComVisible(false)]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
@@ -13,9 +12,9 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/shared", "MahApps.Metro.Converters")]
 [assembly: XmlnsDefinition("http://metro.mahapps.com/winfx/xaml/controls", "MahApps.Metro.Controls")]
 
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: AssemblyTitleAttribute("MahApps.Metro")]
 [assembly: AssemblyDescriptionAttribute("Toolkit for creating Metro styled WPF apps")]
-[assembly: AssemblyProductAttribute("MahApps.Metro 1.1.3-ALPHA")]
+[assembly: AssemblyProductAttribute("MahApps.Metro 1.2.0")]
 [assembly: AssemblyCompany("MahApps")]
