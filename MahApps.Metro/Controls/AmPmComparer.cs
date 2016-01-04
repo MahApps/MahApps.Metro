793e188ad51dade42f1197a9c1e1340fa6844f2f
Michael Mairegger <michael.mairegger@yahoo.de>
2016-01-04 15:03:11 +0100

    Introducing DateTimePicker #2281

---
+++ MahApps.Metro/Controls/AmPmComparer.cs
@@ -0,0 +1,24 @@
+namespace MahApps.Metro.Controls
+{
+    using System.Collections.Generic;
+
+    public class AmPmComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x == 12 && y == 12)
+            {
+                return 0;
+            }
+            if (x == 12)
+            {
+                return -1;
+            }
+            if (y == 12)
+            {
+                return 1;
+            }
+            return x.CompareTo(y);
+        }
+    }
+}
