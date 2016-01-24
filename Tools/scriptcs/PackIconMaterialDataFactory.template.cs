859f6bb88f83d8fb9ad1193af71849f70e81f55b
Jan Karger <punkerat76@gmail.com>
2016-01-24 22:10:29 +0100

    initial PackIcon creation stuff using scriptcs first Material Design, inspired by @ButchersBoy

---
+++ Tools/scriptcs/PackIconMaterialDataFactory.template.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MahApps.Metro
+{
+    /// ******************************************
+    /// This code is auto generated. Do not amend.
+    /// ******************************************
+
+    internal static class PackIconMaterialDataFactory
+    {
+        internal static IDictionary<PackIconMaterialKind, string> Create() => new Dictionary<PackIconMaterialKind, string>
+        {
+        };
+    }
+}
