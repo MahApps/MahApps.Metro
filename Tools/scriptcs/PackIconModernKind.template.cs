3845df62cacfeb7cd4416c80abed64d283004bb2
Jan Karger <punkerat76@gmail.com>
2016-01-25 01:15:11 +0100

    first try for old modern ui icons problems with icons which have more than one paths...

---
+++ Tools/scriptcs/PackIconModernKind.template.cs
@@ -0,0 +1,17 @@
+﻿namespace MahApps.Metro
+{
+    /// ******************************************
+    /// This code is auto generated. Do not amend.
+    /// ******************************************
+
+    /// <summary>
+    /// List of available icons for use with <see cref="PackIcon" />.
+    /// </summary>
+    /// <remarks>
+    /// All icons sourced from Material Design Icons Font - <see cref="http://modernuiicons.com/"/> - in accordance of
+    /// <see cref="https://github.com/Templarian/WindowsIcons/blob/master/WindowsPhone/license.txt"/>.
+    /// </remarks>
+    public enum PackIconModernKind
+    {
+    }
+}
