e8229a4d2ffd4791bbf9d64f483dbbfea400ed0e
Jan Karger <punkerat76@gmail.com>
2016-02-12 00:02:15 +0100

    prepare Icon filtering

--- samples/MetroDemo/ExampleWindows/IconPacksWindow.xaml.cs
+++ samples/MetroDemo/ExampleWindows/IconPacksWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using JetBrains.Annotations;
 using MahApps.Metro;
 using MahApps.Metro.Controls;
 
@@ -13,40 +16,11 @@ namespace MetroDemo.ExampleWindows
     /// </summary>
     public partial class IconPacksWindow : MetroWindow
     {
-        public static readonly DependencyProperty MaterialKindsProperty = DependencyProperty.Register("MaterialKinds", typeof(IEnumerable<PackIconMaterialKind>), typeof(IconPacksWindow), new PropertyMetadata(default(IEnumerable<PackIconMaterialKind>)));
-
-        public IEnumerable<PackIconMaterialKind> MaterialKinds
-        {
-            get { return (IEnumerable<PackIconMaterialKind>)GetValue(MaterialKindsProperty); }
-            set { SetValue(MaterialKindsProperty, value); }
-        }
-
-        public static readonly DependencyProperty ModernKindsProperty = DependencyProperty.Register("ModernKinds", typeof(IEnumerable<PackIconModernKind>), typeof(IconPacksWindow), new PropertyMetadata(default(IEnumerable<PackIconModernKind>)));
-
-        public IEnumerable<PackIconModernKind> ModernKinds
-        {
-            get { return (IEnumerable<PackIconModernKind>)GetValue(ModernKindsProperty); }
-            set { SetValue(ModernKindsProperty, value); }
-        }
-
         public IconPacksWindow()
         {
-            DataContext = this;
+            DataContext = new IconPacksViewModel();
             InitializeComponent();
-            this.Loaded += IconPacksWindow_Loaded;
-        }
-
-        private void IconPacksWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            MaterialKinds = new Lazy<IEnumerable<PackIconMaterialKind>>(
-                () => Enum.GetValues(typeof(PackIconMaterialKind)).OfType<PackIconMaterialKind>()
-                          .OrderBy(k => k.ToString(), StringComparer.InvariantCultureIgnoreCase).ToList()
-                ).Value;
-
-            ModernKinds = new Lazy<IEnumerable<PackIconModernKind>>(
-                () => Enum.GetValues(typeof(PackIconModernKind)).OfType<PackIconModernKind>()
-                          .OrderBy(k => k.ToString(), StringComparer.InvariantCultureIgnoreCase).ToList()
-                ).Value;
+            this.Loaded += (sender, args) => ((IconPacksViewModel)this.DataContext).Initialize();
         }
 
         private void HyperlinkMaterialOnClick(object sender, RoutedEventArgs e)
@@ -63,6 +37,60 @@ namespace MetroDemo.ExampleWindows
         {
             var textBox = (TextBox)sender;
             textBox.Dispatcher.BeginInvoke(new Action(textBox.SelectAll));
+        }
+    }
+
+    public class IconPacksViewModel : INotifyPropertyChanged
+    {
+        public IconPacksViewModel()
+        {
+        }
+
+        public void Initialize()
+        {
+            ModernKinds = new Lazy<IEnumerable<PackIconModernKind>>(
+                () => Enum.GetValues(typeof(PackIconModernKind)).OfType<PackIconModernKind>()
+                          .OrderBy(k => k.ToString(), StringComparer.InvariantCultureIgnoreCase).ToList()
+                ).Value;
+
+            MaterialKinds = new Lazy<IEnumerable<PackIconMaterialKind>>(
+                () => Enum.GetValues(typeof(PackIconMaterialKind)).OfType<PackIconMaterialKind>()
+                          .OrderBy(k => k.ToString(), StringComparer.InvariantCultureIgnoreCase).ToList()
+                ).Value;
+        }
+
+        private IEnumerable<PackIconModernKind> _modernKinds;
+
+        public IEnumerable<PackIconModernKind> ModernKinds
+        {
+            get { return _modernKinds; }
+            set
+            {
+                if (Equals(value, _modernKinds)) return;
+                _modernKinds = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private IEnumerable<PackIconMaterialKind> _materialKinds;
+
+        public IEnumerable<PackIconMaterialKind> MaterialKinds
+        {
+            get { return _materialKinds; }
+            set
+            {
+                if (Equals(value, _materialKinds)) return;
+                _materialKinds = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
