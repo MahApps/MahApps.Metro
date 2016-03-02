0b03ec3f431e5b7c46b80f27ef972878e273f185
Fernando Bastos <fe@pobox.com>
2016-03-02 19:52:00 -0300

    Issue #1835

--- MahApps.Metro/Controls/MetroContentControl.cs
+++ MahApps.Metro/Controls/MetroContentControl.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace MahApps.Metro.Controls
 {
@@ -40,6 +42,18 @@ namespace MahApps.Metro.Controls
 
             Loaded += MetroContentControlLoaded;
             Unloaded += MetroContentControlUnloaded;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var sb = ((Storyboard)GetTemplateChild("AfterLoadedStoryBoard"));
+            sb.Completed += Sb_Completed;
+        }
+
+        private void Sb_Completed(object sender, EventArgs e)
+        {
+            this.InvalidateVisual();
         }
 
         void MetroContentControlIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
@@ -109,5 +123,6 @@ namespace MahApps.Metro.Controls
             }
 
         }
+      
     }
 }
