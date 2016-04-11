28f00415f413be8bce87a6b6dbcf6a11f2c6ba98
Michael Mairegger <michael.mairegger@yahoo.de>
2016-04-11 16:36:13 +0200

    Method ordering.

--- MahApps.Metro/Controls/Helper/VirtualKeyboardHelper.cs
+++ MahApps.Metro/Controls/Helper/VirtualKeyboardHelper.cs
@@ -4,16 +4,22 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System;
+using System.Linq;
+using System.Management;
 
 namespace MahApps.Metro.Controls.Helper
 {
-    using System.Linq;
-    using System.Management;
-
     public class VirtualKeyboardHelper
     {
+        private const uint SC_CLOSE = 61536;
+        private const uint WM_SYSCOMMAND = 274;
+        private static DateTime lastCloseRequest;
+
         public static readonly DependencyProperty EnableVirtualKeyboardProperty = DependencyProperty.RegisterAttached(
-            "EnableVirtualKeyboard", typeof(bool), typeof(VirtualKeyboardHelper), new PropertyMetadata(default(bool), OnEnableVirtualKeyboardChanged));
+            "EnableVirtualKeyboard",
+            typeof(bool),
+            typeof(VirtualKeyboardHelper),
+            new PropertyMetadata(default(bool), OnEnableVirtualKeyboardChanged));
 
         private static void OnEnableVirtualKeyboardChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
@@ -35,45 +41,28 @@ namespace MahApps.Metro.Controls.Helper
             element.SetValue(EnableVirtualKeyboardProperty, value);
         }
 
-        private static DateTime lastCloseRequest;
-
-        public static void CloseVirtualKeyboard()
+        private static void CloseVirtualKeyboard()
         {
-            uint WM_SYSCOMMAND = 274;
-            uint SC_CLOSE = 61536;
-            IntPtr KeyboardWnd = FindWindow("IPTip_Main_Window", null);
-            PostMessage(KeyboardWnd.ToInt32(), WM_SYSCOMMAND, (int)SC_CLOSE, 0);
+            IntPtr keyboardWnd = FindWindow("IPTip_Main_Window", null);
+            PostMessage(keyboardWnd.ToInt32(), WM_SYSCOMMAND, (int)SC_CLOSE, 0);
         }
 
         [DllImport("user32.dll")]
-        public static extern IntPtr FindWindow(string sClassName, string sAppName);
-
-        public static void OpenVirtualKeyboard()
-        {
-            ProcessStartInfo startInfo;
-            if (Environment.Is64BitOperatingSystem)
-            {
-                startInfo = new ProcessStartInfo(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
-            }
-            else
-            {
-                startInfo = new ProcessStartInfo(@"C:\Program Files (x86)\Common Files\Microsoft Shared\ink\TabTip32.exe");
-            }
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            Process.Start(startInfo);
-        }
-
-        [return: MarshalAs(UnmanagedType.Bool)]
-        [DllImport("user32.dll", SetLastError = true)]
-        public static extern bool PostMessage(int hWnd, uint msg, int wParam, int lParam);
+        private static extern IntPtr FindWindow(string sClassName, string sAppName);
 
         private static bool IsSurfaceKeyboardAttached()
         {
-            SelectQuery Sq = new SelectQuery("Win32_Keyboard");
-            ManagementObjectSearcher objOSDetails = new ManagementObjectSearcher(Sq);
-            ManagementObjectCollection osDetailsCollection = objOSDetails.Get();
-            var amountExternalKeyboards = osDetailsCollection.Cast<ManagementObject>().Select(mo => (string)mo.GetPropertyValue("PNPDeviceID")).Count(i => i.StartsWith("USB"));
-            return amountExternalKeyboards > 0;
+            using (var objOsDetails = new ManagementObjectSearcher("Win32_Keyboard"))
+            {
+                using (var osDetailsCollection = objOsDetails.Get())
+                {
+                    var amountExternalKeyboards = osDetailsCollection.Cast<ManagementObject>()
+                                                                     .Select(mo => (string)mo.GetPropertyValue("PNPDeviceID"))
+                                                                     .Count(i => i.StartsWith("USB"));
+
+                    return amountExternalKeyboards > 0;
+                }
+            }
         }
 
         private static void OnGotFocus(object sender, RoutedEventArgs e)
@@ -92,11 +81,30 @@ namespace MahApps.Metro.Controls.Helper
                 .StartNew(() => { Thread.Sleep(10); })
                 .ContinueWith(t =>
                                   {
-                                      if ((DateTime.Now - lastCloseRequest) > TimeSpan.Zero)
+                                      if (DateTime.Now - lastCloseRequest > TimeSpan.Zero)
                                       {
                                           CloseVirtualKeyboard();
                                       }
                                   });
         }
+
+        private static void OpenVirtualKeyboard()
+        {
+            ProcessStartInfo startInfo;
+            if (Environment.Is64BitOperatingSystem)
+            {
+                startInfo = new ProcessStartInfo(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
+            }
+            else
+            {
+                startInfo = new ProcessStartInfo(@"C:\Program Files (x86)\Common Files\Microsoft Shared\ink\TabTip32.exe");
+            }
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(startInfo);
+        }
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool PostMessage(int hWnd, uint msg, int wParam, int lParam);
     }
 }
