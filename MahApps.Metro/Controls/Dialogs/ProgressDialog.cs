MahApps/MahApps.Metro

commit 8a0845f466b08cf3dd9287cf9566a95dcedeaf14

changes for #2459

MahApps.Metro/Controls/Dialogs/ProgressDialog.cs
@@ -10,29 +10,30 @@ namespace MahApps.Metro.Controls.Dialogs
     /// </summary>
     public partial class ProgressDialog : BaseMetroDialog
     {
+        internal ProgressDialog(MetroWindow parentWindow)
+            : this(parentWindow, null)
+        {
+        }
+
         internal ProgressDialog(MetroWindow parentWindow, MetroDialogSettings settings)
             : base(parentWindow, settings)
         {
-            InitializeComponent();
-
-            if (parentWindow.MetroDialogOptions.ColorScheme == MetroDialogColorScheme.Theme)
-            {
-                try
-                {
-                    ProgressBarForeground = ThemeManager.GetResourceFromAppStyle(parentWindow, "AccentColorBrush") as Brush;
-                }
-                catch (Exception) { }
-            }
-
-            else
-            {
-                ProgressBarForeground = Brushes.White;
-            }
+            this.InitializeComponent();
         }
 
-        internal ProgressDialog(MetroWindow parentWindow)
-            : this(parentWindow, null)
-        { }
+        protected override void OnLoaded()
+        {
+            this.NegativeButtonText = this.DialogSettings.NegativeButtonText;
+
+            if (this.DialogSettings.ColorScheme == MetroDialogColorScheme.Theme)
+            {
+                this.SetResourceReference(ProgressBarForegroundProperty, ThemeManager.GetResourceFromAppStyle(this.OwningWindow ?? Application.Current.MainWindow, "AccentColorBrush") as Brush);
+            }
+            else
+            {
+                this.SetResourceReference(ProgressBarForegroundProperty, Brushes.White);
+            }
+        }
 
         public static readonly DependencyProperty ProgressBarForegroundProperty = DependencyProperty.Register("ProgressBarForeground", typeof(Brush), typeof(ProgressDialog), new PropertyMetadata(default(string)));
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(ProgressDialog), new PropertyMetadata(default(string)));

MahApps.Metro/Controls/Dialogs/DialogManager.cs
@@ -228,11 +228,10 @@ namespace MahApps.Metro.Controls.Dialogs
                     settings = settings ?? window.MetroDialogOptions;
 
                     //create the dialog control
-                    var dialog = new ProgressDialog(window)
+                    var dialog = new ProgressDialog(window, settings)
                     {
-                        Message = message,
-                        NegativeButtonText = settings.NegativeButtonText,
                         Title = title,
+                        Message = message,
                         IsCancelable = isCancelable
                     };
 

samples/MetroDemo/MainWindow.xaml.cs
@@ -292,7 +292,14 @@ namespace MetroDemo
 
         private async void ShowProgressDialog(object sender, RoutedEventArgs e)
         {
-            var controller = await this.ShowProgressAsync("Please wait...", "We are baking some cupcakes!");
+            var mySettings = new MetroDialogSettings()
+            {
+                NegativeButtonText = "Close now",
+                AnimateShow = false,
+                AnimateHide = false
+            };
+
+            var controller = await this.ShowProgressAsync("Please wait...", "We are baking some cupcakes!", settings: mySettings);
             controller.SetIndeterminate();
 
             await TaskEx.Delay(5000);

commit 511ac76ff8375ab9ab725db7c55d7e7cc338a4d6

fix progress foreground, broken by 8a0845f466b08cf3dd9287cf9566a95dcedeaf14 (#2459)

MahApps.Metro/Controls/Dialogs/ProgressDialog.cs
@@ -24,23 +24,15 @@ namespace MahApps.Metro.Controls.Dialogs
         protected override void OnLoaded()
         {
             this.NegativeButtonText = this.DialogSettings.NegativeButtonText;
-
-            if (this.DialogSettings.ColorScheme == MetroDialogColorScheme.Theme)
-            {
-                this.SetResourceReference(ProgressBarForegroundProperty, ThemeManager.GetResourceFromAppStyle(this.OwningWindow ?? Application.Current.MainWindow, "AccentColorBrush") as Brush);
-            }
-            else
-            {
-                this.SetResourceReference(ProgressBarForegroundProperty, Brushes.White);
-            }
+            this.SetResourceReference(ProgressBarForegroundProperty, this.DialogSettings.ColorScheme == MetroDialogColorScheme.Theme ? "AccentColorBrush" : "BlackBrush");
         }
 
-        public static readonly DependencyProperty ProgressBarForegroundProperty = DependencyProperty.Register("ProgressBarForeground", typeof(Brush), typeof(ProgressDialog), new PropertyMetadata(default(string)));
+        public static readonly DependencyProperty ProgressBarForegroundProperty = DependencyProperty.Register("ProgressBarForeground", typeof(Brush), typeof(ProgressDialog), new FrameworkPropertyMetadata(default(Brush), FrameworkPropertyMetadataOptions.AffectsRender));
         public static readonly DependencyProperty MessageProperty = DependencyProperty.Register("Message", typeof(string), typeof(ProgressDialog), new PropertyMetadata(default(string)));
         public static readonly DependencyProperty IsCancelableProperty = DependencyProperty.Register("IsCancelable", typeof(bool), typeof(ProgressDialog), new PropertyMetadata(default(bool), (s, e) =>
-        {
-            ((ProgressDialog)s).PART_NegativeButton.Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Hidden;
-        }));
+            {
+                ((ProgressDialog)s).PART_NegativeButton.Visibility = (bool)e.NewValue ? Visibility.Visible : Visibility.Hidden;
+            }));
         public static readonly DependencyProperty NegativeButtonTextProperty = DependencyProperty.Register("NegativeButtonText", typeof(string), typeof(ProgressDialog), new PropertyMetadata("Cancel"));
 
         public string Message

MahApps.Metro/Controls/Dialogs/ProgressDialogController.cs
@@ -10,15 +10,14 @@ namespace MahApps.Metro.Controls.Dialogs
     public class ProgressDialogController
     {
         private ProgressDialog WrappedDialog { get; set; }
-        private Func<Task> CloseCallback { get; set; }
 
-       
+        private Func<Task> CloseCallback { get; set; }
 
         /// <summary>
         /// This event is raised when the associated <see cref="ProgressDialog"/> was closed programmatically.
         /// </summary>
         public event EventHandler Closed;
-   
+
         /// <summary>
         /// This event is raised when the associated <see cref="ProgressDialog"/> was cancelled by the user.
         /// </summary>
@@ -27,12 +26,12 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <summary>
         /// Gets if the Cancel button has been pressed.
         /// </summary>        
-        public bool IsCanceled { get; private set; }        
+        public bool IsCanceled { get; private set; }
 
         /// <summary>
         /// Gets if the wrapped ProgressDialog is open.
         /// </summary>        
-        public bool IsOpen { get; private set;}
+        public bool IsOpen { get; private set; }
 
         internal ProgressDialogController(ProgressDialog dialog, Func<Task> closeCallBack)
         {
@@ -41,29 +40,20 @@ namespace MahApps.Metro.Controls.Dialogs
 
             IsOpen = dialog.IsVisible;
 
-            InvokeAction(() => {
-                WrappedDialog.PART_NegativeButton.Click += PART_NegativeButton_Click;
-            });
+            WrappedDialog.Invoke(() => { WrappedDialog.PART_NegativeButton.Click += PART_NegativeButton_Click; });
 
-            dialog.CancellationToken.Register(() =>
-            {
-                PART_NegativeButton_Click(null, new RoutedEventArgs());
-            });
+            dialog.CancellationToken.Register(() => { PART_NegativeButton_Click(null, new RoutedEventArgs()); });
         }
 
         private void PART_NegativeButton_Click(object sender, RoutedEventArgs e)
         {
-            Action action = () => {
-                IsCanceled = true;
-                var handler = Canceled;
-                if (handler != null)
+            Action action = () =>
                 {
-                    handler(this, EventArgs.Empty);
-                }
-                WrappedDialog.PART_NegativeButton.IsEnabled = false;
-            };
-
-            InvokeAction(action);
+                    this.IsCanceled = true;
+                    this.Canceled?.Invoke(this, EventArgs.Empty);
+                    this.WrappedDialog.PART_NegativeButton.IsEnabled = false;
+                };
+            WrappedDialog.Invoke(action);
         }
 
         /// <summary>
@@ -71,7 +61,7 @@ namespace MahApps.Metro.Controls.Dialogs
         /// </summary>
         public void SetIndeterminate()
         {
-            InvokeAction(() => WrappedDialog.SetIndeterminate());
+            WrappedDialog.Invoke(() => WrappedDialog.SetIndeterminate());
         }
 
         /// <summary>
@@ -80,7 +70,7 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <param name="value"></param>
         public void SetCancelable(bool value)
         {
-            InvokeAction(() => WrappedDialog.IsCancelable = value);
+            WrappedDialog.Invoke(() => WrappedDialog.IsCancelable = value);
         }
 
         /// <summary>
@@ -89,16 +79,15 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <param name="value">The percentage to set as the value.</param>
         public void SetProgress(double value)
         {
-            Action action = () => {
-                if (value < WrappedDialog.Minimum || value > WrappedDialog.Maximum)
+            Action action = () =>
                 {
-                    throw new ArgumentOutOfRangeException("value");
-                }
-
-                WrappedDialog.ProgressValue = value;
-            };
-
-            InvokeAction(action);
+                    if (value < WrappedDialog.Minimum || value > WrappedDialog.Maximum)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(value));
+                    }
+                    WrappedDialog.ProgressValue = value;
+                };
+            WrappedDialog.Invoke(action);
         }
 
         /// <summary>
@@ -106,8 +95,8 @@ namespace MahApps.Metro.Controls.Dialogs
         /// </summary>
         public double Minimum
         {
-            get { return InvokeFunc(() => WrappedDialog.Minimum); }
-            set { InvokeAction(() => WrappedDialog.Minimum = value); }
+            get { return WrappedDialog.Invoke(() => WrappedDialog.Minimum); }
+            set { WrappedDialog.Invoke(() => WrappedDialog.Minimum = value); }
         }
 
         /// <summary>
@@ -115,8 +104,8 @@ namespace MahApps.Metro.Controls.Dialogs
         /// </summary>
         public double Maximum
         {
-            get { return InvokeFunc(() => WrappedDialog.Maximum); }
-            set { InvokeAction(() => WrappedDialog.Maximum = value); }
+            get { return WrappedDialog.Invoke(() => WrappedDialog.Maximum); }
+            set { WrappedDialog.Invoke(() => WrappedDialog.Maximum = value); }
         }
 
         /// <summary>
@@ -125,7 +114,7 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <param name="message">The message to be set.</param>
         public void SetMessage(string message)
         {
-            InvokeAction(() => WrappedDialog.Message = message);
+            WrappedDialog.Invoke(() => WrappedDialog.Message = message);
         }
 
         /// <summary>
@@ -134,10 +123,8 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <param name="title">The title to be set.</param>
         public void SetTitle(string title)
         {
-            InvokeAction(() => WrappedDialog.Title = title);
+            WrappedDialog.Invoke(() => WrappedDialog.Title = title);
         }
-
-       
 
         /// <summary>
         /// Begins an operation to close the ProgressDialog.
@@ -145,49 +132,23 @@ namespace MahApps.Metro.Controls.Dialogs
         /// <returns>A task representing the operation.</returns>
         public Task CloseAsync()
         {
-            Action action = () => {
-                if (!WrappedDialog.IsVisible)
+            Action action = () =>
                 {
-                    throw new InvalidOperationException("Dialog isn't visible to close");
-                }
-                WrappedDialog.Dispatcher.VerifyAccess();
-                WrappedDialog.PART_NegativeButton.Click -= PART_NegativeButton_Click;
-            };
+                    if (!WrappedDialog.IsVisible)
+                    {
+                        throw new InvalidOperationException("Dialog isn't visible to close");
+                    }
+                    WrappedDialog.Dispatcher.VerifyAccess();
+                    WrappedDialog.PART_NegativeButton.Click -= PART_NegativeButton_Click;
+                };
 
-            InvokeAction(action);
+            WrappedDialog.Invoke(action);
 
-            return CloseCallback().ContinueWith(_ => InvokeAction(new Action(() => {
-                IsOpen = false;
-
-                var handler = Closed;
-                if (handler != null) {
-                    handler(this, EventArgs.Empty);
-                }
-            })));
-        }
-
-        private double InvokeFunc(Func<double> getValueFunc)
-        {
-            if (WrappedDialog.Dispatcher.CheckAccess())
-            {
-                return getValueFunc();
-            }
-            else
-            {
-                return (double)WrappedDialog.Dispatcher.Invoke(new Func<double>(getValueFunc));
-            }
-        }
-
-        private void InvokeAction(Action setValueAction)
-        {
-            if (WrappedDialog.Dispatcher.CheckAccess())
-            {
-                setValueAction();
-            }
-            else
-            {
-                WrappedDialog.Dispatcher.Invoke(setValueAction);
-            }
+            return this.CloseCallback().ContinueWith(_ => this.WrappedDialog.Invoke(() =>
+                {
+                    this.IsOpen = false;
+                    this.Closed?.Invoke(this, EventArgs.Empty);
+                }));
         }
     }
 }