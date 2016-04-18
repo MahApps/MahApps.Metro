ac36af2e77dfd4b8df2f5d5850d0f86771704e36
Michael Mairegger <michael.mairegger@yahoo.de>
2016-04-18 08:51:45 +0200

    Refactor method to avoid duplicate code.

--- MahApps.Metro/Controls/Dialogs/DialogCoordinator.cs
+++ MahApps.Metro/Controls/Dialogs/DialogCoordinator.cs
@@ -14,76 +14,67 @@ namespace MahApps.Metro.Controls.Dialogs
 
         public Task<string> ShowInputAsync(object context, string title, string message, MetroDialogSettings metroDialogSettings = null)
         {            
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.ShowInputAsync(title, message, metroDialogSettings));
+            return ExecuteShowMessageDialog(context, window => window.ShowInputAsync(title, message, metroDialogSettings));
         }
 
         public Task<LoginDialogData> ShowLoginAsync(object context, string title, string message, LoginDialogSettings settings = null)
         {
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.ShowLoginAsync(title, message, settings));
+            return ExecuteShowMessageDialog(context, window => window.ShowLoginAsync(title, message, settings));
         }
 
         public Task<MessageDialogResult> ShowMessageAsync(object context, string title, string message, MessageDialogStyle style = MessageDialogStyle.Affirmative, MetroDialogSettings settings = null)
         {
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.ShowMessageAsync(title, message, style, settings));
+            return ExecuteShowMessageDialog(context, window => window.ShowMessageAsync(title, message, style, settings));
         }
 
         public Task<ProgressDialogController> ShowProgressAsync(object context, string title, string message,
             bool isCancelable = false, MetroDialogSettings settings = null)
         {
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.ShowProgressAsync(title, message, isCancelable, settings));
+            return ExecuteShowMessageDialog(context, window => window.ShowProgressAsync(title, message, isCancelable, settings));
         }
 
         public Task ShowMetroDialogAsync(object context, BaseMetroDialog dialog,
             MetroDialogSettings settings = null)
         {
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.ShowMetroDialogAsync(dialog, settings));
+            return ExecuteShowMessageDialog(context, window => window.ShowMetroDialogAsync(dialog, settings));
         }
 
         public Task HideMetroDialogAsync(object context, BaseMetroDialog dialog, MetroDialogSettings settings = null)
         {
-            var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.HideMetroDialogAsync(dialog, settings));
+            return ExecuteShowMessageDialog(context, window => window.HideMetroDialogAsync(dialog, settings));
         }
 
         public Task<TDialog> GetCurrentDialogAsync<TDialog>(object context) where TDialog : BaseMetroDialog
         {
+            return ExecuteShowMessageDialog(context, window => window.GetCurrentDialogAsync<TDialog>());
+        }
+
+        private static Task ExecuteShowMessageDialog(object context, Func<MetroWindow, Task> messageDialogFunc)
+        {
             var metroWindow = GetMetroWindow(context);
-
-            return InvokeIfRequired(metroWindow, window => window.GetCurrentDialogAsync<TDialog>());
+            if (metroWindow.CheckAccess())
+            {
+                return messageDialogFunc(metroWindow);
+            }
+            return metroWindow.Dispatcher.Invoke(new Func<Task>(() => messageDialogFunc(metroWindow))) as Task;
         }
 
-        private static Task InvokeIfRequired(MetroWindow window, Func<MetroWindow, Task> func)
+        private static Task<TResult> ExecuteShowMessageDialog<TResult>(object context, Func<MetroWindow, Task<TResult>> messageDialogFunc)
         {
-            if (window.CheckAccess())
+            var metroWindow = GetMetroWindow(context);
+            if (metroWindow.CheckAccess())
             {
-                return func(window);
+                return messageDialogFunc(metroWindow);
             }
-            return window.Dispatcher.Invoke(new Func<Task>(() => func(window))) as Task;
-        }
-
-        private static Task<TResult> InvokeIfRequired<TResult>(MetroWindow window, Func<MetroWindow, Task<TResult>> func)
-        {
-            if (window.CheckAccess())
-            {
-                return func(window);
-            }
-            return window.Dispatcher.Invoke(new Func<Task<TResult>>(() => func(window))) as Task<TResult>;
+            return metroWindow.Dispatcher.Invoke(new Func<Task<TResult>>(() => messageDialogFunc(metroWindow))) as Task<TResult>;
         }
 
         private static MetroWindow GetMetroWindow(object context)
         {
-            if (context == null) throw new ArgumentNullException("context");
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
             if (!DialogParticipation.IsRegistered(context))
             {
                 throw new InvalidOperationException("Context is not registered. Consider using DialogParticipation.Register in XAML to bind in the DataContext.");
