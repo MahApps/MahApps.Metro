cff62b12f44d643c7444e6d103dc14af24e31d43
Michael Mairegger <michael.mairegger@yahoo.de>
2016-05-30 10:22:58 +0200

    Fix issue where FloatingWatermark does not work when text is input by hand

---
+++ MahApps.Metro/Behaviours/DatePickerTextBoxBehavior.cs
@@ -0,0 +1,21 @@
+﻿namespace MahApps.Metro.Behaviours
+{
+    using System.Windows.Controls;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Interactivity;
+    using MahApps.Metro.Controls;
+
+    public class DatePickerTextBoxBehavior : Behavior<DatePickerTextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.TextChanged += OnTextChanged;
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            AssociatedObject.TemplatedParent.SetValue(TextBoxHelper.HasTextProperty, AssociatedObject.Text.Length > 0);
+        }
+    }
+}
