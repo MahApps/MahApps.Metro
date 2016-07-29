98fa2770b5a38ddcac9e1cc52bb4469f180c83d8
Jan Karger <punkerat76@gmail.com>
2016-07-29 16:42:23 +0200

    fix #2598 DatePickerTextBoxBehavior NullReferenceException

--- MahApps.Metro/Behaviours/DatePickerTextBoxBehavior.cs
+++ MahApps.Metro/Behaviours/DatePickerTextBoxBehavior.cs
@@ -3,6 +3,7 @@
     using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
     using System.Windows.Interactivity;
+    using System.Windows.Threading;
     using MahApps.Metro.Controls;
 
     public class DatePickerTextBoxBehavior : Behavior<DatePickerTextBox>
@@ -10,18 +11,24 @@
         protected override void OnAttached()
         {
             base.OnAttached();
-            AssociatedObject.TextChanged += OnTextChanged;
+            this.AssociatedObject.TextChanged += this.OnTextChanged;
+            this.BeginInvoke(this.SetHasTextProperty, DispatcherPriority.Loaded);
         }
 
         protected override void OnDetaching()
         {
+            this.AssociatedObject.TextChanged -= this.OnTextChanged;
             base.OnDetaching();
-            AssociatedObject.TextChanged -= OnTextChanged;
         }
 
         private void OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            AssociatedObject.TemplatedParent.SetValue(TextBoxHelper.HasTextProperty, AssociatedObject.Text.Length > 0);
+            this.SetHasTextProperty();
+        }
+
+        private void SetHasTextProperty()
+        {
+            this.AssociatedObject.TemplatedParent?.SetValue(TextBoxHelper.HasTextProperty, this.AssociatedObject.Text.Length > 0);
         }
     }
 }
