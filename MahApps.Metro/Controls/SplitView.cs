356105dc35df0ae360448ab5c71fa6d5078def23
Amaury Levé <evangelink@gmail.com>
2016-08-15 16:54:54 +0200

    Get rid of the hacky bit to use the proxy element.
    I wasn't aware of the FrameworkElement proxy to allow binding values of
    the animation so I was using x:Name I was retrieving from the control
    itself to set the binding.
    Also get rid of some of the reformatting.

--- MahApps.Metro/Controls/SplitView.cs
+++ MahApps.Metro/Controls/SplitView.cs
@@ -3,43 +3,17 @@
     using System;
     using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
     using System.Windows.Input;
     using System.Windows.Markup;
     using System.Windows.Media;
-    using System.Windows.Media.Animation;
     using System.Windows.Shapes;
-    using MahApps.Metro.Behaviours;
 
     /// <summary>
     ///     Represents a container with two views; one view for the main content and another view that is typically used for
     ///     navigation commands.
     /// </summary>
-    [TemplatePart(Name = "PART_PaneClipRectangle", Type = typeof(RectangleGeometry))]
-    [TemplatePart(Name = "PART_LightDismissLayer", Type = typeof(Rectangle))]
-    [TemplatePart(Name = "PART_ClosedToOpenOverlayLeftPaneTransformXAnimation                                 ", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedToOpenOverlayLeftPaneClipRectangleTransformXAnimation                    ", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedToOpenOverlayRightPaneTransformXAnimation                                ", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedToOpenOverlayRightPaneClipRectangleTransformXAnimation                   ", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactLeftToOpenCompactOverlayLeftColumnDefinition1WidthAnimation       ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactLeftToOpenCompactOverlayLeftPaneClipRectangleTransformXAnimation  ", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactRightToOpenCompactOverlayRightColumnDefinition2WidthAnimation     ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactRightToOpenCompactOverlayRightPaneClipRectangleTransformXAnimation", Type = typeof(DiscreteDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenOverlayLeftToClosedPanePaneTransformXAnimation                             ", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenOverlayLeftToClosedPaneClipRectangleTransformXAnimation                    ", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenOverlayRightToClosedPanePaneTransformXAnimation                            ", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenOverlayRightToClosedPaneClipRectangleTransformXAnimation                   ", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayRightToClosedCompactRightColumnDefinition2WidthAnimation     ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayLeftToClosedCompactLeftColumnDefinition1Width                ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayLeftToClosedCompactLeftPaneClipRectangleTransformXAnimation  ", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayRightToClosedCompactRightPaneClipRectangleTransformXAnimation", Type = typeof(SplineDoubleKeyFrame))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactLeftColumnDefinition1Width                                        ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactLeftPaneClipRectangleTransformXAnimation                          ", Type = typeof(DoubleAnimation))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactRightColumnDefinition2Width                                       ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_ClosedCompactRightPaneClipRectangleTransformXAnimation                         ", Type = typeof(DoubleAnimation))] // HACK
-    [TemplatePart(Name = "PART_OpenInlineRightColumnDefinition2Width                                          ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayLeftColumnDefinition1Width                                   ", Type = typeof(GridLengthAnimation))] // HACK
-    [TemplatePart(Name = "PART_OpenCompactOverlayRightColumnDefinition2Width                                  ", Type = typeof(GridLengthAnimation))] // HACK
+    [TemplatePart(Name = "PaneClipRectangle", Type = typeof(RectangleGeometry))]
+    [TemplatePart(Name = "LightDismissLayer", Type = typeof(Rectangle))]
     [TemplateVisualState(Name = "Closed                 ", GroupName = "DisplayModeStates")]
     [TemplateVisualState(Name = "ClosedCompactLeft      ", GroupName = "DisplayModeStates")]
     [TemplateVisualState(Name = "ClosedCompactRight     ", GroupName = "DisplayModeStates")]
@@ -279,11 +253,9 @@
         {
             base.OnApplyTemplate();
 
-            this.HackBindingAnimations(); // FIX ME: Remove this hack when it is possible to bind value of animations.
+            this.paneClipRectangle = this.GetTemplateChild("PaneClipRectangle") as RectangleGeometry;
 
-            this.paneClipRectangle = this.GetTemplateChild("PART_PaneClipRectangle") as RectangleGeometry;
-
-            this.lightDismissLayer = this.GetTemplateChild("PART_LightDismissLayer") as Rectangle;
+            this.lightDismissLayer = this.GetTemplateChild("LightDismissLayer") as Rectangle;
             if (this.lightDismissLayer != null)
             {
                 this.lightDismissLayer.MouseDown += this.OnLightDismiss;
@@ -354,184 +326,7 @@
             {
                 this.IsPaneOpen = false;
             }
-        }
-
-        /// <summary>
-        ///     It is currently impossible to bind the value of an animation but one workaround is to retrieve the element using
-        ///     its name and then applying the binding after the creation of the animation...
-        ///     This is the extract from the MSDN:
-        ///     You can't use dynamic resource references or data binding expressions to set Storyboard or animation property
-        ///     values.
-        ///     That's because everything inside a ControlTemplate must be thread-safe, and the timing system must Freeze
-        ///     Storyboard objects to make them thread-safe.
-        ///     A Storyboard cannot be frozen if it or its child timelines contain dynamic resource references or data binding
-        ///     expressions
-        ///     For more info:
-        ///     https://social.msdn.microsoft.com/Forums/vstudio/en-US/1b2bb84d-58c6-4dde-9c6c-00ed466e2382/using-binding-in-visualstate-storyboard?forum=wpf
-        /// </summary>
-        private void HackBindingAnimations()
-        {
-            var closedToOpenOverlayLeftPaneTransformXAnimation = this.GetTemplateChild("PART_ClosedToOpenOverlayLeftPaneTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedToOpenOverlayLeftPaneTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(closedToOpenOverlayLeftPaneTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedToOpenOverlayLeftPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedToOpenOverlayLeftPaneClipRectangleTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedToOpenOverlayLeftPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(closedToOpenOverlayLeftPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedToOpenOverlayRightPaneTransformXAnimation = this.GetTemplateChild("PART_ClosedToOpenOverlayRightPaneTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedToOpenOverlayRightPaneTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(closedToOpenOverlayRightPaneTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedToOpenOverlayRightPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedToOpenOverlayRightPaneClipRectangleTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedToOpenOverlayRightPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(closedToOpenOverlayRightPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedCompactLeftToOpenCompactOverlayLeftColumnDefinition1WidthAnimation = this.GetTemplateChild("PART_ClosedCompactLeftToOpenCompactOverlayLeftColumnDefinition1WidthAnimation") as GridLengthAnimation;
-            if (closedCompactLeftToOpenCompactOverlayLeftColumnDefinition1WidthAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactLeftToOpenCompactOverlayLeftColumnDefinition1WidthAnimation, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var closedCompactLeftToOpenCompactOverlayLeftPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedCompactLeftToOpenCompactOverlayLeftPaneClipRectangleTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedCompactLeftToOpenCompactOverlayLeftPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactLeftToOpenCompactOverlayLeftPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedCompactRightToOpenCompactOverlayRightColumnDefinition2WidthAnimation = this.GetTemplateChild("PART_ClosedCompactRightToOpenCompactOverlayRightColumnDefinition2WidthAnimation") as GridLengthAnimation;
-            if (closedCompactRightToOpenCompactOverlayRightColumnDefinition2WidthAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactRightToOpenCompactOverlayRightColumnDefinition2WidthAnimation, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var closedCompactRightToOpenCompactOverlayRightPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedCompactRightToOpenCompactOverlayRightPaneClipRectangleTransformXAnimation") as DiscreteDoubleKeyFrame;
-            if (closedCompactRightToOpenCompactOverlayRightPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactRightToOpenCompactOverlayRightPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openOverlayLeftToClosedPanePaneTransformXAnimation = this.GetTemplateChild("PART_OpenOverlayLeftToClosedPanePaneTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openOverlayLeftToClosedPanePaneTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(openOverlayLeftToClosedPanePaneTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openOverlayLeftToClosedPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_OpenOverlayLeftToClosedPaneClipRectangleTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openOverlayLeftToClosedPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(openOverlayLeftToClosedPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openOverlayRightToClosedPanePaneTransformXAnimation = this.GetTemplateChild("PART_OpenOverlayRightToClosedPanePaneTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openOverlayRightToClosedPanePaneTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(openOverlayRightToClosedPanePaneTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openOverlayRightToClosedPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_OpenOverlayRightToClosedPaneClipRectangleTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openOverlayRightToClosedPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLength") { Source = this };
-                BindingOperations.SetBinding(openOverlayRightToClosedPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openCompactOverlayRightToClosedCompactRightColumnDefinition2WidthAnimation = this.GetTemplateChild("PART_OpenCompactOverlayRightToClosedCompactRightColumnDefinition2WidthAnimation") as GridLengthAnimation;
-            if (openCompactOverlayRightToClosedCompactRightColumnDefinition2WidthAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayRightToClosedCompactRightColumnDefinition2WidthAnimation, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var openCompactOverlayLeftToClosedCompactLeftColumnDefinition1Width = this.GetTemplateChild("PART_OpenCompactOverlayLeftToClosedCompactLeftColumnDefinition1Width") as GridLengthAnimation;
-            if (openCompactOverlayLeftToClosedCompactLeftColumnDefinition1Width != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayLeftToClosedCompactLeftColumnDefinition1Width, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var openCompactOverlayLeftToClosedCompactLeftPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_OpenCompactOverlayLeftToClosedCompactLeftPaneClipRectangleTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openCompactOverlayLeftToClosedCompactLeftPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayLeftToClosedCompactLeftPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var openCompactOverlayRightToClosedCompactRightPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_OpenCompactOverlayRightToClosedCompactRightPaneClipRectangleTransformXAnimation") as SplineDoubleKeyFrame;
-            if (openCompactOverlayRightToClosedCompactRightPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayRightToClosedCompactRightPaneClipRectangleTransformXAnimation, DoubleKeyFrame.ValueProperty, binding);
-            }
-
-            var closedCompactLeftColumnDefinition1Width = this.GetTemplateChild("PART_ClosedCompactLeftColumnDefinition1Width") as GridLengthAnimation;
-            if (closedCompactLeftColumnDefinition1Width != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactLeftColumnDefinition1Width, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var closedCompactLeftPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedCompactLeftPaneClipRectangleTransformXAnimation") as DoubleAnimation;
-            if (closedCompactLeftPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.NegativeOpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactLeftPaneClipRectangleTransformXAnimation, DoubleAnimation.ToProperty, binding);
-            }
-
-            var closedCompactRightColumnDefinition2Width = this.GetTemplateChild("PART_ClosedCompactRightColumnDefinition2Width") as GridLengthAnimation;
-            if (closedCompactRightColumnDefinition2Width != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactRightColumnDefinition2Width, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var closedCompactRightPaneClipRectangleTransformXAnimation = this.GetTemplateChild("PART_ClosedCompactRightPaneClipRectangleTransformXAnimation") as DoubleAnimation;
-            if (closedCompactRightPaneClipRectangleTransformXAnimation != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneLengthMinusCompactLength") { Source = this };
-                BindingOperations.SetBinding(closedCompactRightPaneClipRectangleTransformXAnimation, DoubleAnimation.ToProperty, binding);
-            }
-
-            var openInlineRightColumnDefinition2Width = this.GetTemplateChild("PART_OpenInlineRightColumnDefinition2Width") as GridLengthAnimation;
-            if (openInlineRightColumnDefinition2Width != null)
-            {
-                var binding = new Binding("TemplateSettings.OpenPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(openInlineRightColumnDefinition2Width, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var openCompactOverlayLeftColumnDefinition1Width = this.GetTemplateChild("PART_OpenCompactOverlayLeftColumnDefinition1Width") as GridLengthAnimation;
-            if (openCompactOverlayLeftColumnDefinition1Width != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayLeftColumnDefinition1Width, GridLengthAnimation.ToProperty, binding);
-            }
-
-            var openCompactOverlayRightColumnDefinition2Width = this.GetTemplateChild("PART_OpenCompactOverlayRightColumnDefinition2Width") as GridLengthAnimation;
-            if (openCompactOverlayRightColumnDefinition2Width != null)
-            {
-                var binding = new Binding("TemplateSettings.CompactPaneGridLength") { Source = this };
-                BindingOperations.SetBinding(openCompactOverlayRightColumnDefinition2Width, GridLengthAnimation.ToProperty, binding);
-            }
-        }
+        }        
 
         private void OnLightDismiss(object sender, MouseButtonEventArgs e)
         {
