ff69fed4fc387d9e39145d8f908c19ab504147c0
Jan Karger <punkerat76@gmail.com>
2016-08-17 20:34:39 +0200

    Prepare DynamicFlyout for memory leak testing

--- samples/MetroDemo/ExampleWindows/DynamicFlyout.xaml.cs
+++ samples/MetroDemo/ExampleWindows/DynamicFlyout.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace MetroDemo.ExampleWindows {
     /// <summary>
@@ -18,7 +6,12 @@ namespace MetroDemo.ExampleWindows {
     /// </summary>
     public partial class DynamicFlyout {
         public DynamicFlyout() {
-            InitializeComponent();
+            this.InitializeComponent();
+        }
+
+        private void CloseFlyoutClick(object sender, RoutedEventArgs e)
+        {
+            this.IsOpen = false;
         }
     }
 }
