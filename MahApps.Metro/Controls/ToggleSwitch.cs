26c53bc467e7a9e27cda87613643111b530105b7
Jan Karger <jan.karger@inform-software.com>
2016-09-13 16:20:09 +0200

    Switch to MIT

--- MahApps.Metro/Controls/ToggleSwitch.cs
+++ MahApps.Metro/Controls/ToggleSwitch.cs
@@ -1,9 +1,4 @@
-﻿// (c) Copyright Microsoft Corporation.
-// This source is subject to the Microsoft Public License (Ms-PL).
-// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
-// All other rights reserved.
-
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Globalization;
 using System.Windows;
