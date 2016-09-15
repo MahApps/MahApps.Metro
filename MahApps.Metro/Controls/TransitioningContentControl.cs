1c7290bf272c75b5f27f9037feec458c62468f08
Jan Karger <jan.karger@inform-software.com>
2016-09-15 09:40:39 +0200

    Revert deleting Ms-PL notice
      Using the code is fine, but not removing the license notice for a bunch of stuff without getting MSFT, Tim Heuer and Avalon OK

--- MahApps.Metro/Controls/TransitioningContentControl.cs
+++ MahApps.Metro/Controls/TransitioningContentControl.cs
@@ -1,3 +1,8 @@
+// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
