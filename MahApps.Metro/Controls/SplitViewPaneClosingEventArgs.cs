2e3f8ce33850fc2d70d94a26cd5d5a545b791588
Amaury Levé <evangelink@gmail.com>
2016-09-20 15:33:33 +0200

    Adds the SplitView control.
    A first implementation of the SplitView control.

---
+++ MahApps.Metro/Controls/SplitViewPaneClosingEventArgs.cs
@@ -0,0 +1,17 @@
+﻿namespace MahApps.Metro.Controls
+{
+    /// <summary>
+    ///     Provides event data for the <see cref="SplitView.PaneClosing" /> event.
+    /// </summary>
+    public sealed class SplitViewPaneClosingEventArgs
+#if NET4
+        : System.EventArgs
+#endif
+    {
+        /// <summary>
+        ///     Gets or sets a value that indicates whether the pane closing action should be canceled.
+        /// </summary>
+        /// <returns>true to cancel the pane closing action; otherwise, false.</returns>
+        public bool Cancel { get; set; }
+    }
+}
---
+++ samples/MetroDemo/ExampleViews/SplitViewExamples.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace MetroDemo.ExampleViews
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using MahApps.Metro.Controls;
+
+    /// <summary>
+    ///     Interaction logic for SplitViewExamples.xaml
+    /// </summary>
+    public partial class SplitViewExamples
+    {
+        public SplitViewExamples()
+        {
+            this.InitializeComponent();
+
+            this.SimpleSplitview.Tag = false;
+            this.ShellSplitView.Tag = false;
+            this.HomeRadioButton.IsChecked = true;
+        }
+
+        private void AboutRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.ShellSplitViewContentText != null)
+                this.ShellSplitViewContentText.Text = "About page";
+        }
+
+        private void HomeRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.ShellSplitViewContentText != null)
+                this.ShellSplitViewContentText.Text = "Home page";
+        }
+
+        private void SettingsRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.ShellSplitViewContentText != null)
+                this.ShellSplitViewContentText.Text = "Settings page";
+        }
+
+        private void DontCheck(object sender, RoutedEventArgs e)
+        {
+            // don't let the radiobutton check
+            var radioButton = sender as RadioButton;
+            if (radioButton != null)
+                radioButton.IsChecked = false;
+        }
+
+        private void Splitview_PaneClosing(object sender, SplitViewPaneClosingEventArgs e)
+        {
+            var splitView = sender as SplitView;
+
+            if (splitView == null)
+                return;
+
+            e.Cancel = (bool)splitView.Tag;
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (this.ShellSplitView == null)
+                return;
+
+            this.ShellSplitView.IsPaneOpen = !this.ShellSplitView.IsPaneOpen;
+            DontCheck(sender, e);
+        }
+    }
+}
