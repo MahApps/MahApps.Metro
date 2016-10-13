2fed9c9d91695903d0548fc4dffb7f1af3a1a917
Jan Karger <punkerat76@gmail.com>
2016-10-14 00:27:11 +0200

    Restructure repo (#2701)
    * Restructure solution and projects (tests not yet finished)
    
    * Move Caliburn theme to shared project
    
    * Move Caliburn App.xaml to shared project
    
    * Move Metro.Demo App.xaml to shared project
    
    * Add MahApps.Metro.Tests
    
    * paket auto-restore on
    
    * Update packages
    
    * Move all to src
    
    * Unload Mahapps.Metro.Tests.NET45 (doesn't works)
    
    * Fix output and nuspec
    
    * Remove unnecessary usings

---
+++ src/MahApps.Metro.Samples/MahApps.Metro.Caliburn.Demo/MahApps.Metro.Caliburn.Demo.Shared/ViewModels/Flyouts/Flyout3ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using MetroDemo.Models;
+
+namespace Caliburn.Metro.Demo.ViewModels.Flyouts
+{
+    public class Flyout3ViewModel : FlyoutBaseViewModel
+    {
+        private readonly IObservableCollection<Artist> artists =
+            new BindableCollection<Artist>();
+
+        public IObservableCollection<Artist> Artists
+        {
+            get
+            {
+                return this.artists;
+            }
+        }
+
+        public Flyout3ViewModel()
+        {
+            SampleData.Seed();
+            this.Artists.AddRange(SampleData.Artists);
+            this.Header = "third";
+            this.Position = Position.Right;
+        }
+    }
+}
