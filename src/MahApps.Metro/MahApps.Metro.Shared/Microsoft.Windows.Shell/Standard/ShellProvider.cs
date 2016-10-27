b49de02d93d02bb4b4b6edb6cc754d8b3176b926
Danghor <danghor+github.com@gmail.com>
2016-10-27 23:17:41 +0200

    Remove unnecessary usings

--- src/MahApps.Metro/MahApps.Metro.Shared/Microsoft.Windows.Shell/Standard/ShellProvider.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Microsoft.Windows.Shell/Standard/ShellProvider.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
-    using System.Runtime.CompilerServices;
     using System.Runtime.InteropServices;
     using System.Runtime.InteropServices.ComTypes;
     using System.Text;
