0e7aa0d37ae5899e6d7e81ae821efde10db9b407
Deadpikle <mikebobby@sbcglobal.net>
2016-11-01 10:47:40 -0400

    Make sure this.handle is valid before setting property on windowChrome

--- src/MahApps.Metro/MahApps.Metro.Shared/Behaviours/BorderlessWindowBehavior.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Behaviours/BorderlessWindowBehavior.cs
@@ -375,7 +375,10 @@ namespace MahApps.Metro.Behaviours
                 {
                     // Can't get monitor info, so just remove all border thickness
                     this.AssociatedObject.BorderThickness = new Thickness(0);
-                    this.windowChrome.ResizeBorderThickness = new Thickness(0);
+                    if (this.handle != IntPtr.Zero)
+                    {
+                        this.windowChrome.ResizeBorderThickness = new Thickness(0);
+                    }
                 }
             }
             else
