0c1d02f864df98166f70f4ef1132ba8b31c53cb5
Michael Mairegger <michael.mairegger@yahoo.de>
2017-02-03 20:02:56 +0100

    Make Popup private

--- src/MahApps.Metro/MahApps.Metro.Shared/Controls/TimePicker/TimePickerBase.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/TimePicker/TimePickerBase.cs
@@ -168,7 +168,7 @@ namespace MahApps.Metro.Controls
         private Selector _hourInput;
         private UIElement _minuteHand;
         private Selector _minuteInput;
-        protected Popup _popup;
+        private Popup _popup;
         private UIElement _secondHand;
         private Selector _secondInput;
         protected DatePickerTextBox _textBox;
