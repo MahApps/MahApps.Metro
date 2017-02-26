0bed25d4ced993cc957f5237bb623a7ec94b64ba
Jan Karger <jan.karger@inform-software.com>
2017-02-26 15:06:16 +0100

    Add Badged control

---
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/Badged.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using ControlzEx;
+
+namespace MaterialDesignThemes.Wpf
+{
+    [TemplatePart(Name = BadgeContainerPartName, Type = typeof(UIElement))]
+    public class Badged : BadgedEx
+    {
+        static Badged()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Badged), new FrameworkPropertyMetadata(typeof(Badged)));
+        }
+    }
+}
