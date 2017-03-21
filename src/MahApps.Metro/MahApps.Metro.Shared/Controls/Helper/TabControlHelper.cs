05f9e22ccb504f4abdd5bad2d18181e9003236b0
Jan Karger <jan.karger@inform-software.com>
2017-03-21 16:28:07 +0100

    Add new Underlined attached property   New property to control the type of the underline which can be TabItem or TabPanel (with TabItem)   Add Obsolete tag to IsUnderlined property.

--- src/MahApps.Metro/MahApps.Metro.Shared/Controls/Helper/TabControlHelper.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/Helper/TabControlHelper.cs
@@ -1,36 +1,83 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace MahApps.Metro.Controls
 {
-    using System.ComponentModel;
+    /// <summary>
+    /// Specifies the underline position of a TabControl.
+    /// </summary>
+    public enum UnderlinedType
+    {
+        None,
+        TabItem,
+        TabPanel
+    }
 
     public static class TabControlHelper
     {
         /// <summary>
-        /// Defines whether the underline below the <see cref="TabControl"/> is shown or not.
+        /// Defines whether the underline below the <see cref="TabItem"/> is shown or not.
         /// </summary>
+        [Obsolete(@"This property will be deleted in the next release. You should now use the Underlined attached property.")]
         public static readonly DependencyProperty IsUnderlinedProperty =
-            DependencyProperty.RegisterAttached("IsUnderlined", typeof(bool), typeof(TabControlHelper), new PropertyMetadata(false));
+            DependencyProperty.RegisterAttached("IsUnderlined",
+                                                typeof(bool),
+                                                typeof(TabControlHelper),
+                                                new PropertyMetadata(false,
+                                                                     (o, e) =>
+                                                                         {
+                                                                             var element = o as UIElement;
+                                                                             if (element != null && e.OldValue != e.NewValue && e.NewValue is bool)
+                                                                             {
+                                                                                 TabControlHelper.SetUnderlined(element, (bool)e.NewValue ? UnderlinedType.TabItem : UnderlinedType.None);
+                                                                             }
+                                                                         }));
 
         [Category(AppName.MahApps)]
         [AttachedPropertyBrowsableForType(typeof(TabControl))]
-        [AttachedPropertyBrowsableForType(typeof(TabItem))]
+        [Obsolete(@"This property will be deleted in the next release. You should now use the Underlined attached property.")]
         public static bool GetIsUnderlined(UIElement element)
         {
             return (bool)element.GetValue(IsUnderlinedProperty);
         }
 
+        [Obsolete(@"This property will be deleted in the next release. You should now use the Underlined attached property.")]
         public static void SetIsUnderlined(UIElement element, bool value)
         {
             element.SetValue(IsUnderlinedProperty, value);
         }
 
         /// <summary>
+        /// Defines whether the underline below the <see cref="TabItem"/> or <see cref="TabPanel"/> is shown or not.
+        /// </summary>
+        public static readonly DependencyProperty UnderlinedProperty =
+            DependencyProperty.RegisterAttached("Underlined",
+                                                typeof(UnderlinedType),
+                                                typeof(TabControlHelper),
+                                                new PropertyMetadata(UnderlinedType.None));
+
+        [Category(AppName.MahApps)]
+        [AttachedPropertyBrowsableForType(typeof(TabControl))]
+        public static UnderlinedType GetUnderlined(UIElement element)
+        {
+            return (UnderlinedType)element.GetValue(UnderlinedProperty);
+        }
+
+        public static void SetUnderlined(UIElement element, UnderlinedType value)
+        {
+            element.SetValue(UnderlinedProperty, value);
+        }
+
+        /// <summary>
         /// This property can be used to set the Transition for animated TabControls
         /// </summary>
         public static readonly DependencyProperty TransitionProperty =
-            DependencyProperty.RegisterAttached("Transition", typeof(TransitionType), typeof(TabControlHelper),
+            DependencyProperty.RegisterAttached("Transition",
+                                                typeof(TransitionType),
+                                                typeof(TabControlHelper),
                                                 new FrameworkPropertyMetadata(TransitionType.Default, FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.Inherits));
 
         [Category(AppName.MahApps)]
