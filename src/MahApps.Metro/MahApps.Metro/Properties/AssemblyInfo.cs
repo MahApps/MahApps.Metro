040ba4ea0129d34e1832c2e81422138d77e08cb1
Jan Karger <jan.karger@inform-software.com>
2017-04-13 21:14:56 +0200

    Set v1.5.0.23   and update NuGet.exe

--- src/MahApps.Metro/MahApps.Metro/Properties/AssemblyInfo.cs
+++ src/MahApps.Metro/MahApps.Metro/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows.Markup;
 [assembly: AssemblyDescription("A toolkit for creating Metro / Modern UI styled WPF apps.")]
 [assembly: AssemblyCompany("MahApps")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: AssemblyInformationalVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.23")]
+[assembly: AssemblyFileVersion("1.5.0.23")]
+[assembly: AssemblyInformationalVersion("1.5.0.23")]
 [assembly: AssemblyProduct("MahApps.Metro 1.5.0")]
