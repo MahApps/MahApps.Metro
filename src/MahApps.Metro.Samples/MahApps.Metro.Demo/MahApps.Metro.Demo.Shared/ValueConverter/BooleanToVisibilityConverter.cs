5cd062fa732170a8fd3bfc320ed0b972c08ade8f
Alex Kuchta <amkuchta@gmail.com>
2017-04-27 23:06:03 -0400

    Updates ComboBox, DatePicker, PasswordBox to use watermark enhancements

---
+++ src/MahApps.Metro.Samples/MahApps.Metro.Demo/MahApps.Metro.Demo.Shared/ValueConverter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MetroDemo.ValueConverter
+{
+    class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() != string.Empty)
+            {
+                bool isChecked = System.Convert.ToBoolean(value);
+                if (isChecked)
+                { return "Visible"; }
+                else
+                { return "Collapsed"; }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
