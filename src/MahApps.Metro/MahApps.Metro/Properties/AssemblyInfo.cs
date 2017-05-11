1b5cfbfaaa5ba5c0a76f1a9c78775799e0a84874
Zach Saunders <Zach.Saunders@brewers.com>
2017-05-11 13:11:52 -0500

    Added ComboDataGrid Control

--- src/MahApps.Metro/MahApps.Metro/Properties/AssemblyInfo.cs
+++ src/MahApps.Metro/MahApps.Metro/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Windows.Markup;
 [assembly: AssemblyDescription("A toolkit for creating Metro / Modern UI styled WPF apps.")]
 [assembly: AssemblyCompany("MahApps")]
 
-[assembly: AssemblyVersion("1.4.0.42")]
-[assembly: AssemblyFileVersion("1.4.0.42")]
-[assembly: AssemblyInformationalVersion("1.4.0.42")]
+[assembly: AssemblyVersion("1.4.2017.65")]
+[assembly: AssemblyFileVersion("1.4.2017.65")]
+[assembly: AssemblyInformationalVersion("1.4.2017.64")]
 [assembly: AssemblyProduct("MahApps.Metro 1.4.0")]
