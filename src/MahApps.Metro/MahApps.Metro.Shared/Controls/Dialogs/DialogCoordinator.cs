fe9d8df0de38a6649f36ce3d89d3fb0f4588636b
Alexandre Murari Junior <murarialex@gmail.com>
2017-05-20 17:55:01 -0300

    Added event delegates in the dialog coordinator class and interface, to be invoked before the show dialog methods.

--- src/MahApps.Metro/MahApps.Metro.Shared/Controls/Dialogs/DialogCoordinator.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/Dialogs/DialogCoordinator.cs
@@ -13,54 +13,72 @@ namespace MahApps.Metro.Controls.Dialogs
 
         public Task<string> ShowInputAsync(object context, string title, string message, MetroDialogSettings settings = null)
         {
+            this.BeforeShowInputAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.ShowInputAsync(title, message, settings));
         }
 
         public string ShowModalInputExternal(object context, string title, string message, MetroDialogSettings metroDialogSettings = null)
         {
+            this.BeforeShowModalInputExternal?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.ShowModalInputExternal(title, message, metroDialogSettings);
         }
 
         public Task<LoginDialogData> ShowLoginAsync(object context, string title, string message, LoginDialogSettings settings = null)
         {
+            this.BeforeShowLoginAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.ShowLoginAsync(title, message, settings));
         }
 
         public LoginDialogData ShowModalLoginExternal(object context, string title, string message, LoginDialogSettings settings = null)
         {
+            this.BeforeShowModalLoginExternal?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.ShowModalLoginExternal(title, message, settings);
         }
 
         public Task<MessageDialogResult> ShowMessageAsync(object context, string title, string message, MessageDialogStyle style = MessageDialogStyle.Affirmative, MetroDialogSettings settings = null)
         {
+            this.BeforeShowMessageAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.ShowMessageAsync(title, message, style, settings));
         }
 
         public MessageDialogResult ShowModalMessageExternal(object context, string title, string message, MessageDialogStyle style = MessageDialogStyle.Affirmative, MetroDialogSettings settings = null)
         {
+            this.BeforeShowModalMessageExternal?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.ShowModalMessageExternal(title, message, style, settings);
         }
 
         public Task<ProgressDialogController> ShowProgressAsync(object context, string title, string message, bool isCancelable = false, MetroDialogSettings settings = null)
         {
+            this.BeforeShowProgressAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.ShowProgressAsync(title, message, isCancelable, settings));
         }
 
         public Task ShowMetroDialogAsync(object context, BaseMetroDialog dialog, MetroDialogSettings settings = null)
         {
+            this.BeforeShowMetroDialogAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.ShowMetroDialogAsync(dialog, settings));
         }
 
         public Task HideMetroDialogAsync(object context, BaseMetroDialog dialog, MetroDialogSettings settings = null)
         {
+            this.BeforeHideMetroDialogAsync?.Invoke(this, EventArgs.Empty);
+
             var metroWindow = GetMetroWindow(context);
             return metroWindow.Invoke(() => metroWindow.HideMetroDialogAsync(dialog, settings));
         }
@@ -90,5 +108,23 @@ namespace MahApps.Metro.Controls.Dialogs
             }
             return metroWindow;
         }
+
+        public EventHandler BeforeShowInputAsync { get; set; }
+
+        public EventHandler BeforeShowModalInputExternal { get; set; }
+
+        public EventHandler BeforeShowLoginAsync { get; set; }
+
+        public EventHandler BeforeShowModalLoginExternal { get; set; }
+
+        public EventHandler BeforeShowMessageAsync { get; set; }
+
+        public EventHandler BeforeShowModalMessageExternal { get; set; }
+
+        public EventHandler BeforeShowProgressAsync { get; set; }
+
+        public EventHandler BeforeShowMetroDialogAsync { get; set; }
+
+        public EventHandler BeforeHideMetroDialogAsync { get; set; }
     }
 }
