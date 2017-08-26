6943cc92f6490d29ac39a0f0e73cab5b493e378a
punker76 <punkerat76@gmail.com>
2017-08-26 21:39:11 +0200

    use PropertyChangeNotifier from ControlzEx

--- src/MahApps.Metro/MahApps.Metro.Shared/Controls/GlowWindow.xaml.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/GlowWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Interop;
 using System.Windows.Media.Animation;
+using ControlzEx;
 using ControlzEx.Standard;
 using ControlzEx.Native;
 
