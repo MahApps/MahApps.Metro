9ceb910eb5ed01b8644eee76b4f808731a404d84
Jan Karger <punkerat76@gmail.com>
2017-10-11 19:49:25 +0200

    Derives now directly from GroupBox Add own AutomationPeer class Use GroupBoxHelper.HeaderForeground with Foreground as default

--- src/MahApps.Metro/MahApps.Metro.Shared/Controls/MetroHeaderControl.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/MetroHeaderControl.cs
@@ -1,13 +1,35 @@
 ﻿using System.Windows;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 
 namespace MahApps.Metro.Controls
 {
-    public class MetroHeaderControl : HeaderedContentControl
+    public class MetroHeaderControl : GroupBox
     {
         static MetroHeaderControl()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroHeaderControl), new FrameworkPropertyMetadata(typeof(MetroHeaderControl)));
+        }
+
+        /// <summary>
+        /// Creates AutomationPeer (<see cref="M:System.Windows.UIElement.OnCreateAutomationPeer" />)
+        /// </summary>
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new MetroHeaderControlAutomationPeer(this);
+        }
+    }
+
+    public class MetroHeaderControlAutomationPeer : GroupBoxAutomationPeer
+    {
+        public MetroHeaderControlAutomationPeer(GroupBox owner)
+            : base(owner)
+        {
+        }
+
+        protected override string GetClassNameCore()
+        {
+            return "MetroHeaderControl";
         }
     }
 }
