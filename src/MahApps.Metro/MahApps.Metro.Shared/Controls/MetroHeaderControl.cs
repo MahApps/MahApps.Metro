0e2421745ef6e7889eba8f03550b25be1ddcb7c0
Jan Karger <punkerat76@gmail.com>
2017-10-11 16:16:47 +0200

    Add new MetroHeaderControl control   which can contains any other content and can display a header on top of this content.

---
+++ src/MahApps.Metro/MahApps.Metro.Shared/Controls/MetroHeaderControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public class MetroHeaderControl : HeaderedContentControl
+    {
+        static MetroHeaderControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroHeaderControl), new FrameworkPropertyMetadata(typeof(MetroHeaderControl)));
+        }
+    }
+}
