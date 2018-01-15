5b6bcdc0ec7be9e7f6626876b80ab24b59d46410
Jan Karger <punkerat76@gmail.com>
2018-01-15 16:43:54 +0100

    - Remove internal close command and use the CloseTabItemAction - Clear style and template for TabItem to prevent nasty binding expression errors - Allow TabControlHelper dependency properties for TabItem too (brushes and Underlined property)

--- src/MahApps.Metro/MahApps.Metro.Shared/Actions/CloseTabItemAction.cs
+++ src/MahApps.Metro/MahApps.Metro.Shared/Actions/CloseTabItemAction.cs
@@ -117,35 +117,47 @@ namespace MahApps.Metro.Actions
                 }
             }
 
-            var closeAction =
-                new Action(
-                    () =>
-                        {
-                            // TODO Raise a closing event to cancel this action
+            if (tabControl is MetroTabControl && tabItem is MetroTabItem)
+            {
+                // run the command handler for the TabControl
+                // see #555
+                tabControl.BeginInvoke(() => ((MetroTabControl)tabControl).CloseThisTabItem((MetroTabItem)tabItem));
+            }
+            else
+            {
+                var closeAction =
+                    new Action(
+                        () =>
+                            {
+                                // TODO Raise a closing event to cancel this action
 
-                            if (tabControl.ItemsSource == null)
-                            {
-                                // if the list is hard-coded (i.e. has no ItemsSource)
-                                // then we remove the item from the collection
-                                tabControl.Items.Remove(tabItem);
-                            }
-                            else
-                            {
-                                // if ItemsSource is something we cannot work with, bail out
-                                var collection = tabControl.ItemsSource as IList;
-                                if (collection == null)
+                                if (tabControl.ItemsSource == null)
                                 {
-                                    return;
+                                    // if the list is hard-coded (i.e. has no ItemsSource)
+                                    // then we remove the item from the collection
+                                    tabItem.ClearStyle();
+                                    tabControl.Items.Remove(tabItem);
                                 }
-                                // find the item and kill it (I mean, remove it)
-                                var item2Remove = collection.OfType<object>().FirstOrDefault(item => tabItem == item || tabItem.DataContext == item);
-                                if (item2Remove != null)
+                                else
                                 {
-                                    collection.Remove(item2Remove);
+                                    // if ItemsSource is something we cannot work with, bail out
+                                    var collection = tabControl.ItemsSource as IList;
+                                    if (collection == null)
+                                    {
+                                        return;
+                                    }
+
+                                    // find the item and kill it (I mean, remove it)
+                                    var item2Remove = collection.OfType<object>().FirstOrDefault(item => tabItem == item || tabItem.DataContext == item);
+                                    if (item2Remove != null)
+                                    {
+                                        tabItem.ClearStyle();
+                                        collection.Remove(item2Remove);
+                                    }
                                 }
-                            }
-                        });
-            this.BeginInvoke(closeAction);
+                            });
+                this.BeginInvoke(closeAction);
+            }
         }
 
         private static void OnCommandChanged(CloseTabItemAction action, DependencyPropertyChangedEventArgs e)
@@ -180,6 +192,7 @@ namespace MahApps.Metro.Actions
             {
                 return;
             }
+
             var command = this.Command;
             this.AssociatedObject.IsEnabled = command == null || command.CanExecute(this.GetCommandParameter());
         }
