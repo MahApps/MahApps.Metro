4394a03850300b846811dd0819fb49e4490a3741
Jan Karger <punkerat76@gmail.com>
2018-02-07 22:07:50 +0100

    ModernUI first ones: - ModernUI resources style dictionary - ModernUI.Light and ModernUI.Dark themes - Styles for Button, CheckBox, RadioButton, ScrollBar, ScrollViewer, TextBlock (Header)

---
+++ src/MahApps.Metro.Samples/MahApps.Metro.Demo/MahApps.Metro.Demo.Shared/ExampleWindows/ModernUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MahApps.Metro;
+using MahApps.Metro.Controls;
+
+namespace MetroDemo.ExampleWindows
+{
+    public partial class ModernUI : MetroWindow
+    {
+        public ModernUI()
+        {
+            this.InitializeComponent();
+
+            ThemeManager.AddAppTheme("ModernUI.Light", new Uri("pack://application:,,,/MahApps.Metro;component/Styles/ModernUI/ModernUI.Light.xaml", UriKind.RelativeOrAbsolute));
+            ThemeManager.AddAppTheme("ModernUI.Dark", new Uri("pack://application:,,,/MahApps.Metro;component/Styles/ModernUI/ModernUI.Dark.xaml", UriKind.RelativeOrAbsolute));
+            ThemeManager.IsThemeChanged += ThemeManager_IsThemeChanged;
+        }
+
+        private void ThemeManager_IsThemeChanged(object sender, OnThemeChangedEventArgs e)
+        {
+            ThemeManager.IsThemeChanged -= ThemeManager_IsThemeChanged;
+
+            var newTheme = e.AppTheme.Name.Contains("Light") ? ThemeManager.GetAppTheme("ModernUI.Light") : ThemeManager.GetAppTheme("ModernUI.Dark");
+            ThemeManager.ChangeAppStyle(this, e.Accent, newTheme);
+
+            ThemeManager.IsThemeChanged += ThemeManager_IsThemeChanged;
+        }
+    }
+}
