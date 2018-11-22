2a8a0d0f44ecc8c7e13b66df37caab9d6b4d8c04
Jan Karger <punkerat76@gmail.com>
2018-11-23 00:12:37 +0100

    The project-system has still some issues like broken Intellisense and not connected Xaml files.

--- src/MahApps.Metro/Controls/RevealImage.xaml.cs
+++ src/MahApps.Metro/Controls/RevealImage.xaml.cs
@@ -7,50 +7,48 @@ using System.Windows.Media.Animation;
 
 namespace MahApps.Metro.Controls
 {
-    public partial class RevealImage
+    public partial class RevealImage : UserControl
     {
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(RevealImage), new UIPropertyMetadata(""));
         public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(RevealImage), new UIPropertyMetadata(null));
 
         public string Text
         {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
+            get { return (string)this.GetValue(TextProperty); }
+            set { this.SetValue(TextProperty, value); }
         }
 
         public ImageSource Image
         {
-            get { return (ImageSource)GetValue(ImageProperty); }
-            set { SetValue(ImageProperty, value); }
+            get { return (ImageSource)this.GetValue(ImageProperty); }
+            set { this.SetValue(ImageProperty, value); }
         }
-
-        
 
         public RevealImage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
         private static void TypewriteTextblock(string textToAnimate, TextBlock txt, TimeSpan timeSpan)
         {
             var story = new Storyboard
-            {
-                FillBehavior = FillBehavior.HoldEnd
-            };
+                        {
+                            FillBehavior = FillBehavior.HoldEnd
+                        };
 
             DiscreteStringKeyFrame discreteStringKeyFrame;
             var stringAnimationUsingKeyFrames = new StringAnimationUsingKeyFrames
-            {
-                Duration = new Duration(timeSpan)
-            };
+                                                {
+                                                    Duration = new Duration(timeSpan)
+                                                };
 
             var tmp = string.Empty;
             foreach (var c in textToAnimate)
             {
                 discreteStringKeyFrame = new DiscreteStringKeyFrame
-                {
-                    KeyTime = KeyTime.Paced
-                };
+                                         {
+                                             KeyTime = KeyTime.Paced
+                                         };
                 tmp += c;
                 discreteStringKeyFrame.Value = tmp;
                 stringAnimationUsingKeyFrames.KeyFrames.Add(discreteStringKeyFrame);
@@ -64,8 +62,7 @@ namespace MahApps.Metro.Controls
 
         private void GridMouseEnter(object sender, MouseEventArgs e)
         {
-
-            TypewriteTextblock(Text.ToUpper(), textBlock, TimeSpan.FromSeconds(.25));
+            TypewriteTextblock(this.Text.ToUpper(), this.textBlock, TimeSpan.FromSeconds(.25));
         }
     }
 }
