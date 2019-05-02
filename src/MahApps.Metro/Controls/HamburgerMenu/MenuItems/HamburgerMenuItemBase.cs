00e1a64d903bcfea6418d946f0dd60a784b7a47d
punker76 <punkerat76@gmail.com>
2019-05-02 23:18:02 +0200

    (GH-3434) Add new HamburgerMenuSeparatorItem for HamburgerMenu

---
+++ src/MahApps.Metro/Controls/HamburgerMenu/MenuItems/HamburgerMenuItemBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace MahApps.Metro.Controls
+{
+    public class HamburgerMenuItemBase : Freezable
+    {
+        /// <summary>
+        /// Identifies the <see cref="Tag"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TagProperty = DependencyProperty.Register(nameof(Tag), typeof(object), typeof(HamburgerMenuItemBase), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets a value that specifies an user specific value.
+        /// </summary>
+        public object Tag
+        {
+            get
+            {
+                return this.GetValue(TagProperty);
+            }
+
+            set
+            {
+                this.SetValue(TagProperty, value);
+            }
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new HamburgerMenuItemBase();
+        }
+    }
+}
---
+++ src/MahApps.Metro/Controls/HamburgerMenu/MenuItems/HamburgerMenuItemStyleSelector.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Metro.Controls
+{
+    public class HamburgerMenuItemStyleSelector : StyleSelector
+    {
+        public bool IsItemOptions { get; set; }
+
+        /// <inheritdoc />
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            if (container != null)
+            {
+                var listBox = ItemsControl.ItemsControlFromItemContainer(container);
+                var hamburgerMenu = listBox?.TryFindParent<HamburgerMenu>();
+                if (hamburgerMenu != null)
+                {
+                    if (item is HamburgerMenuSeparatorItem)
+                    {
+                        if (hamburgerMenu.SeparatorItemContainerStyle != null)
+                        {
+                            return hamburgerMenu.SeparatorItemContainerStyle;
+                        }
+                    }
+                    else if (item is HamburgerMenuItem)
+                    {
+                        var itemContainerStyle = this.IsItemOptions ? hamburgerMenu.ItemContainerStyle : hamburgerMenu.OptionsItemContainerStyle;
+                        if (itemContainerStyle != null)
+                        {
+                            return itemContainerStyle;
+                        }
+                    }
+                }
+            }
+
+            return base.SelectStyle(item, container);
+        }
+    }
+}
