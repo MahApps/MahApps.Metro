7659963ecf1934aa59513747491dced6e45860a3
punker76 <punkerat76@gmail.com>
2019-05-28 23:16:02 +0200

    Move OverlayBehavior to it's own file

--- src/MahApps.Metro/Controls/WindowCommandsOverlayBehavior.cs
+++ src/MahApps.Metro/Controls/WindowCommandsOverlayBehavior.cs
@@ -15,25 +15,4 @@ namespace MahApps.Metro.Controls
         /// </summary>
         HiddenTitleBar = 1 << 0
     }
-
-    [Flags]
-    public enum OverlayBehavior
-    {
-        /// <summary>
-        /// Doesn't overlay Flyouts nor a hidden TitleBar.
-        /// </summary>
-        Never = 0,
-
-        /// <summary>
-        /// Overlays opened <see cref="Flyout"/> controls.
-        /// </summary>
-        Flyouts = 1 << 0,
-
-        /// <summary>
-        /// Overlays a hidden TitleBar.
-        /// </summary>
-        HiddenTitleBar = 1 << 1,
-
-        Always = ~(-1 << 2)
-    }
 }
