21305dd2502d93da4384a11c431a3088bcd53156
Evangelink <evangelink@gmail.com>
2019-06-06 14:22:36 +0200

    Improve demo experience

---
+++ src/MahApps.Metro.Samples/MahApps.Metro.Demo/ValueConverter/PlacementToOppositePlacementConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MetroDemo.ValueConverter
+{
+    internal class PlacementToOppositePlacementConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value as Dock?)
+            {
+                case Dock.Bottom:
+                    return Dock.Top;
+
+                case Dock.Left:
+                    return Dock.Right;
+
+                case Dock.Right:
+                    return Dock.Left;
+
+                case Dock.Top:
+                default:
+                    return Dock.Bottom;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
