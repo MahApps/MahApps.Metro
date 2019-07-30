2312e54e4f6f9b37ed120a588e54d139a0521d0b
timunie <47110241+timunie@users.noreply.github.com>
2019-07-30 16:19:06 +0200

    Initial Code
    - Implemented ColorItem-Class with Basic RGB -> HSV conversation
    - Started with Implementing a Color Canwas

---
+++ src/MahApps.Metro/Controls/ColorPicker/ColorCanvas.cs
@@ -0,0 +1,27 @@
+﻿using MahApps.Metro.Controls.ColorPicker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    public class ColorCanvas: Control
+    {
+
+        public static readonly DependencyProperty SelectedColorItemProperty = DependencyProperty.Register("SelectedColorItem", typeof(ColorPickerItem), typeof(ColorCanvas), new PropertyMetadata(new ColorPickerItem(Colors.GreenYellow)));
+            
+        public ColorPickerItem SelectedColorItem
+        {
+            get { return (ColorPickerItem)GetValue(SelectedColorItemProperty); }
+            set { SetValue(SelectedColorItemProperty, value); }
+        }
+
+        public Color SelectedColor => SelectedColorItem.RGB;
+
+    }
+}
