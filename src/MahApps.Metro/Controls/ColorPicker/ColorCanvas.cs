2eafde2afed8c2563c330077875fd1467a903294
timunie <47110241+timunie@users.noreply.github.com>
2019-07-31 16:36:04 +0200

    Added some DP to ColorCanvas

--- src/MahApps.Metro/Controls/ColorPicker/ColorCanvas.cs
+++ src/MahApps.Metro/Controls/ColorPicker/ColorCanvas.cs
@@ -13,15 +13,92 @@ namespace MahApps.Metro.Controls
     public class ColorCanvas: Control
     {
 
-        public static readonly DependencyProperty SelectedColorItemProperty = DependencyProperty.Register("SelectedColorItem", typeof(ColorPickerItem), typeof(ColorCanvas), new PropertyMetadata(new ColorPickerItem(Colors.GreenYellow)));
-            
-        public ColorPickerItem SelectedColorItem
+        //public static readonly DependencyProperty SelectedColorItemProperty = DependencyProperty.Register("SelectedColorItem", typeof(ColorPickerItem), typeof(ColorCanvas), new PropertyMetadata(new ColorPickerItem(Colors.GreenYellow)));
+
+        //public ColorPickerItem SelectedColorItem
+        //{
+        //    get { return (ColorPickerItem)GetValue(SelectedColorItemProperty); }
+        //    set { SetValue(SelectedColorItemProperty, value); }
+        //}
+
+        //public Color SelectedColor => SelectedColorItem.RGB;
+
+
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register("SelectedColor", typeof(Color), typeof(ColorCanvas), new FrameworkPropertyMetadata(Colors.Black, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty AProperty = DependencyProperty.Register("A", typeof(byte), typeof(ColorCanvas), new FrameworkPropertyMetadata((byte)255, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ColorChannelChanged));
+        public static readonly DependencyProperty RProperty = DependencyProperty.Register("R", typeof(byte), typeof(ColorCanvas), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ColorChannelChanged));
+        public static readonly DependencyProperty GProperty = DependencyProperty.Register("G", typeof(byte), typeof(ColorCanvas), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ColorChannelChanged));
+        public static readonly DependencyProperty BProperty = DependencyProperty.Register("B", typeof(byte), typeof(ColorCanvas), new FrameworkPropertyMetadata((byte)0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, ColorChannelChanged));
+
+        public static readonly DependencyProperty HueProperty = DependencyProperty.Register("Hue", typeof(double), typeof(ColorCanvas), new PropertyMetadata(0d));
+        public static readonly DependencyProperty SaturationProperty = DependencyProperty.Register("Saturation", typeof(double), typeof(ColorCanvas), new PropertyMetadata(0d));
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(double), typeof(ColorCanvas), new PropertyMetadata(0d));
+
+        public Color SelectedColor
         {
-            get { return (ColorPickerItem)GetValue(SelectedColorItemProperty); }
-            set { SetValue(SelectedColorItemProperty, value); }
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
         }
 
-        public Color SelectedColor => SelectedColorItem.RGB;
+        #region ARGB
+        public byte A
+        {
+            get { return (byte)GetValue(AProperty); }
+            set { SetValue(AProperty, value); }
+        }
+
+        public byte R
+        {
+            get { return (byte)GetValue(RProperty); }
+            set { SetValue(RProperty, value); }
+        }
+
+        public byte G
+        {
+            get { return (byte)GetValue(GProperty); }
+            set { SetValue(GProperty, value); }
+        }
+
+
+        public byte B
+        {
+            get { return (byte)GetValue(BProperty); }
+            set { SetValue(BProperty, value); }
+        }
+
+        public static void ColorChannelChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is ColorCanvas colorCanvas)
+            {
+                colorCanvas.SetCurrentValue(SelectedColorProperty, Color.FromArgb(colorCanvas.A, colorCanvas.R, colorCanvas.G, colorCanvas.B));
+
+                var hsv = new HSVColor(colorCanvas.SelectedColor);
+                colorCanvas.SetCurrentValue(HueProperty, hsv.Hue);
+                colorCanvas.SetCurrentValue(SaturationProperty, hsv.Saturation);
+                colorCanvas.SetCurrentValue(ValueProperty, hsv.Value);
+            }
+        }
+        #endregion
+
+
+        public double Hue
+        {
+            get { return (double)GetValue(HueProperty); }
+            set { SetValue(HueProperty, value); }
+        }
+
+        public double Saturation
+        {
+            get { return (double)GetValue(SaturationProperty); }
+            set { SetValue(SaturationProperty, value); }
+        }
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
 
     }
 }
---
+++ src/MahApps.Metro/Controls/ColorPicker/HSVColor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls.ColorPicker
+{
+    public struct HSVColor
+    {
+
+        public double Hue { get; private set; }
+        public double Saturation { get; private set; }
+        public double Value { get; private set; }
+
+        public HSVColor(Color color)
+        {
+            Hue = 0;
+            Saturation = 0;
+            Value = 0;
+
+            var max = Math.Max(color.R, Math.Max(color.G, color.B));
+            var min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            // H
+
+            if (max == color.R)
+            {
+                Hue = 60 * (0 + (color.G - color.B) * 1.0 / (max - min));
+            }
+            else if (max == color.G)
+            {
+                Hue = 60 * (2 + (color.B - color.R) * 1.0 / (max - min));
+            }
+            else if (max == color.B)
+            {
+                Hue = 60 * (4 + (color.R - color.G) * 1.0 / (max - min));
+            }
+
+            if (Hue < 0)
+            {
+                Hue = Hue + 360;
+            }
+
+            // S 
+            if (max == 0)
+            {
+                Saturation = 0;
+            }
+            else
+            {
+                Saturation = (max - min) * 1d / max;
+            }
+
+            // V
+            Value = max / 255d;
+        }
+    }
+}
