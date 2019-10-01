36ba46033edea5b7ff60d6287eb447766e7f2774
punker76 <punkerat76@gmail.com>
2019-10-01 16:19:39 +0200

    Allow using own items together with the new HamburgerMenuItemStyleSelector
    The new style selector doesn't work together with other items then directly derived from HamburgerMenuItem class. This commit fixes this, so it's possible to use any other classes too. Additionally it's possible to use classes derived from the new interface IHamburgerMenuItem.

---
+++ src/MahApps.Metro/Controls/HamburgerMenu/MenuItems/IHamburgerMenuItem.cs
@@ -0,0 +1,13 @@
+﻿namespace MahApps.Metro.Controls
+{
+    public interface IHamburgerMenuItem
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this item is enabled in the user interface (UI).
+        /// </summary>
+        /// <returns>
+        /// true if the item is enabled; otherwise, false. The default value is true.
+        /// </returns>
+        bool IsEnabled { get; set; }
+    }
+}
