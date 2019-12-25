b8ae0e191c60451b6536ae2368673897868d8115
punker76 <punkerat76@gmail.com>
2019-12-25 13:11:59 +0100

    (GH-3715) Code format ContentControlEx

--- src/MahApps.Metro/Controls/ContentControlEx.cs
+++ src/MahApps.Metro/Controls/ContentControlEx.cs
@@ -5,44 +5,39 @@ namespace MahApps.Metro.Controls
 {
     public class ContentControlEx : ContentControl
     {
-        /// <summary>
-        /// The DependencyProperty for the CharacterCasing property.
-        /// Controls whether or not content is converted to upper or lower case
-        /// Default Value: CharacterCasing.Normal
-        /// </summary>
-        public static readonly DependencyProperty ContentCharacterCasingProperty =
-            DependencyProperty.Register("ContentCharacterCasing",
-                                        typeof(CharacterCasing),
-                                        typeof(ContentControlEx),
-                                        new FrameworkPropertyMetadata(CharacterCasing.Normal, FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsMeasure),
-                                        value => CharacterCasing.Normal <= (CharacterCasing)value && (CharacterCasing)value <= CharacterCasing.Upper);
+        /// <summary>Identifies the <see cref="ContentCharacterCasing"/> dependency property.</summary>
+        public static readonly DependencyProperty ContentCharacterCasingProperty
+            = DependencyProperty.Register(
+                nameof(ContentCharacterCasing),
+                typeof(CharacterCasing),
+                typeof(ContentControlEx),
+                new FrameworkPropertyMetadata(CharacterCasing.Normal, FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsMeasure),
+                value => CharacterCasing.Normal <= (CharacterCasing)value && (CharacterCasing)value <= CharacterCasing.Upper);
 
         /// <summary> 
-        /// Character casing of the Content
+        /// Gets or sets the character casing of the Content.
         /// </summary> 
         public CharacterCasing ContentCharacterCasing
         {
-            get { return (CharacterCasing)GetValue(ContentCharacterCasingProperty); }
-            set { SetValue(ContentCharacterCasingProperty, value); }
+            get { return (CharacterCasing)this.GetValue(ContentCharacterCasingProperty); }
+            set { this.SetValue(ContentCharacterCasingProperty, value); }
         }
 
-        /// <summary>
-        /// The DependencyProperty for the RecognizesAccessKey property. 
-        /// Default Value: false 
-        /// </summary> 
-        public static readonly DependencyProperty RecognizesAccessKeyProperty =
-            DependencyProperty.Register("RecognizesAccessKey",
-                                        typeof(bool),
-                                        typeof(ContentControlEx),
-                                        new FrameworkPropertyMetadata(false));
+        /// <summary>Identifies the <see cref="RecognizesAccessKey"/> dependency property.</summary>
+        public static readonly DependencyProperty RecognizesAccessKeyProperty
+            = DependencyProperty.Register(
+                nameof(RecognizesAccessKey),
+                typeof(bool),
+                typeof(ContentControlEx),
+                new FrameworkPropertyMetadata(false));
 
         /// <summary> 
         /// Determine if the inner ContentPresenter should use AccessText in its style
         /// </summary> 
         public bool RecognizesAccessKey
         {
-            get { return (bool)GetValue(RecognizesAccessKeyProperty); }
-            set { SetValue(RecognizesAccessKeyProperty, value); }
+            get { return (bool)this.GetValue(RecognizesAccessKeyProperty); }
+            set { this.SetValue(RecognizesAccessKeyProperty, value); }
         }
 
         static ContentControlEx()
