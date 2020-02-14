95c06ae17b8d94255b6f3313e4d9fed7f07b811d
punker76 <punkerat76@gmail.com>
2020-02-14 23:50:46 +0100

    Remove MahApps.Brushes.WhiteColor, because we have already MahApps.Brushes.White

--- src/MahApps.Metro/Controls/Flyout.cs
+++ src/MahApps.Metro/Controls/Flyout.cs
@@ -342,7 +342,6 @@ namespace MahApps.Metro.Controls
             resources["MahApps.Brushes.Flyout.Background"] = newBrush;
             resources["MahApps.Brushes.Control.Background"] = newBrush;
             resources["MahApps.Brushes.White"] = newBrush;
-            resources["MahApps.Brushes.WhiteColor"] = newBrush;
             resources["MahApps.Brushes.DisabledWhite"] = newBrush;
             resources["MahApps.Brushes.Window.Background"] = newBrush;
             resources[SystemColors.WindowBrushKey] = newBrush;
