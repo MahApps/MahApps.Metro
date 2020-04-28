213117db18a429f0eeb8dbd4db128654820898c7
punker76 <punkerat76@gmail.com>
2020-04-28 22:54:29 +0200

    (GH-3768) Add Visibility property to HambugerMenuItemBase
    (GH-3768) Add Visibility property to HambugerMenuItemBase

--- src/MahApps.Metro/Controls/HamburgerMenu/MenuItems/HamburgerMenuItemBase.cs
+++ src/MahApps.Metro/Controls/HamburgerMenu/MenuItems/HamburgerMenuItemBase.cs
@@ -10,6 +10,11 @@ namespace MahApps.Metro.Controls
         public static readonly DependencyProperty TagProperty = DependencyProperty.Register(nameof(Tag), typeof(object), typeof(HamburgerMenuItemBase), new PropertyMetadata(null));
 
         /// <summary>
+        /// Identifies the <see cref="Visibility" /> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty VisibilityProperty = DependencyProperty.Register(nameof(Visibility), typeof(Visibility), typeof(HamburgerMenuItemBase), new PropertyMetadata(Visibility.Visible));
+
+        /// <summary>
         /// Gets or sets a value that specifies an user specific value.
         /// </summary>
         public object Tag
@@ -23,6 +28,18 @@ namespace MahApps.Metro.Controls
             {
                 this.SetValue(TagProperty, value);
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the user interface (UI) visibility of this element. This is a dependency property.
+        /// </summary>
+        /// <returns>
+        /// A value of the enumeration. The default value is System.Windows.Visibility.Visible.
+        /// </returns>
+        public Visibility Visibility
+        {
+            get { return (Visibility)GetValue(VisibilityProperty); }
+            set { SetValue(VisibilityProperty, value); }
         }
 
         protected override Freezable CreateInstanceCore()
