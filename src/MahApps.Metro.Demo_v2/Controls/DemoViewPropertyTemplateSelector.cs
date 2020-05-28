5b92cef41b48f5f5ad963ebd159af8da364e8830
Tim <tim.uniewski@gmail.com>
2020-05-28 14:45:01 +0200

    Init new demo App

---
+++ src/MahApps.Metro.Demo_v2/Controls/DemoViewPropertyTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MahApps.Demo.Controls
+{
+    public class DemoViewPropertyTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FallbackTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is DemoViewProperty demoProperty)
+            {
+                return demoProperty.DataTemplate ?? FallbackTemplate;
+            }
+            else
+            {
+                return FallbackTemplate;
+            }
+        }
+    }
+}
---
+++ src/MahApps.Metro.Demo_v2/Converter/EnumToItemSourceConverter.cs
@@ -0,0 +1,27 @@
+﻿using MahApps.Metro.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahApps.Demo.Converter
+{
+    public class EnumToItemSourceConverter : MarkupConverter
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                return Enum.GetValues(value.GetType());
+            }
+            throw new ArgumentException("the provided value is not a valid Enum");
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
