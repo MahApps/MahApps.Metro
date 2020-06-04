6d34b6becfe956a0ef1c7b1d2de62f48bde0493a
Tim <tim.uniewski@gmail.com>
2020-06-04 10:02:03 +0200

    Implement a BaseClass which support:
    - INotifyPropertyChanged
    - INotifyDataErrorInfo

---
+++ src/MahApps.Metro.Demo_v2/Core/BaseClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MahApps.Demo.Core
+{
+    public class BaseClass : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        #region INotifyPropertyChanged
+
+        // This event tells the UI to update
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        #endregion INotifyPropertyChanged
+
+        #region INotifyDataErrorInfo
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public bool HasErrors => _errorsByPropertyName.Count > 0;
+
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (string.IsNullOrEmpty(propertyName))
+            {
+                return null;
+            }
+            else
+            {
+                return _errorsByPropertyName.ContainsKey(propertyName)
+                        ? _errorsByPropertyName[propertyName]
+                        : null;
+            }
+        }
+
+        public bool GetHasError(string PropertyName)
+        {
+            return _errorsByPropertyName.ContainsKey(PropertyName);
+        }
+
+        public void AddError(string propertyName, string error)
+        {
+            if (!_errorsByPropertyName.ContainsKey(propertyName))
+                _errorsByPropertyName[propertyName] = new List<string>();
+
+            if (!_errorsByPropertyName[propertyName].Contains(error))
+            {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        public void ClearErrors(string propertyName)
+        {
+            if (_errorsByPropertyName.ContainsKey(propertyName))
+            {
+                _errorsByPropertyName.Remove(propertyName);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        public void OnErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        #endregion INotifyDataErrorInfo
+    }
+}
