6ba2db2d422fff00a4aa949250749331455299fd
punker76 <punkerat76@gmail.com>
2020-06-17 00:52:18 +0200

    (GH-3839) Allow setting AutomationProperties for HamburgerMenuItem
    Allow setting the AutomationProperties for HamburgerMenuItem
    
    - AutomationProperties.HelpText
    - AutomationProperties.LabeledBy
    - AutomationProperties.Name
    
    If Name or HelpText are empty the Label and ToolTip properties will be used.

---
+++ src/MahApps.Metro/Converters/HamburgerMenuItemAccessibleConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace MahApps.Metro.Converters
+{
+    [ValueConversion(typeof(object), typeof(object))]
+    [MarkupExtensionReturnType(typeof(HamburgerMenuItemAccessibleConverter))]
+    public class HamburgerMenuItemAccessibleConverter : MarkupMultiConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+            {
+                return Binding.DoNothing;
+            }
+
+            return value;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is null)
+            {
+                return Binding.DoNothing;
+            }
+
+            var automationPropertiesValue = values.ElementAtOrDefault(1) as string;
+            if (!string.IsNullOrEmpty(automationPropertiesValue))
+            {
+                return automationPropertiesValue;
+            }
+
+            return this.Convert(values.ElementAtOrDefault(0), targetType, parameter, culture);
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return targetTypes.Select(t => Binding.DoNothing).ToArray();
+        }
+    }
+}
