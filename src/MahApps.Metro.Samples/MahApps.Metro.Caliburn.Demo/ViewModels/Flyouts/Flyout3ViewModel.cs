0caa8c075c40011cd5ba6278bc796d1bc4a96c50
punker76 <punkerat76@gmail.com>
2020-07-11 00:17:20 +0200

    Add license file header to demo and test proojects

--- src/MahApps.Metro.Samples/MahApps.Metro.Caliburn.Demo/ViewModels/Flyouts/Flyout3ViewModel.cs
+++ src/MahApps.Metro.Samples/MahApps.Metro.Caliburn.Demo/ViewModels/Flyouts/Flyout3ViewModel.cs
@@ -1,4 +1,8 @@
-﻿using Caliburn.Micro;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Caliburn.Micro;
 using MahApps.Metro.Controls;
 //using MetroDemo.Models;
 
--- src/Mahapps.Metro.Tests/GlobalSuppressions.cs
+++ src/Mahapps.Metro.Tests/GlobalSuppressions.cs
@@ -1,6 +1,5 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("WpfAnalyzers.DependencyProperty", "WPF0041:Set mutable dependency properties using SetCurrentValue.", Justification = "Not needed for test projects.")]
