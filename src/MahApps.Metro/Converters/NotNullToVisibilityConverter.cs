68acd71a75a629788ccbab6cce5b3aa378926971
Tim <tim.uniewski@gmail.com>
2020-07-12 19:55:45 +0200

    Use NotNullToVisibilityConverter instead of DataTemplate.Triggers

---
+++ src/MahApps.Metro/Converters/NotNullToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MahApps.Metro.Converters
+{
+    public class NotNullToVisibilityConverter : MarkupConverter
+    {
+        NotNullToVisibilityConverter _instance;
+
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (_instance == null) _instance = new NotNullToVisibilityConverter();
+            return _instance;
+        }
+    }
+}
