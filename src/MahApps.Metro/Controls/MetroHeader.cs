37d10ce05be4f50c63f0f7ca2bd35fbe0ce9c3f4
punker76 <punkerat76@gmail.com>
2021-12-30 00:32:46 +0100

    feat(controls): add orientation property to MetroHeader

--- src/MahApps.Metro/Controls/MetroHeader.cs
+++ src/MahApps.Metro/Controls/MetroHeader.cs
@@ -11,14 +11,38 @@ namespace MahApps.Metro.Controls
 {
     public class MetroHeader : GroupBox
     {
+        /// <summary>Identifies the <see cref="Orientation"/> dependency property.</summary>
+        public static readonly DependencyProperty OrientationProperty
+            = DependencyProperty.Register(nameof(Orientation),
+                                          typeof(Orientation),
+                                          typeof(MetroHeader),
+                                          new PropertyMetadata(Orientation.Vertical));
+
+        /// <summary>
+        /// Gets or sets the <see cref="Orientation"/> used for the header.
+        /// </summary>
+        /// <remarks>
+        /// If set to <see cref="System.Windows.Controls.Orientation.Vertical"/> the header will be above the content.
+        /// If set to <see cref="System.Windows.Controls.Orientation.Horizontal"/> the header will be to the left of the content.
+        /// </remarks>
+        public Orientation Orientation
+        {
+            get => (Orientation)this.GetValue(OrientationProperty);
+            set => this.SetValue(OrientationProperty, value);
+        }
+
         static MetroHeader()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(MetroHeader), new FrameworkPropertyMetadata(typeof(MetroHeader)));
         }
 
-        /// <summary>
-        /// Creates AutomationPeer (<see cref="M:System.Windows.UIElement.OnCreateAutomationPeer" />)
-        /// </summary>
+        /// <inheritdoc />
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        /// <inheritdoc />
         protected override AutomationPeer OnCreateAutomationPeer()
         {
             return new MetroHeaderAutomationPeer(this);
