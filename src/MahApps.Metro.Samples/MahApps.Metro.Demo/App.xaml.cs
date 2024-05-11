73ed66b98ae1a6bd72d6cf20d36052c0de53c625
Bastian Schmidt <batzen@gmx.org>
2024-05-11 08:05:53 +0200

    Improving native ContextMenu in demo

--- src/MahApps.Metro.Samples/MahApps.Metro.Demo/App.xaml.cs
+++ src/MahApps.Metro.Samples/MahApps.Metro.Demo/App.xaml.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Windows;
+using ControlzEx.Theming;
 
 namespace MetroDemo
 {
@@ -11,5 +12,28 @@ namespace MetroDemo
     /// </summary>
     public partial class App : Application
     {
+        /// <inheritdoc />
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+            ThemeManager.Current.SyncTheme();
+
+            ThemeManager.Current.ThemeChanged += HandleThemeChanged;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            AppModeHelper.SyncAppMode();
+#pragma warning restore CS0618 // Type or member is obsolete
+
+            base.OnStartup(e);
+
+            return;
+
+            void HandleThemeChanged(object? sender, ThemeChangedEventArgs themeChangedEventArgs)
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                AppModeHelper.SyncAppMode();
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+        }
     }
 }
