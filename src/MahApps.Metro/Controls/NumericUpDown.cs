caa0bb26e4299fa6cb38f0df429df936e7619546
Vinicius Cunha <vinicius.cunha36@hotmail.com>
2026-06-29 16:35:53 -0300

    feat(NumericUpDown): add SyncTextWithValueWhileEditing to refresh text on external value change

--- src/MahApps.Metro/Controls/NumericUpDown.cs
+++ src/MahApps.Metro/Controls/NumericUpDown.cs
@@ -365,6 +365,28 @@ namespace MahApps.Metro.Controls
             set => this.SetValue(InterceptManualEnterProperty, BooleanBoxes.Box(value));
         }
 
+        /// <summary>Identifies the <see cref="SyncTextWithValueWhileEditing"/> dependency property.</summary>
+        public static readonly DependencyProperty SyncTextWithValueWhileEditingProperty
+            = DependencyProperty.Register(nameof(SyncTextWithValueWhileEditing),
+                                          typeof(bool),
+                                          typeof(NumericUpDown),
+                                          new PropertyMetadata(BooleanBoxes.FalseBox));
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the displayed text is kept in sync with the
+        /// <see cref="Value"/> while the control is being edited (has keyboard focus), when the value
+        /// is changed from an external source such as a binding. When set to <see langword="false"/>
+        /// (the default), the current behavior is kept: the text is only refreshed from the value once
+        /// the control loses focus.
+        /// </summary>
+        [Category("Behavior")]
+        [DefaultValue(false)]
+        public bool SyncTextWithValueWhileEditing
+        {
+            get => (bool)this.GetValue(SyncTextWithValueWhileEditingProperty);
+            set => this.SetValue(SyncTextWithValueWhileEditingProperty, BooleanBoxes.Box(value));
+        }
+
         /// <summary>Identifies the <see cref="Value"/> dependency property.</summary>
         public static readonly DependencyProperty ValueProperty
             = DependencyProperty.Register(nameof(Value),
@@ -1234,6 +1256,15 @@ namespace MahApps.Metro.Controls
                 }
 
                 if (this.valueTextBox != null)
+                {
+                    this.InternalSetText(newValue);
+                }
+            }
+            else if (this.SyncTextWithValueWhileEditing && this.valueTextBox != null)
+            {
+                var expectedText = newValue.HasValue ? FormattedValueString(newValue.Value, this.StringFormat, this.SpecificCultureInfo)
+                                                     : null;
+                if (!string.Equals(this.valueTextBox.Text, expectedText, StringComparison.Ordinal))
                 {
                     this.InternalSetText(newValue);
                 }
